0e490a6388672afa5eed70171a35e1c8ad0ab731
Mikhail Shamin <mshamin@crystals.ru>
2013-02-21 16:21:31 +0400

    Added capistrano deploy config

---
+++ app/config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "LightHouse"
+set :domain,      "cs-watchman"
+set :deploy_to,   "/var/www/lighthouse"
+set :app_path,    "app"
+set :user,        "watchman"
+
+set :repository,  "git@cs-watchman:lighthouse.git"
+set :scm,         :git
+
+ssh_options[:forward_agent] = true
+
+set :deploy_via, :remote_cache
+set :use_sudo, false
+default_run_options[:pty] = true
+
+set :use_composer, true
+set :update_vendors, true
+
+set :use_set_permissions,   true
+set :permission_method,   :acl
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+
+set  :keep_releases,  5
+
+logger.level = Logger::DEBUG
