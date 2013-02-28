a0b4abda35332b1664fcd3e476ac62c94a4da0dd
Mikhail Shamin <mshamin@crystals.ru>
2013-02-28 15:51:00 +0400

    Added create index.xml task in webfront deploy

--- webfront/config/deploy.rb
+++ webfront/config/deploy.rb
@@ -1,4 +1,3 @@
-set :stages, %w(staging autotests production)
 set :default_stage, "staging"
 set :stage_dir, "config/stages"
 
@@ -30,11 +29,21 @@ set :normalize_asset_timestamps, false
 
 namespace :lighthouse do
 
+  task :default do
+    build
+    create_index
+  end
+
   desc "Build webfront app"
   task :build, :roles => :app, :except => { :no_release => true } do
-    stream "#{try_sudo} sh -c 'cd #{latest_release} && ./mayak_build.cmd'"
+    run "cd #{latest_release} && ./mayak_build.cmd"
+  end
+
+  desc "Copy index_%env%.xml"
+  task :create_index, :roles => :app, :except => { :no_release => true } do
+    run "cd #{latest_release} && cp index.#{stage}.xml index.xml"
   end
 
 end
 
-after "deploy:finalize_update", "lighthouse:build"
+after "deploy:finalize_update", "lighthouse"
