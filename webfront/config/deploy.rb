095dc9b44b0f6801a77008b45102449350bdc4af
Mikhail Shamin <mshamin@crystals.ru>
2013-02-28 19:21:03 +0400

    Changed api paths in index.xml files

--- webfront/config/deploy.rb
+++ webfront/config/deploy.rb
@@ -15,7 +15,7 @@ set :scm,         :git
 
 ssh_options[:forward_agent] = true
 
-set :use_sudo, true
+set :use_sudo, false
 default_run_options[:pty] = true
 
 role :web,        domain                         # Your HTTP server, Apache/etc
