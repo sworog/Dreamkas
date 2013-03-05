0cbeb83ceed9f8b192d8a3120361f6c894ed26e1
Andrey <atolpeev@crystals.ru>
2013-03-05 16:15:19 +0400

    Added logger;

--- autotests/src/test/java/project/lighthouse/autotests/jbehave/AcceptanceTestSuite.java
+++ autotests/src/test/java/project/lighthouse/autotests/jbehave/AcceptanceTestSuite.java
@@ -1,9 +1,13 @@
 package project.lighthouse.autotests.jbehave;
 
 import net.thucydides.jbehave.ThucydidesJUnitStories;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.Charset;
 
 public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 	public AcceptanceTestSuite() {
-		findStoriesIn("**/Actual/");		
+        LoggerFactory.getLogger(AcceptanceTestSuite.class).info(Charset.defaultCharset().toString());
+        findStoriesIn("**/Actual/");
 	}
 }
