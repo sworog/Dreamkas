f0f9131eb539d2af86c9e79a1eee3ff0e507ec6a
Fedot <vfedorov@crystals.ru>
2013-03-05 17:22:17 +0000

    Add rest bundle to appkernel

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new LightHouse\CoreBundle\LightHouseCoreBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
