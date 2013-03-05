6c5984618d3b9dc89879e1ca451b8dad4c15ff44
Mikhail Shamin <mshamin@crystals.ru>
2013-03-05 17:42:52 +0400

    Added JMSSerializerBundle

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new LightHouse\CoreBundle\LightHouseCoreBundle(),
             new FOS\RestBundle\FOSRestBundle(),
         );
