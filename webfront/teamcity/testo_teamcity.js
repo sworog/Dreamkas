baf8e5f5fea1a90c59e82a54798c16933b6819ed
Дмитрий Карловский <dkarlovskiy@crystals.ru>
2013-03-06 16:26:43 +0400

    убираем пинги и редуцирование имен

--- webfront/teamcity/testo_teamcity.js
+++ webfront/teamcity/testo_teamcity.js
@@ -30,26 +30,18 @@ function run(){
             console.log( "##teamcity[message text='timeout']" )
         }, 30000 )
         
-        var i= 0
-        var interval= setInterval( function(){
-            console.log(i++)
-            log(i)
-        },100)
-        
         socket.on( 'test:done', function( states ){
-            clearInterval( interval )
-            console.log( "0" )
             console.log( "\n##teamcity[testSuiteStarted name='browser.unittest']\n" )
             console.log( "\n##teamcity[message text='count: " + Object.keys( states ).length + "']\n" )
             
             for( var agent in states ){
                 agent= agent.replace( /'/g, "|'" ).replace( /\n/g, "|n" ).replace( /\r/g, "|r" ).replace( /\|/g, "||" ).replace( /\]/g, "|]" )
-                console.log( "##teamcity[testStarted name='" + agent[0] + "']" )
+                console.log( "##teamcity[testStarted name='" + agent + "']" )
                 
-                if( !states[ agent ] ) console.log( "##teamcity[testFailed  name='" + agent[0] + "']" )
+                if( !states[ agent ] ) console.log( "##teamcity[testFailed  name='" + agent + "']" )
                 
-                console.log( "##teamcity[testFinished name='" + agent[0] + "']" )
-                log( "##teamcity[testFinished name='" + agent[0] + "']" )
+                console.log( "##teamcity[testFinished name='" + agent + "']" )
+                log( "##teamcity[testFinished name='" + agent + "']" )
             }
             
             console.log( "\n##teamcity[testSuiteFinished name='browser.unittest']\n" )
