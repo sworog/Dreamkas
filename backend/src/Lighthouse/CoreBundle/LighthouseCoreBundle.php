18981c478f6ac7b05071e1e80aa18312ced9ebb1
Mikhail Shamin <mshamin@crystals.ru>
2013-03-06 16:21:53 +0400

    Тесты на команду очищения базы данных

--- backend/src/Lighthouse/CoreBundle/LighthouseCoreBundle.php
+++ backend/src/Lighthouse/CoreBundle/LighthouseCoreBundle.php
@@ -2,8 +2,20 @@
 
 namespace Lighthouse\CoreBundle;
 
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LighthouseCoreBundle extends Bundle
 {
+    /**
+     * Register container aware commands
+     *
+     * @param Application $application An Application instance
+     */
+    public function registerCommands(Application $application)
+    {
+        parent::registerCommands($application);
+
+        $application->add($this->container->get('lighthouse.core.command.create_database'));
+    }
 }
