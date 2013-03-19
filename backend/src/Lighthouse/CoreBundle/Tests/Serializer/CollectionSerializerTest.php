2674df642fce2f724324044e1f04dee28f752317
Mikhail Shamin <mshamin@crystals.ru>
2013-03-19 19:46:59 +0400

    Вынес xml для теста коллекции в отдельный файл

--- backend/src/Lighthouse/CoreBundle/Tests/Serializer/CollectionSerializerTest.php
+++ backend/src/Lighthouse/CoreBundle/Tests/Serializer/CollectionSerializerTest.php
@@ -24,39 +24,7 @@ class CollectionSerializerTest extends WebTestCase
         $serializer = $this->getContainer()->get('serializer');
         $result = $serializer->serialize($collection, 'xml');
 
-        $expected = '<?xml version="1.0" encoding="UTF-8"?>
-<tests>
-    <test>
-        <id>1</id>
-        <name><![CDATA[name_1]]></name>
-        <desc><![CDATA[description_1]]></desc>
-        <orderDate><![CDATA[01.03.2013]]></orderDate>
-    </test>
-    <test>
-        <id>2</id>
-        <name><![CDATA[name_2]]></name>
-        <desc><![CDATA[description_2]]></desc>
-        <orderDate><![CDATA[02.03.2013]]></orderDate>
-    </test>
-    <test>
-        <id>3</id>
-        <name><![CDATA[name_3]]></name>
-        <desc><![CDATA[description_3]]></desc>
-        <orderDate><![CDATA[03.03.2013]]></orderDate>
-    </test>
-    <test>
-        <id>4</id>
-        <name><![CDATA[name_4]]></name>
-        <desc><![CDATA[description_4]]></desc>
-        <orderDate><![CDATA[04.03.2013]]></orderDate>
-    </test>
-    <test>
-        <id>5</id>
-        <name><![CDATA[name_5]]></name>
-        <desc><![CDATA[description_5]]></desc>
-        <orderDate><![CDATA[05.03.2013]]></orderDate>
-    </test>
-</tests>';
-        $this->assertXmlStringEqualsXmlString($expected, $result);
+        $expectedFile = __DIR__ . '/../Fixtures/Document/TestCollection.xml';
+        $this->assertXmlStringEqualsXmlFile($expectedFile, $result);
     }
 }
