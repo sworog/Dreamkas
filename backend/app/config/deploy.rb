707c1cdb22bbfab4f11305036b42f7d58d8648ef
Mikhail Shamin <mshamin@crystals.ru>
2013-03-20 20:20:31 +0400

    Деплой апи на разные хосты

--- backend/app/config/deploy.rb
+++ backend/app/config/deploy.rb
@@ -3,7 +3,16 @@ set :stage_dir, "app/config/stages"
 
 require 'capistrano/ext/multistage'
 
-set :application, "api"
+def current_branch
+    `git branch`.match(/\* (\S+)\s/m)[1]\
+end
+
+set :branch, current_branch || "master" unless exists?(:branch)
+
+puts "--> Branch ".yellow + "#{branch}".red + " will be used for deploy".yellow
+
+set :host, branch unless exists?(:host)
+set :application, "#{host}.api"
 set :domain,      "alexandria.lighthouse.cs"
 set :deploy_to,   "/var/www/#{application}"
 set :deploy_via,  :remote_cache_subfolder
@@ -26,7 +35,7 @@ set :update_vendors, true
 set :use_set_permissions,   false
 
 role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain                         # This may be the same as your `Web` server
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
 set  :keep_releases,  5
@@ -53,4 +62,31 @@ namespace :symfony do
         end
     end
 
+end
+
+namespace :deploy do
+
+    desc "Check & setup environment if needed"
+    task :setup_if_needed, :roles => :app, :except => { :no_release => true } do
+        begin
+            check
+        rescue Exception => error
+            setup
+        end
+    end
+
+    task :restart, :roles => :app, :except => { :no_release => true } do
+        run "sudo service php5-fpm reload"
+        puts "--> PHP-FPM was successfully reloaded".green
+    end
+end
+
+after "multistage:ensure" do
+    set :application, "#{host}.api.#{stage}"
+    set :deploy_to,   "/var/www/#{application}"
+    set :symfony_env_prod, stage
+end
+
+after "deploy:restart" do
+    puts "--> API was successfully deployed to ".green + "http://#{application}.lighthouse.cs".yellow
 end
--- backend/app/config/stages/staging.rb
+++
@@ -1,2 +0,0 @@
-set :symfony_env_prod, "staging"
-set :deploy_to,   "/var/www/#{application}-staging"
