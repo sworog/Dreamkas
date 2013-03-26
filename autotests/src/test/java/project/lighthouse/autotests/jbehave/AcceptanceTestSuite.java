afbdef90a701dc7a16723c91fbd4fac38c3e1423
Andrey <atolpeev@crystals.ru>
2013-03-26 17:07:47 +0400

    Изменены названия пакетов с историями;

--- autotests/src/test/java/project/lighthouse/autotests/jbehave/AcceptanceTestSuite.java
+++ autotests/src/test/java/project/lighthouse/autotests/jbehave/AcceptanceTestSuite.java
@@ -7,18 +7,19 @@ public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 
     public static final String CURRENT_SPRINT = "lighthouse.autotests.sprint";
     public static final String CURRENT_STORY = "lighthouse.autotests.story.name";
+    public static final String CURRENT_BRANCH = "lighthouse.autotests.branch";
 
 	public AcceptanceTestSuite() {
 
         EnvironmentVariables environmentVariables = getEnvironmentVariables();
-        String sprint = environmentVariables.getProperty(CURRENT_SPRINT, null);
-        String story = environmentVariables.getProperty(CURRENT_STORY, null);
-        if(sprint != null) {
-            String storiesPath = "**/sprint".concat(sprint);
-            findStoriesIn(storiesPath);
-        }
-        if (story != null){
-            findStoriesCalled(story);
+        String branch = environmentVariables.getProperty(CURRENT_BRANCH, null);
+        if(branch != null){
+            if(branch.startsWith("us-")){
+                findStoriesCalled(branch.substring(3));
+            }
+            if(branch.startsWith("sprint-")){
+                findStoriesIn("**/" + branch);
+            }
         }
 	}
 }
