3282c18df4d968f870546d6d7d869804531465e7
Mikhail Shamin <mshamin@crystals.ru>
2013-03-26 20:29:16 +0400

    Слегка ускорил тесты бека

--- backend/src/Lighthouse/CoreBundle/Test/WebTestCase.php
+++ backend/src/Lighthouse/CoreBundle/Test/WebTestCase.php
@@ -5,18 +5,33 @@ namespace Lighthouse\CoreBundle\Test;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseTestCase;
 use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use AppKernel;
 
 class WebTestCase extends BaseTestCase
 {
     protected function setUp()
     {
-        static::initKernel();
     }
 
+    /**
+     * @return AppKernel
+     */
     protected static function initKernel()
     {
         static::$kernel = static::createKernel();
         static::$kernel->boot();
+        return static::$kernel;
+    }
+
+    /**
+     * @param array $options
+     * @return AppKernel
+     */
+    protected static function createKernel(array $options = array())
+    {
+        $options['debug'] = isset($options['debug']) ? $options['debug'] : false;
+        return parent::createKernel($options);
     }
 
     /**
@@ -24,7 +39,7 @@ class WebTestCase extends BaseTestCase
      */
     protected function getContainer()
     {
-        return static::$kernel->getContainer();
+        return static::initKernel()->getContainer();
     }
 
     protected function clearMongoDb()
