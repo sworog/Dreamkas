4f049dfd068299801e283b4944e3ca73db05965a
Mikhail Shamin <mshamin@crystals.ru>
2013-03-26 14:02:04 +0400

    Зарефакторил деплой рецепт, добавил описания таск

--- deploy/capistrano/recipes/deploy.rb
+++ deploy/capistrano/recipes/deploy.rb
@@ -16,6 +16,7 @@ namespace :deploy do
         capifony_puts_ok
     end
 
+    desc "Init deploy configuration using current git branch"
     task :init do
 
         current_branch = `git rev-parse --abbrev-ref HEAD`.delete("\n")
@@ -33,15 +34,26 @@ namespace :deploy do
         puts "--> Application will be deployed to ".yellow + application_url.red
     end
 
+    desc "Setup parameters: upload parameters.yml and rename database"
     task :setup_parameters, :roles => :app, :except => { :no_release => true } do
+        upload_parameters
+        rename_database_name
+    end
+
+    desc "Upload current parameters.yml to shared folder"
+    task :upload_parameters, :roles => :app, :except => { :no_release => true } do
         origin_file = "app/config/parameters.yml"
-        destination_file = shared_path + "/app/config/parameters.yml" # Notice the shared_path
+        destination_file = shared_path + "/app/config/parameters.yml"
 
         try_sudo "mkdir -p #{File.dirname(destination_file)}"
         top.upload(origin_file, destination_file)
+    end
 
-        database_name = application.gsub(/\./, '_')
-        puts "--> Database name in ".yellow + "parameters.yml".bold.yellow + " will be set to ".yellow + database_name.red
+    desc "Rename database_name in app/config/parameters.yml. Application name will be used (%branch.stage.env%) unless -S database_name=%database_name% argument is provided"
+    task :rename_database_name, :roles => :app, :except => { :no_release => true } do
+        set :database_name, application.gsub(/\./, '_') unless exists?(:database_name)
+        puts "--> Database name in ".yellow + "parameters.yml".bold.yellow + " will be set to ".yellow + "#{database_name}".red
+        destination_file = shared_path + "/app/config/parameters.yml"
         run "sed -r -i 's/^(\\s+database_name:\\s+).+$/\\1#{database_name}/g' #{destination_file}"
     end
 
