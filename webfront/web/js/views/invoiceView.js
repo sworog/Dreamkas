9deff4bea83f1b7e648f40aba515366dcfe90b0f
Fedot <vfedorov@crystals.ru>
2013-04-05 15:35:06 +0000

    Изменил способ приведения даты к нужному виду

--- webfront/web/js/views/invoiceView.js
+++ webfront/web/js/views/invoiceView.js
@@ -15,8 +15,12 @@ var InvoiceView = Backbone.View.extend({
     render: function() {
         var data = this.model.toJSON();
 
-        var date = new Date(data.acceptanceDate);
-        data.acceptanceDate = date.format(this.model.datePrintFormat + " " + this.model.timeFormat);
+        var date = data.acceptanceDate.replace(/(\d+)\-(\d+)\-(\d+)T(\d+):(\d+).*/, "$3.$2.$1 $4:$5");
+        data.acceptanceDate = date;
+
+        if(data.supplierInvoiceDate) {
+            data.supplierInvoiceDate = data.supplierInvoiceDate.replace(/(\d+)\-(\d+)\-(\d+)T(\d+):(\d+).*/, "$3.$2.$1");
+        }
 
         this.$el.html(this.template(data));
 
