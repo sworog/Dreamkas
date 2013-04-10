2a71d51479cfacbc03cf3cfff3ccf835961a2ebf
Mikhail Shamin <mshamin@crystals.ru>
2013-04-10 17:00:10 +0400

    Валидатор range c большим количеством проверок чем в стандартном

---
+++ backend/src/Lighthouse/CoreBundle/Validator/Constraints/Range.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\MissingOptionsException;
+
+/**
+ * @Annotation
+ */
+class Range extends Constraint
+{
+    public $ltMessage           = 'lighthouse.validation.errors.range.lt';
+    public $lteMessage          = 'lighthouse.validation.errors.range.lte';
+    public $gtMessage           = 'lighthouse.validation.errors.range.gt';
+    public $gteMessage          = 'lighthouse.validation.errors.range.gte';
+    public $notNumericMessage   = 'lighthouse.validation.errors.range.not_numeric';
+
+    public $lt;
+    public $lte;
+    public $gt;
+    public $gte;
+
+    public function __construct($options = null)
+    {
+        parent::__construct($options);
+
+        if (null === $this->lt
+            && null === $this->lte
+            && null === $this->gt
+            && null === $this->gte
+        ) {
+            throw new MissingOptionsException(
+                'Either option "gt", "gte", "lt" or "lte" must be given for constraint ' . __CLASS__,
+                array('lt', 'lte', 'gt', 'gte')
+            );
+        }
+    }
+}
---
+++ backend/src/Lighthouse/CoreBundle/Validator/Constraints/RangeValidator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class RangeValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        if (null === $value) {
+            return;
+        }
+
+        if (!is_numeric($value)) {
+            $this->context->addViolation(
+                $constraint->notNumericMessage,
+                array(
+                    '{{ value }}' => $value,
+                )
+            );
+            return;
+        }
+
+        if (null !== $constraint->gt && $value <= $constraint->gt) {
+            $this->context->addViolation(
+                $constraint->gtMessage,
+                array(
+                    '{{ value }}' => $value,
+                    '{{ limit }}' => $constraint->gt,
+                )
+            );
+            return;
+        } elseif (null !== $constraint->gte && $value < $constraint->gte) {
+            $this->context->addViolation(
+                $constraint->gteMessage,
+                array(
+                    '{{ value }}' => $value,
+                    '{{ limit }}' => $constraint->gte,
+                )
+            );
+            return;
+        }
+
+        if (null !== $constraint->lt && $value >= $constraint->lt) {
+            $this->context->addViolation(
+                $constraint->gtMessage,
+                array(
+                    '{{ value }}' => $value,
+                    '{{ limit }}' => $constraint->lt,
+                )
+            );
+            return;
+        } elseif (null !== $constraint->lte && $value > $constraint->lte) {
+            $this->context->addViolation(
+                $constraint->gteMessage,
+                array(
+                    '{{ value }}' => $value,
+                    '{{ limit }}' => $constraint->lte,
+                )
+            );
+            return;
+        }
+    }
+}
