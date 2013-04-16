d876965ac5d93b1cb0ba55b49dc002f6dce23bfb
Andrew Borovin <andrew@borovin.com>
2013-04-16 19:08:20 +0400

    app fix

--- webfront/web/main.js
+++ webfront/web/main.js
@@ -11,6 +11,8 @@ require(
                 pushState: true
             });
 
+            window.app = new Backbone.Router();
+
             $("body").on('click', 'a', function(e) {
                 e.preventDefault();
                 router.navigate($(this).attr('href'), {trigger: true});
