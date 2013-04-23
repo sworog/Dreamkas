158ff3d02ed9ef8125cde86136303ac9a4b0c182
Andrey <atolpeev@crystals.ru>
2013-04-23 11:31:05 +0400

    Добавлен класс для хранения статичных коллекций;

---
+++ autotests/src/main/java/project/lighthouse/autotests/StaticDataCollections.java
@@ -0,0 +1,9 @@
+package project.lighthouse.autotests;
+
+import java.util.ArrayList;
+
+public class StaticDataCollections {
+
+    public static ArrayList<String> products = new ArrayList<>();
+    public static ArrayList<String> invoices = new ArrayList<>();
+}
