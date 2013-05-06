36d83aee7864ea6ec0dcd6fb9f1d441288834a07
Fedot <vfedorov@crystals.ru>
2013-05-06 13:13:16 +0400

    Команда для запуска пересчета средней цены закупки

--- backend/src/Lighthouse/CoreBundle/LighthouseCoreBundle.php
+++ backend/src/Lighthouse/CoreBundle/LighthouseCoreBundle.php
@@ -3,12 +3,15 @@
 namespace Lighthouse\CoreBundle;
 
 use Lighthouse\CoreBundle\DependencyInjection\Compiler\DocumentRepositoryPass;
+use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Doctrine\ODM\MongoDB\Mapping\Types\Type;
 
 class LighthouseCoreBundle extends Bundle
 {
+    protected $commands = array();
+
     public function __construct()
     {
         Type::registerType('money', 'Lighthouse\CoreBundle\Types\MongoDB\MoneyType');
@@ -23,4 +26,13 @@ class LighthouseCoreBundle extends Bundle
 
         $container->addCompilerPass(new DocumentRepositoryPass());
     }
+
+    /*
+    public function registerCommands(Application $application)
+    {
+        $container = new ContainerBuilder();
+        $container->get('lighthouse.core.command.recalculate_average_purchase_price');
+        var_dump($container->get('lighthouse.core.command.recalculate_average_purchase_price'));
+    }
+    */
 }
