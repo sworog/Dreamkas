589117128ee285f36ef9db3bf452d758965f6828
Andrey <atolpeev@crystals.ru>
2013-05-08 13:53:47 +0400

    Поправлены статичные коллекции.

--- autotests/src/main/java/project/lighthouse/autotests/StaticDataCollections.java
+++ autotests/src/main/java/project/lighthouse/autotests/StaticDataCollections.java
@@ -1,13 +1,12 @@
 package project.lighthouse.autotests;
 
-import project.lighthouse.autotests.objects.Invoice;
-import project.lighthouse.autotests.objects.Product;
-
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class StaticDataCollections {
 
-    public static HashMap<String, Product> products = new HashMap<>();
-    public static HashMap<String, Invoice> invoices = new HashMap<>();
+    //    public static HashMap<String, Product> products = new HashMap<>();
+//    public static HashMap<String, Invoice> invoices = new HashMap<>();
+    public static ArrayList<String> products = new ArrayList<>();
+    public static ArrayList<String> invoices = new ArrayList<>();
     public static String TIMEOUT = "5000";
 }
