027dc3c334be022f93beee74a96f20b0f0935ea2
Andrew Borovin <andrew@borovin.com>
2013-05-08 13:00:12 +0400

    calendar

---
+++ webfront/web/libs/jquery.mod.js
@@ -0,0 +1,48 @@
+(function($) {
+    $.fn.mod = function(mod, val) {
+        var modClass;
+
+        if (typeof val == 'undefined') {
+
+            $.each($(this).attr('class').split(' '), function(index, className) {
+                if (className.indexOf(mod) == 0) {
+                    modClass = className;
+                    return false;
+                }
+            });
+
+            return modClass.split('_').pop();
+
+        }
+
+        return this.each(function() {
+
+            $.each($(this).attr('class').split(' '), function(index, className) {
+                if (className.indexOf(mod) >= 0) {
+                    modClass = className;
+                    return false;
+                }
+            });
+
+            if (modClass) {
+                $(this).removeClass(modClass);
+            }
+
+            $(this).addClass(mod + '_' + val);
+
+        });
+
+    };
+
+    $.fn.removeMod = function(mod) {
+        return this.each(function() {
+            var $block = $(this);
+            $.each($(this).attr('class').split(' '), function(index, className) {
+                if (className.indexOf(mod) >= 0) {
+                    $block.removeClass(className);
+                    return false;
+                }
+            });
+        });
+    };
+})(jQuery);
