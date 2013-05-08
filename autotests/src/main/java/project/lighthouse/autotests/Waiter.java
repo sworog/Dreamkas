8426588164545ac7080f5bd4dad95e345ac25ec7
Andrey <atolpeev@crystals.ru>
2013-05-08 18:27:24 +0400

    Изменен метод получения веб элемента.

--- autotests/src/main/java/project/lighthouse/autotests/Waiter.java
+++ autotests/src/main/java/project/lighthouse/autotests/Waiter.java
@@ -1,12 +1,10 @@
 package project.lighthouse.autotests;
 
-import com.google.common.base.Function;
-import net.thucydides.core.webdriver.WebdriverAssertionError;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Waiter {
 
@@ -17,34 +15,21 @@ public class Waiter {
     }
 
     public WebElement getFluentWaitWebElement(final By by) {
-        return new FluentWait<>(driver)
+        return new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(by));
+       /* return new FluentWait<>(driver)
                 // Waiting StaticDataCollections.TIMEOUT seconds for an element to be present on the page, checking
-//                // for its presence once every 1 seconds.
+                // for its presence once every 1 seconds.
                 .withTimeout(Integer.parseInt(StaticDataCollections.TIMEOUT), TimeUnit.MILLISECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS)
-                .ignoring(NoSuchElementException.class)
-                .ignoring(WebdriverAssertionError.class)
-                .ignoring(AssertionError.class)
-                .ignoring(StaleElementReferenceException.class)
-                .ignoring(ElementNotVisibleException.class)
+                .pollingEvery(100, TimeUnit.MILLISECONDS)
+                .ignoring(Throwable.class)
                 .until(new Function<WebDriver, WebElement>() {
                     public WebElement apply(WebDriver driver) {
                         return driver.findElement(by);
                     }
-                });
+                });*/
     }
 
     public WebElement getWebElement(By findBy) {
         return getFluentWaitWebElement(findBy);
-    }
-
-    public void checkElementIsNotVisible(final By findBy) {
-        new FluentWait<>(driver)
-                // Waiting 2 seconds for an element to be present on the page, checking
-//                // for its presence once every 1 seconds.
-                .withTimeout(2, TimeUnit.SECONDS)
-                .pollingEvery(1, TimeUnit.SECONDS)
-                .ignoring(NoSuchElementException.class)
-                .until(ExpectedConditions.invisibilityOfElementLocated(findBy));
     }
 }
