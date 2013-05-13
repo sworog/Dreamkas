7b4587c176399cf796c617d2492893cc75ee66b5
Andrew Borovin <andrew@borovin.com>
2013-05-13 18:10:19 +0400

    фикс автокомплита

--- webfront/web/blocks/invoice/invoice.js
+++ webfront/web/blocks/invoice/invoice.js
@@ -397,6 +397,65 @@ define(
                 block.$el.find('.invoice__dataInputControlsTr').remove();
 
                 block.set('dataEditing', false);
+            },
+            autocompleteToInput: function($input) {
+                var name = $input.attr('lh_product_autocomplete');
+                $input.autocomplete({
+                    source: function(request, response) {
+                        $.ajax({
+                            url: baseApiUrl + "/products/" + name + "/search.json",
+                            dataType: "json",
+                            data: {
+                                query: request.term
+                            },
+                            success: function(data) {
+                                response($.map(data, function(item) {
+                                    return {
+                                        label: item[name],
+                                        product: item
+                                    }
+                                }));
+                            }
+                        })
+                    },
+                    minLength: 3,
+                    select: function(event, ui) {
+                        $(this).parents("form").find("[lh_product_autocomplete='name']").val(ui.item.product.name);
+                        $(this).parents("form").find("[lh_product_autocomplete='sku']").val(ui.item.product.sku);
+                        $(this).parents("form").find("[lh_product_autocomplete='barcode']").val(ui.item.product.barcode);
+                        $(this).parents("form").find("[name='product']").val(ui.item.product.id);
+
+                        $(this).parents("form").find("[name='quantity']").focus();
+                    }
+                });
+                $input.keyup(function(event) {
+                    var keyCode = $.ui.keyCode;
+                    switch (event.keyCode) {
+                        case keyCode.PAGE_UP:
+                        case keyCode.PAGE_DOWN:
+                        case keyCode.UP:
+                        case keyCode.DOWN:
+                        case keyCode.ENTER:
+                        case keyCode.NUMPAD_ENTER:
+                        case keyCode.TAB:
+                        case keyCode.LEFT:
+                        case keyCode.RIGHT:
+                        case keyCode.ESCAPE:
+                            return;
+                            break;
+                        default:
+                            var term = $(this).autocomplete('getTerm');
+                            if (term != $(this).val()) {
+                                var inputs = ['name', 'sku', 'barcode'];
+                                for (var i in inputs) {
+                                    if (inputs[i] != name) {
+                                        $(this).parents("form").find("[lh_product_autocomplete='" + inputs[i] + "']").val('').trigger('input');
+                                    }
+                                }
+                                $(this).parents("form").find("[name='product']").val('');
+                            }
+                    }
+                });
             }
         });
     }
