3a5c5966ca1a76612edd771b59882740b6a88e0c
Andrew Borovin <andrew@borovin.com>
2013-05-25 01:30:23 +0400

    tables

--- webfront/web/kit/table/table.js
+++ webfront/web/kit/table/table.js
@@ -1,15 +1,17 @@
 define(
     [
-        '/kit/block.js'
+        '/kit/block.js',
+        './table.templates.js'
     ],
-    function(Block) {
+    function(Block, templates) {
         return Block.extend({
             loading: false,
+            columns: [],
+            data: [],
+            tagName: 'table',
             className: 'table',
+            templates: templates,
 
-            initialize: function(){
-                var block = this;
-            },
             'set:loading': function(loading){
                 var block = this;
                 block.find('thead').toggleClass('preloader_rows', loading);
---
+++ webfront/web/kit/table/table.templates.js
@@ -0,0 +1,7 @@
+define(function(require) {
+    return {
+        index: require('tpl!./templates/index.html'),
+        tr: require('tpl!./templates/tr.html'),
+        td: require('tpl!./templates/td.html')
+    }
+});
