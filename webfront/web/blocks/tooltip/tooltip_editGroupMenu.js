250f05d642629acc9b471831ce38e1a18cdfd225
Andrew Borovin <andrew@borovin.com>
2013-05-30 14:31:38 +0400

    удаление групп

--- webfront/web/blocks/tooltip/tooltip_editGroupMenu.js
+++ webfront/web/blocks/tooltip/tooltip_editGroupMenu.js
@@ -1,15 +1,35 @@
 define(
     [
-        './tooltip_editMenu.js'
+        './tooltip_editMenu.js',
+        '/models/catalogGroup.js'
     ],
-    function(tooltip_editMenu) {
+    function(tooltip_editMenu, CatalogGroupModel) {
         return tooltip_editMenu.extend({
+            groupId: null,
             events: {
                 'click .catalogEditTooltip__editLink': function(e){
                     e.preventDefault();
                     var block = this;
 
 
+                },
+                'click .tooltip__removeLink': function(e){
+                    e.preventDefault();
+                    var block = this,
+                        groups = block.classModel.get('groups'),
+                        groupModel = new CatalogGroupModel({
+                            id: block.groupId
+                        });
+
+                    groupModel.destroy({
+                        success: function(){
+                            block.classModel.set('groups', _.reject(groups, function(group){
+                                return group.id === block.groupId
+                            }));
+                        }
+                    });
+
+                    block.hide();
                 }
             }
         });
