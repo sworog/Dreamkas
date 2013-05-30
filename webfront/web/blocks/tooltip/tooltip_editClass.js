775843052871b3cd5593b0375fa0a55506868a96
Andrew Borovin <andrew@borovin.com>
2013-05-30 19:54:57 +0400

    редактирование классов и групп

--- webfront/web/blocks/tooltip/tooltip_editClass.js
+++ webfront/web/blocks/tooltip/tooltip_editClass.js
@@ -1,20 +1,42 @@
 define(
     [
         '/kit/tooltip/tooltip.js',
-        '/kit/form/form.js'
+        '/kit/form/form.js',
+        '/models/catalogClass.js',
+        'tpl!./templates/tooltip_editClass.html'
     ],
-    function(Tooltip, Form) {
+    function(Tooltip, Form, CatalogClassModel, tooltip_editClassTpl) {
         return Tooltip.extend({
             classModel: null,
+            addClass: 'tooltip_editClass',
+            templates: {
+                content: tooltip_editClassTpl
+            },
 
             initialize: function(){
                 var block = this;
 
+                block.classModel = block.classModel || new CatalogClassModel();
+
                 Tooltip.prototype.initialize.call(this);
 
                 block.form = new Form({
+                    el: block.el.getElementsByClassName('form'),
                     model: block.classModel
                 });
+
+                block.listenTo(block.form, {
+                    successSubmit: function(){
+                        block.hide();
+                    }
+                });
+            },
+            show: function(){
+                var block = this;
+
+                Tooltip.prototype.show.apply(this, arguments);
+
+                block.form.$el.find('[type="text"]').eq(0).focus();
             }
         });
     }
