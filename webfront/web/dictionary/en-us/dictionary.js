5cee0407d6635273086088278b96eadf128e5303
Andrew Borovin <andrew@borovin.com>
2013-06-19 16:50:24 +0400

    locale

--- webfront/web/dictionary/en-us/dictionary.js
+++ webfront/web/dictionary/en-us/dictionary.js
@@ -1,3 +1,11 @@
 define({
-    'продукты': 'Products'
+    'Продукты': 'Products',
+    'Пульт управления коммерческого директора': 'Remote management of commercial director',
+    'Каталог': 'Catalog',
+    'Накладные': 'Invoices',
+    'Остатки': 'Balance',
+    'Списания': 'Write offs',
+    'Алтарь администратора': 'Altar of administrator',
+    'Консоль товароведа': 'Console goods manager',
+    'Пользователи': 'Users'
 });
