758f413480cdae17adfcd977941f4ba4facda765
Mikhail Shamin <mshamin@crystals.ru>
2013-06-24 16:03:39 +0400

    Client create command

--- backend/src/Lighthouse/CoreBundle/Document/Auth/Client.php
+++ backend/src/Lighthouse/CoreBundle/Document/Auth/Client.php
@@ -4,6 +4,7 @@ namespace Lighthouse\CoreBundle\Document\Auth;
 
 use FOS\OAuthServerBundle\Document\Client as BaseClient;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use OAuth2\OAuth2;
 
 /**
  * @MongoDB\Document()
@@ -11,8 +12,35 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 class Client extends BaseClient
 {
     /**
-     * @MongoDB\Id
+     * @MongoDB\Id(
+     *      strategy="custom",
+     *      options={
+     *          "class" = "Lighthouse\CoreBundle\MongoDB\Id\SelectiveGenerator"
+     *      }
+     * )
      * @var string
      */
     protected $id;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->allowedGrantTypes = array(
+            OAuth2::GRANT_TYPE_USER_CREDENTIALS,
+            OAuth2::GRANT_TYPE_AUTH_CODE,
+            OAuth2::GRANT_TYPE_REFRESH_TOKEN,
+        );
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 }
