1fe79f234815d820439daf9af074ddce93d9594b
Andrey <atolpeev@crystals.ru>
2013-07-02 15:44:03 +0400

    Overrided ProductCreatePage class fieldInput method for new user roles replaces.

--- autotests/src/main/java/project/lighthouse/autotests/pages/administrator/users/UserCreatePage.java
+++ autotests/src/main/java/project/lighthouse/autotests/pages/administrator/users/UserCreatePage.java
@@ -1,11 +1,15 @@
 package project.lighthouse.autotests.pages.administrator.users;
 
 import net.thucydides.core.annotations.DefaultUrl;
+import org.jbehave.core.model.ExamplesTable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import project.lighthouse.autotests.elements.Input;
 import project.lighthouse.autotests.elements.Select;
 import project.lighthouse.autotests.pages.commercialManager.product.ProductCreatePage;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @DefaultUrl("/users/create")
 public class UserCreatePage extends ProductCreatePage {
@@ -13,6 +17,15 @@ public class UserCreatePage extends ProductCreatePage {
     public UserCreatePage(WebDriver driver) {
         super(driver);
     }
+
+    public static final HashMap<String, String> roles = new HashMap<String, String>() {
+        {
+            put("commercialManager", "ROLE_COMMERCIAL_MANAGER");
+            put("storeManager", "ROLE_STORE_MANAGER");
+            put("departmentManager", "ROLE_DEPARTMENT_MANAGER");
+            put("administrator", "ROLE_ADMINISTRATOR");
+        }
+    };
 
     @Override
     public void createElements() {
@@ -37,5 +50,22 @@ public class UserCreatePage extends ProductCreatePage {
     public void backToTheUsersListPageLink() {
         String link = "//*[@class='page__backLink']";
         findElement(By.xpath(link)).click();
+    }
+
+    public String replaceSelectedValue(String value) {
+        for (Map.Entry<String, String> role : roles.entrySet()) {
+            value = value.replaceAll(role.getKey(), role.getValue());
+        }
+        return value;
+    }
+
+    @Override
+    public void fieldInput(ExamplesTable fieldInputTable) {
+        for (Map<String, String> row : fieldInputTable.getRows()) {
+            String elementName = row.get("elementName");
+            String inputText = row.get("inputText");
+            String updatedText = replaceSelectedValue(inputText);
+            input(elementName, updatedText);
+        }
     }
 }
