605f38bf2e24074595a44bc26c34d9145e07fcc8
Andrew Borovin <andrew@borovin.com>
2013-07-03 16:05:49 +0400

    sale page fixes

--- webfront/web/pages/common/sale.js
+++ webfront/web/pages/common/sale.js
@@ -8,7 +8,14 @@ define(function(require) {
         templates: {
             '#content': require('tpl!./templates/sale.html')
         },
+        permissions: {
+            purchases: 'POST'
+        },
         initialize: function(){
+            var page = this;
+
+            page.render();
+
             new SaleBox({
                 el: document.getElementById('saleBox')
             });
