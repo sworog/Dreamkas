9944b8ba06cb79122181e607f463d228bab11bf0
Andrey <atolpeev@crystals.ru>
2013-07-04 15:35:05 +0400

    Refactored AcceptanceTestSuite class.

--- autotests/src/test/java/project/lighthouse/autotests/jbehave/AcceptanceTestSuite.java
+++ autotests/src/test/java/project/lighthouse/autotests/jbehave/AcceptanceTestSuite.java
@@ -7,17 +7,29 @@ import project.lighthouse.autotests.StaticDataCollections;
 public class AcceptanceTestSuite extends ThucydidesJUnitStories {
 
     private static final String CURRENT_BRANCH = "lighthouse.autotests.branch";
-    private static final String IMPLICITY_WAIT = "webdriver.timeouts.implicitlywait";
+    private static final String IMPLICITLY_WAIT = "webdriver.timeouts.implicitlywait";
     private static final String WEB_DRIVER_BASE_URL = "webdriver.base.url";
 
-    public AcceptanceTestSuite() {
+    EnvironmentVariables environmentVariables = getEnvironmentVariables();
 
-        EnvironmentVariables environmentVariables = getEnvironmentVariables();
-        String timeout = environmentVariables.getProperty(IMPLICITY_WAIT, null);
+    public AcceptanceTestSuite() {
+        setImplicitlyWaitTimeOut();
+        setWebDriverBaseUrl();
+        runTestSuite();
+    }
+
+    private void setImplicitlyWaitTimeOut() {
+        String timeout = environmentVariables.getProperty(IMPLICITLY_WAIT, null);
         if (timeout != null) {
             StaticDataCollections.TIMEOUT = timeout;
         }
+    }
+
+    private void setWebDriverBaseUrl() {
         StaticDataCollections.WEB_DRIVER_BASE_URL = environmentVariables.getProperty(WEB_DRIVER_BASE_URL, null);
+    }
+
+    private void runTestSuite() {
         String branch = environmentVariables.getProperty(CURRENT_BRANCH, null);
         if (branch != null) {
             if (branch.startsWith("us-")) {
