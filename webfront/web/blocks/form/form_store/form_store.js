277e311e53480cc17cb1bb76a5031c5b12abd756
Andrew Borovin <andrew@borovin.com>
2013-07-18 15:16:40 +0400

    store form redirect url

--- webfront/web/blocks/form/form_store/form_store.js
+++ webfront/web/blocks/form/form_store/form_store.js
@@ -4,13 +4,19 @@ define(function(require) {
 
         return Form.extend({
             blockName: 'form_store',
-            redirectUrl: function(){
-                var block = this;
-
-                return '/stores/' + block.model.id;
-            },
             templates: {
                 index: require('tpl!blocks/form/form_store/templates/index.html')
+            },
+            initialize: function(){
+                var block = this;
+
+                Form.prototype.initialize.apply(block, arguments);
+
+                block.redirectUrl = '/stores/';
+
+                if (block.model.id){
+                    block.redirectUrl += block.model.id;
+                }
             }
         });
     }
