d55d0a5c1dd565bdf6b084be188da31fe57b37d3
Andrew Borovin <andrew@borovin.com>
2013-07-24 12:48:00 +0400

    add jquery.require to libs

--- webfront/web/libs/libs.js
+++ webfront/web/libs/libs.js
@@ -2,6 +2,7 @@ define(function(require) {
     require('underscore');
     require('backbone');
     require('jquery');
+    require('jquery.require');
     require('jquery-ui');
     require('libs/lhAutocomplete');
     require('moment');
