8e3e9eda01428e501d80f8323c5a41b10cbc5473
Andrey <atolpeev@crystals.ru>
2013-07-24 12:29:49 +0400

    Added the new line for failure step message of test detail message output.

--- autotests/src/main/java/project/lighthouse/autotests/thucydides/TeamCityStepListener.java
+++ autotests/src/main/java/project/lighthouse/autotests/thucydides/TeamCityStepListener.java
@@ -118,7 +118,7 @@ public class TeamCityStepListener implements StepListener {
     }
 
     public String failureStepMessage(TestOutcome result) {
-        return String.format("Failing step: %s. ", getFailureStep(result));
+        return String.format("Failing step: %s.\r\n", getFailureStep(result));
     }
 
     public String getFailureStep(TestOutcome result) {
