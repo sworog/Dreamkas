7396be1efc8e55758ed0557f2645f0f4d794ebde
Fedot <vfedorov@crystals.ru>
2013-09-06 19:35:44 +0400

    Add empty test

---
+++ backend/src/Lighthouse/CoreBundle/Tests/Converter/ConvertToXmlForSetTen.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Tests\Converter;
+
+use Lighthouse\CoreBundle\Test\WebTestCase;
+
+class ConvertToXmlForSetTen extends WebTestCase
+{
+    public function testConvertProductsToXml()
+    {
+
+    }
+}
