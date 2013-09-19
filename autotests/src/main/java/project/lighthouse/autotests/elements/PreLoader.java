e1319d5294349d1f93a470f9c8e8884935450076
Andrey <atolpeev@crystals.ru>
2013-09-19 13:44:36 +0400

    Renamed PreLoader java file.

---
+++ autotests/src/main/java/project/lighthouse/autotests/elements/PreLoader.java
@@ -0,0 +1,21 @@
+package project.lighthouse.autotests.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import project.lighthouse.autotests.Waiter;
+
+public class PreLoader {
+
+    WebDriver driver;
+    Waiter waiter;
+    private static final String PRE_LOADER_XPATH = "//*[*[contains(@class, 'preloader_rows')] and *[not(contains(@class, 'preloader_spinner'))]]";
+
+    public PreLoader(WebDriver driver) {
+        this.driver = driver;
+        waiter = new Waiter(driver, 5000);
+    }
+
+    public void await() {
+        waiter.waitUntilIsNotVisible(By.xpath(PRE_LOADER_XPATH));
+    }
+}
