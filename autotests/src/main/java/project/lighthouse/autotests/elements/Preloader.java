93da95fdecc8a25c38101870ee0689981e135d50
Andrey <atolpeev@crystals.ru>
2013-09-19 13:39:46 +0400

    Added Preloader.java to the project.

---
+++ autotests/src/main/java/project/lighthouse/autotests/elements/Preloader.java
@@ -0,0 +1,11 @@
+package project.lighthouse.autotests.elements;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: atolpeev
+ * Date: 19.09.13
+ * Time: 12:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class Preloader {
+}
