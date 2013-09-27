cf581a6c76674f5e05c8c339d817150506f271ef
Mikhail Shamin <mshamin@crystals.ru>
2013-09-27 15:23:37 +0400

    deploy:remove for webfront

--- webfront/config/deploy.rb
+++ webfront/config/deploy.rb
@@ -34,3 +34,13 @@ after "deploy:restart" do
 end
 
 after "deploy:finalize_update", "webfront"
+
+namespace :deploy do
+    namespace :remove do
+        task :default, :roles => :app, :except => { :no_release => true } do
+            if Capistrano::CLI.ui.ask("Are you sure drop " + application_url.yellow + " (y/n)") == 'y'
+                host
+            end
+        end
+    end
+end
