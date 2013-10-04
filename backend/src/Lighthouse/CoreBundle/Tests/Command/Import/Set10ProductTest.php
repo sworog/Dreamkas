a5d0061373e580972a571eede579b822b03bd58c
Mikhail Shamin <mshamin@crystals.ru>
2013-10-04 16:38:01 +0400

    Fixed command test

--- backend/src/Lighthouse/CoreBundle/Tests/Command/Import/Set10ProductTest.php
+++ backend/src/Lighthouse/CoreBundle/Tests/Command/Import/Set10ProductTest.php
@@ -16,6 +16,7 @@ class Set10ProductTest extends ContainerAwareTestCase
 
         $input = array(
             'file' => __DIR__ . "/../../Fixtures/Integration/Set10/Import/goods.xml",
+            'batch-size' => 4
         );
 
         $exitCode = $commandTester->execute($input);
@@ -25,14 +26,8 @@ class Set10ProductTest extends ContainerAwareTestCase
         $display = $commandTester->getDisplay();
 
         $this->assertContains("Starting import", $display);
-
+        $this->assertContains('....', $display);
         $this->assertContains("Flushing", $display);
-
-        $this->assertContains('Persist product "Шар Чупа Чупс Смешарики Машина времени шокол.25г"', $display);
-        $this->assertContains('Persist product "Шарики Брюгген Чокин Крипс рисовые шок.250г"', $display);
-        $this->assertContains('Persist product "Шашлык из креветок пр-во Лэнд"', $display);
-        $this->assertContains('Persist product "Шашлык из курицы (филе) п/ф Кулинария"', $display);
-
         $this->assertContains("Done", $display);
 
 
@@ -44,12 +39,10 @@ class Set10ProductTest extends ContainerAwareTestCase
 
         $this->assertContains("Starting import", $display);
 
+        $this->assertContains('EEEE', $display);
         $this->assertContains('sku: Такой артикул уже есть', $display);
 
-        $this->assertNotContains('Persist product "Шар Чупа Чупс Смешарики Машина времени шокол.25г"', $display);
-        $this->assertNotContains('Persist product "Шарики Брюгген Чокин Крипс рисовые шок.250г"', $display);
-        $this->assertNotContains('Persist product "Шашлык из креветок пр-во Лэнд"', $display);
-        $this->assertNotContains('Persist product "Шашлык из курицы (филе) п/ф Кулинария"', $display);
+        $this->assertNotContains('....', $display);
 
         $this->assertContains("Done", $display);
     }
