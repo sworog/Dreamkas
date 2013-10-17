629c5995acc3c886ffa63d232e7a73e3460202aa
Mikhail Shamin <mshamin@crystals.ru>
2013-10-17 18:05:28 +0400

    Added dot to run tests sh script

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-app/console cache:clear --no-warmup --env=test
-app/console cache:warmup --env=test
-bin/phpunit -c app/ $@
+./app/console cache:clear --no-warmup --env=test
+./app/console cache:warmup --env=test
+./bin/phpunit -c app/ $@
