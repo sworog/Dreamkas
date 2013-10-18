64548189abc5c57fdd2d9441920e79b5a88ab3ec
Andrey <atolpeev@crystals.ru>
2013-10-18 18:45:26 +0400

    Added implementation for compareWithExampleTable(ExamplesTable examplesTable) method.

--- autotests/src/main/java/project/lighthouse/autotests/objects/productInvoiceListObjectsList.java
+++ autotests/src/main/java/project/lighthouse/autotests/objects/productInvoiceListObjectsList.java
@@ -1,12 +1,32 @@
 package project.lighthouse.autotests.objects;
 
+import junit.framework.Assert;
 import org.jbehave.core.model.ExamplesTable;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ProductInvoiceListObjectsList extends ArrayList<ProductInvoiceListObject> {
 
     public void compareWithExampleTable(ExamplesTable examplesTable) {
-        //TODO make method
+
+        List<Map<String, String>> notFoundRows = new ArrayList<>();
+        for (Map<String, String> row : examplesTable.getRows()) {
+            Boolean found = false;
+            for (ProductInvoiceListObject productInvoiceListObject : this) {
+                if (productInvoiceListObject.rowIsEqual(row)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                notFoundRows.add(row);
+            }
+        }
+        if (notFoundRows.size() > 0) {
+            String errorMessage = String.format("These rows are nt found: %s", notFoundRows.toArray());
+            Assert.fail(errorMessage);
+        }
     }
 }
