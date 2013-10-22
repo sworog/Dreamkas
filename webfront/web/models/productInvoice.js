d8419bcf21a49829ed5d193818691eae68986e24
Andrew Borovin <andrew@borovin.com>
2013-10-22 12:54:34 +0400

    redesign product invoices page

--- webfront/web/models/productInvoice.js
+++ webfront/web/models/productInvoice.js
@@ -1,12 +1,20 @@
 define(function(require) {
     //requirements
     var Model = require('kit/core/model'),
+        computeAttr = require('kit/utils/computeAttr'),
         currentUserModel = require('models/currentUser');
+
+    require('moment');
 
     return Model.extend({
         modelName: 'productInvoice',
         urlRoot: function() {
             return LH.baseApiUrl + '/stores/' + currentUserModel.stores.at(0).id + '/products/' + this.get('product').id + '/invoiceProducts';
+        },
+        defaults: {
+            acceptanceDateFormatted: computeAttr(['acceptanceDate'], function(acceptanceDate){
+                return moment(acceptanceDate).format('DD.MM.YYYY');
+            })
         }
     });
 });
