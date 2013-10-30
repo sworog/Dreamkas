ede0d84e958d34e7bb7096d4206aea98033a7b8b
Fedot <vfedorov@crystals.ru>
2013-10-30 16:55:00 +0400

    Add filter for store writeoff

---
+++ backend/src/Lighthouse/CoreBundle/Document/WriteOff/WriteOffsFilter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Document\WriteOff;
+
+use Lighthouse\CoreBundle\Request\ParamConverter\Filter\FilterInterface;
+
+class WriteOffsFilter implements FilterInterface
+{
+    /**
+     * @var string
+     */
+    protected $number;
+
+    /**
+     * @return string
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param $number
+     */
+    public function setNumber($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasNumber()
+    {
+        return null !== $this->number;
+    }
+
+    /**
+     * @param array $data
+     * @return null
+     */
+    public function populate(array $data)
+    {
+        if (isset($data['number'])) {
+            $this->number = $data['number'];
+        }
+    }
+
+}
