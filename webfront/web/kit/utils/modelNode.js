c80c8622a161cd1d085449b77f6a9f8fdfaa09e3
Andrew Borovin <andrew@borovin.com>
2013-10-30 19:54:14 +0400

    fix modelNode

--- webfront/web/kit/utils/modelNode.js
+++ webfront/web/kit/utils/modelNode.js
@@ -1,18 +1,25 @@
 define(function(require) {
     //requirements
-    var translate = require('./translate'),
+    var getText = require('./getText'),
         get = require('./get');
 
     require('jquery');
 
-    return function(model, attr){
-        var attrValue = model.get(attr),
-            text = translate(get(model, 'dictionary'), typeof attrValue === 'undefined' ? '' : attrValue),
+    function ensureAttr(model, attr) {
+        var attrValue = model.get(attr);
+
+        return getText(get(model, 'dictionary'), typeof attrValue === 'undefined' ? '' : attrValue);
+    }
+
+    return function(model, attr) {
+        var text = ensureAttr(model, attr),
             nodeTemplate = '<span model_name="' + model.modelName + '" model_id="' + model.id + '" model_attr="' + attr + '">' + text + '</span>';
 
         var handlers = {};
 
-        handlers['change:' + attr] = function(){
+        handlers['change:' + attr] = function() {
+            var text = ensureAttr(model, attr);
+
             $('body')
                 .find('[model_id="' + model.id + '"][model_attr="' + attr + '"]')
                 .html(text);
