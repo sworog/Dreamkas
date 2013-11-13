c4f7a4146005967a783cc8ef94ffd7b6eb524898
Andrew Borovin <andrew@borovin.com>
2013-11-13 13:47:45 +0400

    add new store model attrs

--- webfront/web/models/storeProduct.js
+++ webfront/web/models/storeProduct.js
@@ -17,31 +17,56 @@ define(function(require) {
             return LH.baseApiUrl + '/products'
         },
         defaults: {
-            amount: 0,
+            inventory: 0,
+
+            inventoryDaysInt: computeAttr(['inventoryDays'], function(inventoryDays){
+                return inventoryDays ? String.prototype.split.call(inventoryDays, '.')[0] : '';
+            }),
+
+            inventoryDaysFloat: computeAttr(['inventoryDays'], function(inventoryDays){
+                return inventoryDays ? String.prototype.split.call(inventoryDays, '.')[1] : '';
+            }),
+
+            averageDailySalesInt: computeAttr(['averageDailySales'], function(averageDailySales){
+                return averageDailySales ? String.prototype.split.call(averageDailySales, '.')[0] : '';
+            }),
+
+            averageDailySalesFloat: computeAttr(['averageDailySales'], function(averageDailySales){
+                return averageDailySales ? String.prototype.split.call(averageDailySales, '.')[1] : '';
+            }),
+
             unitsFormatted: computeAttr(['product.units'], function(){
                 return LH.units(this.get('product.units'), 'smallShort');
             }),
+
             retailPricePreference: 'retailMarkup',
+
             averagePurchasePriceFormatted: computeAttr(['averagePurchasePrice'], function(averagePurchasePrice){
                 return averagePurchasePrice ? (LH.formatPrice(averagePurchasePrice) + ' р.') : '&mdash;';
             }),
+
             averagePurchasePriceInt: computeAttr(['averagePurchasePrice'], function(averagePurchasePrice){
                 return averagePurchasePrice ? String.prototype.split.call(averagePurchasePrice, '.')[0] : '';
             }),
+
             averagePurchasePriceFloat: computeAttr(['averagePurchasePrice'], function(averagePurchasePrice){
                 return averagePurchasePrice ? String.prototype.split.call(averagePurchasePrice, '.')[1] : '';
             }),
+
             purchasePriceFormatted: computeAttr(['product.purchasePrice'], function(purchasePrice){
                 return purchasePrice ? (LH.formatPrice(purchasePrice) + ' р.') : '&mdash;';
             }),
+
             lastPurchasePriceFormatted: computeAttr(['lastPurchasePrice'], function(lastPurchasePrice){
                 var purchasePriceFormatted = this.get('product.purchasePrice') ? (LH.formatPrice(this.get('product.purchasePrice')) + ' р.') : '&mdash;';
                 return lastPurchasePrice ? (LH.formatPrice(lastPurchasePrice) + ' р.') : purchasePriceFormatted;
             }),
+
             lastPurchasePriceInt: computeAttr(['lastPurchasePrice'], function(lastPurchasePrice){
                 lastPurchasePrice = lastPurchasePrice || this.get('product.purchasePrice');
                 return lastPurchasePrice ? String.prototype.split.call(lastPurchasePrice, '.')[0] : '';
             }),
+
             lastPurchasePriceFloat: computeAttr(['lastPurchasePrice'], function(lastPurchasePrice){
                 lastPurchasePrice = lastPurchasePrice || this.get('product.purchasePrice');
                 return lastPurchasePrice ? String.prototype.split.call(lastPurchasePrice, '.')[1] : '';
