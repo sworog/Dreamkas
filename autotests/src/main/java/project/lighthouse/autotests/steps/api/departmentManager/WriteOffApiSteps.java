a44eccb20e9859ed4ea3c96ea6fe81b9b74dcbf2
Andrey <atolpeev@crystals.ru>
2013-11-18 12:52:32 +0400

    Moved all writeOffApiSteps logic to high-level class.

--- autotests/src/main/java/project/lighthouse/autotests/steps/api/departmentManager/WriteOffApiSteps.java
+++ autotests/src/main/java/project/lighthouse/autotests/steps/api/departmentManager/WriteOffApiSteps.java
@@ -1,103 +1,29 @@
 package project.lighthouse.autotests.steps.api.departmentManager;
 
-import junit.framework.Assert;
 import net.thucydides.core.annotations.Step;
-import org.jbehave.core.model.ExamplesTable;
 import org.json.JSONException;
 import project.lighthouse.autotests.ApiConnect;
 import project.lighthouse.autotests.StaticData;
-import project.lighthouse.autotests.elements.DateTime;
-import project.lighthouse.autotests.objects.api.Store;
-import project.lighthouse.autotests.objects.api.User;
 import project.lighthouse.autotests.objects.api.WriteOff;
-import project.lighthouse.autotests.steps.api.administrator.UserApiSteps;
-import project.lighthouse.autotests.steps.api.commercialManager.CatalogApiSteps;
-import project.lighthouse.autotests.steps.api.commercialManager.StoreApiSteps;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class WriteOffApiSteps extends DepartmentManagerApi {
-
-    StoreApiSteps storeApiSteps = new StoreApiSteps();
-    UserApiSteps userApiSteps = new UserApiSteps();
-    CatalogApiSteps catalogApiSteps = new CatalogApiSteps();
 
     public WriteOffApiSteps() throws JSONException {
     }
 
     @Step
-    public WriteOff createWriteOffThroughPost(String writeOffNumber) throws IOException, JSONException {
-        Store store = storeApiSteps.createStoreThroughPost();
-        User user = userApiSteps.getUser(DEFAULT_USER_NAME);
-        catalogApiSteps.promoteDepartmentManager(store, user.getUserName());
-        return createWriteOffThroughPost(writeOffNumber, store.getNumber(), user.getUserName());
-    }
-
-    @Step
-    public WriteOff createWriteOffThroughPost(String writeOffNumber, String storeName, String userName) throws JSONException, IOException {
-        WriteOff writeOff = new WriteOff(writeOffNumber, DateTime.getTodayDate(DateTime.DATE_PATTERN));
+    public WriteOff createWriteOffThroughPost(String writeOffNumber, String date, String storeName, String userName) throws JSONException, IOException {
+        WriteOff writeOff = new WriteOff(writeOffNumber, date);
         String storeId = StaticData.stores.get(storeName).getId();
         writeOff.setStoreId(storeId);
         return new ApiConnect(userName, "lighthouse").createWriteOffThroughPost(writeOff);
-    }
-
-    @Step
-    public WriteOff createWriteOffThrougPost(String storeName, String userName, ExamplesTable examplesTable) throws JSONException, IOException {
-        String number = "", date = "";
-        for (Map<String, String> row : examplesTable.getRows()) {
-            String elementName = row.get("elementName");
-            String elementValue = row.get("elementValue");
-            switch (elementName) {
-                case "number":
-                    number = elementValue;
-                    break;
-                case "date":
-                    date = elementValue;
-                    break;
-                default:
-                    Assert.fail(String.format("No such elementName '%s'", elementName));
-                    break;
-            }
-        }
-        WriteOff writeOff = new WriteOff(number, date);
-        String storeId = StaticData.stores.get(storeName).getId();
-        writeOff.setStoreId(storeId);
-        return new ApiConnect(userName, "lighthouse").createWriteOffThroughPost(writeOff);
-    }
-
-    @Step
-    public WriteOff createWriteOffThroughPost(String writeOffNumber, String productSku, String quantity, String price, String cause)
-            throws IOException, JSONException {
-        WriteOff writeOff = createWriteOffThroughPost(writeOffNumber);
-        addProductToWriteOff(writeOffNumber, productSku, quantity, price, cause, DEFAULT_USER_NAME);
-        return writeOff;
-    }
-
-    //TODO given method
-    public WriteOff createWriteOffThroughPost(String writeOffNumber, String productSku, String quantity, String price, String cause, String storeName, String userName) throws IOException, JSONException {
-        WriteOff writeOff = createWriteOffThroughPost(writeOffNumber, storeName, userName);
-        addProductToWriteOff(writeOffNumber, productSku, quantity, price, cause, userName);
-        return writeOff;
     }
 
     @Step
     public void addProductToWriteOff(String writeOffNumber, String productSku, String quantity, String price, String cause, String userName) throws JSONException, IOException {
         new ApiConnect(userName, "lighthouse").addProductToWriteOff(writeOffNumber, productSku, quantity, price, cause);
-    }
-
-    @Step
-    public void createWriteOffAndNavigateToIt(String writeOffNumber, String productSku, String quantity, String price, String cause)
-            throws JSONException, IOException {
-        createWriteOffThroughPost(writeOffNumber, productSku, quantity, price, cause);
-        navigateToWriteOffPage(writeOffNumber);
-    }
-
-    @Step
-    public void createWriteOffAndNavigateToIt(String writeOffNumber)
-            throws JSONException, IOException {
-        createWriteOffThroughPost(writeOffNumber);
-        navigateToWriteOffPage(writeOffNumber);
     }
 
     @Step
