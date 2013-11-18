cd673d8da4e1241287475187bb31362750ebbc8c
Andrey <atolpeev@crystals.ru>
2013-11-18 12:26:03 +0400

    Moved all invoiceApiSteps logic to high-level class.

--- autotests/src/main/java/project/lighthouse/autotests/steps/api/departmentManager/InvoiceApiSteps.java
+++ autotests/src/main/java/project/lighthouse/autotests/steps/api/departmentManager/InvoiceApiSteps.java
@@ -1,109 +1,24 @@
 package project.lighthouse.autotests.steps.api.departmentManager;
 
-import junit.framework.Assert;
 import net.thucydides.core.annotations.Step;
-import org.jbehave.core.model.ExamplesTable;
 import org.json.JSONException;
 import project.lighthouse.autotests.ApiConnect;
 import project.lighthouse.autotests.StaticData;
-import project.lighthouse.autotests.elements.DateTime;
 import project.lighthouse.autotests.objects.api.Invoice;
-import project.lighthouse.autotests.objects.api.Store;
-import project.lighthouse.autotests.objects.api.User;
-import project.lighthouse.autotests.steps.api.administrator.UserApiSteps;
-import project.lighthouse.autotests.steps.api.commercialManager.CatalogApiSteps;
-import project.lighthouse.autotests.steps.api.commercialManager.StoreApiSteps;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class InvoiceApiSteps extends DepartmentManagerApi {
-
-    StoreApiSteps storeApiSteps = new StoreApiSteps();
-    UserApiSteps userApiSteps = new UserApiSteps();
-    CatalogApiSteps catalogApiSteps = new CatalogApiSteps();
 
     public InvoiceApiSteps() throws JSONException {
     }
 
     @Step
-    public Invoice createInvoiceThroughPost(String invoiceName) throws JSONException, IOException {
-        Store store = storeApiSteps.createStoreThroughPost();
-        User user = userApiSteps.getUser(DEFAULT_USER_NAME);
-        catalogApiSteps.promoteDepartmentManager(store, user.getUserName());
-        return createInvoiceThroughPost(invoiceName, store.getNumber(), user.getUserName());
-    }
-
-    @Step
-    public Invoice createInvoiceThroughPost(String invoiceName, String storeName, String userName) throws JSONException, IOException {
-        return createInvoiceThroughPost(invoiceName, DateTime.getTodayDate(DateTime.DATE_TIME_PATTERN), storeName, userName);
-    }
-
-    @Step
-    public Invoice createInvoiceThroughPost(String invoiceName, String date, String storeName, String userName) throws JSONException, IOException {
-        Invoice invoice = new Invoice(invoiceName, "supplier", date, "accepter", "legalEntity", "", "");
+    public Invoice createInvoiceThroughPost(String invoiceName, String date, String supplier, String accepter, String legalEntity, String supplierInvoiceSku, String supplierInvoiceDate, String storeName, String userName) throws JSONException, IOException {
+        Invoice invoice = new Invoice(invoiceName, supplier, date, accepter, legalEntity, supplierInvoiceSku, supplierInvoiceDate);
         String storeId = StaticData.stores.get(storeName).getId();
         invoice.setStoreId(storeId);
         return new ApiConnect(userName, "lighthouse").createInvoiceThroughPost(invoice);
-    }
-
-    @Step
-    public Invoice createInvoiceThroughPost(String storeName, String userName, ExamplesTable examplesTable) throws JSONException, IOException {
-        String sku = "", acceptanceDate = "", supplier = "", accepter = "", legalEntity = "", supplierInvoiceSku = "", supplierInvoiceDate = "";
-        for (Map<String, String> row : examplesTable.getRows()) {
-            String elementName = row.get("elementName");
-            String elementValue = row.get("elementValue");
-            switch (elementName) {
-                case "sku":
-                    sku = elementValue;
-                    break;
-                case "acceptanceDate":
-                    acceptanceDate = elementValue;
-                    break;
-                case "supplier":
-                    supplier = elementValue;
-                    break;
-                case "accepter":
-                    accepter = elementValue;
-                    break;
-                case "legalEntity":
-                    legalEntity = elementValue;
-                    break;
-                case "supplierInvoiceSku":
-                    supplierInvoiceSku = elementValue;
-                    break;
-                case "supplierInvoiceDate":
-                    supplierInvoiceDate = elementValue;
-                    break;
-                default:
-                    Assert.fail(String.format("No such elementName '%s'", elementName));
-                    break;
-            }
-        }
-        Invoice invoice = new Invoice(sku, supplier, acceptanceDate, accepter, legalEntity, supplierInvoiceSku, supplierInvoiceDate);
-        String storeId = StaticData.stores.get(storeName).getId();
-        invoice.setStoreId(storeId);
-        return new ApiConnect(userName, "lighthouse").createInvoiceThroughPost(invoice);
-    }
-
-    @Step
-    public void createInvoiceThroughPostAndNavigateToIt(String invoiceName) throws JSONException, IOException {
-        createInvoiceThroughPost(invoiceName);
-        navigateToTheInvoicePage(invoiceName);
-    }
-
-    @Step
-    public void createInvoiceThroughPostWithProductAndNavigateToIt(String invoiceName, String productSku) throws IOException, JSONException {
-        createInvoiceThroughPost(invoiceName);
-        addProductToInvoice(invoiceName, productSku, "1", "1", DEFAULT_USER_NAME);
-        navigateToTheInvoicePage(invoiceName);
-    }
-
-    //TODO make given method->>>
-    public void createInvoiceThroughPostWithProductAndNavigateToIt(String invoiceName, String productSku, String storeName, String userName) throws IOException, JSONException {
-        createInvoiceThroughPost(invoiceName, storeName, userName);
-        addProductToInvoice(invoiceName, productSku, "1", "1", userName);
-        navigateToTheInvoicePage(invoiceName);
     }
 
     @Step
--- autotests/src/test/java/project/lighthouse/autotests/jbehave/api/EndInvoiceApiSteps.java
+++ autotests/src/test/java/project/lighthouse/autotests/jbehave/api/EndInvoiceApiSteps.java
@@ -4,12 +4,20 @@ import net.thucydides.core.annotations.Steps;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.model.ExamplesTable;
 import org.json.JSONException;
+import org.junit.Assert;
+import project.lighthouse.autotests.elements.DateTime;
 import project.lighthouse.autotests.helper.DateTimeHelper;
+import project.lighthouse.autotests.objects.api.Store;
 import project.lighthouse.autotests.objects.api.SubCategory;
+import project.lighthouse.autotests.objects.api.User;
+import project.lighthouse.autotests.steps.api.administrator.UserApiSteps;
+import project.lighthouse.autotests.steps.api.commercialManager.CatalogApiSteps;
 import project.lighthouse.autotests.steps.api.commercialManager.ProductApiSteps;
+import project.lighthouse.autotests.steps.api.commercialManager.StoreApiSteps;
 import project.lighthouse.autotests.steps.api.departmentManager.InvoiceApiSteps;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class EndInvoiceApiSteps {
 
@@ -21,31 +29,78 @@ public class EndInvoiceApiSteps {
     @Steps
     ProductApiSteps productApiSteps;
 
+    @Steps
+    StoreApiSteps storeApiSteps;
+
+    @Steps
+    UserApiSteps userApiSteps;
+
+    @Steps
+    CatalogApiSteps catalogApiSteps;
+
     @Given("there is the invoice '$invoiceSku' with product '$productName' name, '$productSku' sku, '$productBarCode' barcode, '$productUnits' units")
     public void givenThereIsInvoiceWithProduct(String invoiceSku, String productName, String productSku, String productBarCode, String productUnits) throws JSONException, IOException {
         productApiSteps.createProductThroughPost(productName, productSku, productBarCode, productUnits, "123", SubCategory.DEFAULT_NAME);
-        invoiceApiSteps.createInvoiceThroughPostWithProductAndNavigateToIt(invoiceSku, productSku);
+        givenThereIsTheInvoiceWithSku(invoiceSku);
+        invoiceApiSteps.addProductToInvoice(invoiceSku, productSku, "1", "1", "departmentManager");
+        invoiceApiSteps.navigateToTheInvoicePage(invoiceSku);
     }
 
     @Given("there is the invoice with '$sku' sku")
     public void givenThereIsTheInvoiceWithSku(String sku) throws JSONException, IOException {
-        invoiceApiSteps.createInvoiceThroughPost(sku);
+        Store store = storeApiSteps.createStoreThroughPost();
+        User user = userApiSteps.getUser("departmentManager");
+        catalogApiSteps.promoteDepartmentManager(store, user.getUserName());
+        givenThereIsTheInvoiceInTheStore(sku, store.getNumber(), user.getUserName());
     }
 
     @Given("there is the invoice with sku '$sku' in the store with number '$number' ruled by department manager with name '$userName'")
     public void givenThereIsTheInvoiceInTheStore(String sku, String number, String userName) throws IOException, JSONException {
-        invoiceApiSteps.createInvoiceThroughPost(sku, number, userName);
+        invoiceApiSteps.createInvoiceThroughPost(sku, DateTime.getTodayDate(DateTime.DATE_TIME_PATTERN), "supplier", "accepter", "legalEntity", "", "", number, userName);
     }
 
     @Given("there is the date invoice with sku '$sku' and date '$date' in the store with number '$number' ruled by department manager with name '$userName'")
     public void givenThereIsTheInvoiceInTheStore(String sku, String date, String number, String userName) throws IOException, JSONException {
-        invoiceApiSteps.createInvoiceThroughPost(sku, new DateTimeHelper(date).convertDateTime(), number, userName);
+        invoiceApiSteps.createInvoiceThroughPost(sku, new DateTimeHelper(date).convertDateTime(), "supplier", "accepter", "legalEntity", "", "", number, userName);
     }
 
     @Given("there is the invoice in the store with number '$number' ruled by department manager with name '$userName' with values $exampleTable")
     public void givenThereIsTheInvoiceInTheStoreWithValues(String number, String userName, ExamplesTable examplesTable) throws IOException, JSONException {
-        invoiceApiSteps.createInvoiceThroughPost(number, userName, examplesTable);
-        this.examplesTable = examplesTable;
+        String sku = "", acceptanceDate = "", supplier = "", accepter = "", legalEntity = "", supplierInvoiceSku = "", supplierInvoiceDate = "";
+        for (Map<String, String> row : examplesTable.getRows()) {
+            String elementName = row.get("elementName");
+            String elementValue = row.get("elementValue");
+            switch (elementName) {
+                case "sku":
+                    sku = elementValue;
+                    break;
+                case "acceptanceDate":
+                    acceptanceDate = elementValue;
+                    break;
+                case "supplier":
+                    supplier = elementValue;
+                    break;
+                case "accepter":
+                    accepter = elementValue;
+                    break;
+                case "legalEntity":
+                    legalEntity = elementValue;
+                    break;
+                case "supplierInvoiceSku":
+                    supplierInvoiceSku = elementValue;
+                    break;
+                case "supplierInvoiceDate":
+                    supplierInvoiceDate = elementValue;
+                    break;
+                default:
+                    Assert.fail(String.format("No such elementName '%s'", elementName));
+                    break;
+            }
+        }
+        invoiceApiSteps.createInvoiceThroughPost(sku, acceptanceDate, supplier, accepter, legalEntity, supplierInvoiceSku, supplierInvoiceDate, number, userName);
+        EndInvoiceApiSteps.examplesTable = examplesTable;
+
+
     }
 
     @Given("the user navigates to the invoice page with name '$invoiceName'")
