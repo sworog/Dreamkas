947d324e3247704766e1dffea63ed4be6c6fb9a4
Fedot <vfedorov@crystals.ru>
2013-11-20 19:32:02 +0400

    Add test

--- backend/src/Lighthouse/CoreBundle/Tests/Controller/InvoiceProductControllerTest.php
+++ backend/src/Lighthouse/CoreBundle/Tests/Controller/InvoiceProductControllerTest.php
@@ -1578,4 +1578,87 @@ class InvoiceProductControllerTest extends WebTestCase
         Assert::assertJsonPathEquals(3.34, "*.amountVAT", $getResponse);
         Assert::assertJsonPathEquals(99.99, "*.quantity", $getResponse);
     }
+
+    public function testInvoiceProductVATFieldChangeIncludesVATInInvoice()
+    {
+        $productId1 = $this->createProduct(array('name' => 'Кефир 1%', 'sku' => 'кефир_1%', 'purchasePrice' => 35.24));
+
+        $invoiceId1 = $this->createInvoice(
+            array(
+                'sku' => 'MU-866',
+                'acceptanceDate' => '2013-10-18T09:39:47+0400',
+                'includesVAT' => true,
+            ),
+            $this->storeId,
+            $this->departmentManager
+        );
+
+        $this->createInvoiceProduct($invoiceId1, $productId1, 99.99, 36.78);
+
+        $accessToken = $this->factory->auth($this->departmentManager);
+        $getResponse = $this->clientJsonRequest(
+            $accessToken,
+            'GET',
+            '/api/1/stores/' . $this->storeId . '/products/' . $productId1 . '/invoiceProducts'
+        );
+
+        $this->assertResponseCode(200);
+        Assert::assertJsonPathCount(1, '0.id', $getResponse);
+        Assert::assertJsonPathEquals(3677.63, "0.totalPrice", $getResponse);
+        Assert::assertJsonPathEquals(3343.66, "0.totalPriceWithoutVAT", $getResponse);
+        Assert::assertJsonPathEquals(333.97, "0.totalAmountVAT", $getResponse);
+        Assert::assertJsonPathEquals(36.78, "0.price", $getResponse);
+        Assert::assertJsonPathEquals(33.44, "0.priceWithoutVAT", $getResponse);
+        Assert::assertJsonPathEquals(3.34, "0.amountVAT", $getResponse);
+        Assert::assertJsonPathEquals(99.99, "0.quantity", $getResponse);
+
+
+        $invoiceDataModify = $this->clientJsonRequest(
+            $accessToken,
+            'GET',
+            '/api/1/stores/' . $this->storeId . '/invoices/' . $invoiceId1
+        );
+        $this->assertResponseCode(200);
+
+        $invoiceDataModify['includesVAT'] = false;
+        unset(
+            $invoiceDataModify['id'],
+            $invoiceDataModify['store'],
+            $invoiceDataModify['createdDate'],
+            $invoiceDataModify['sumTotal'],
+            $invoiceDataModify['sumTotalWithoutVAT'],
+            $invoiceDataModify['totalAmountVAT'],
+            $invoiceDataModify['itemsCount'],
+            $invoiceDataModify['products']
+        );
+
+        $invoiceResponse = $this->clientJsonRequest(
+            $accessToken,
+            'PUT',
+            '/api/1/stores/' . $this->storeId . '/invoices/' . $invoiceId1,
+            $invoiceDataModify
+        );
+        $this->assertResponseCode(200);
+
+        Assert::assertJsonPathEquals(false, 'includesVAT', $invoiceResponse);
+        Assert::assertJsonPathEquals(3677.63, 'sumTotal', $invoiceResponse);
+        Assert::assertJsonPathEquals(3343.66, 'sumTotalWithoutVAT', $invoiceResponse);
+        Assert::assertJsonPathEquals(333.97, 'totalAmountVAT', $invoiceResponse);
+
+        $getResponse = $this->clientJsonRequest(
+            $accessToken,
+            'GET',
+            '/api/1/stores/' . $this->storeId . '/products/' . $productId1 . '/invoiceProducts'
+        );
+
+        $this->assertResponseCode(200);
+        Assert::assertJsonPathCount(1, '0.id', $getResponse);
+        Assert::assertJsonPathEquals(3677.63, "0.totalPrice", $getResponse);
+        Assert::assertJsonPathEquals(3343.66, "0.totalPriceWithoutVAT", $getResponse);
+        Assert::assertJsonPathEquals(333.97, "0.totalAmountVAT", $getResponse);
+        Assert::assertJsonPathEquals(36.78, "0.price", $getResponse);
+        Assert::assertJsonPathEquals(33.44, "0.priceWithoutVAT", $getResponse);
+        Assert::assertJsonPathEquals(3.34, "0.amountVAT", $getResponse);
+        Assert::assertJsonPathEquals(99.99, "0.quantity", $getResponse);
+    }
 }
