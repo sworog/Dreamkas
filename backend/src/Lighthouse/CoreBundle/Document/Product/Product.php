25c7785abadb1a023334743297fedb43c21cb8dd
Mikhail Shamin <mshamin@crystals.ru>
2013-11-21 13:05:56 +0400

    Use getClassName() instead of text class name

--- backend/src/Lighthouse/CoreBundle/Document/Product/Product.php
+++ backend/src/Lighthouse/CoreBundle/Document/Product/Product.php
@@ -6,6 +6,7 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use JMS\Serializer\Annotation as Serializer;
 use Lighthouse\CoreBundle\Document\AbstractDocument;
 use Lighthouse\CoreBundle\Document\Classifier\SubCategory\SubCategory;
+use Lighthouse\CoreBundle\Document\Product\Version\ProductVersion;
 use Lighthouse\CoreBundle\Rounding\AbstractRounding;
 use Lighthouse\CoreBundle\Types\Numeric\Money;
 use Lighthouse\CoreBundle\Versionable\VersionableInterface;
@@ -195,6 +196,6 @@ class Product extends AbstractDocument implements VersionableInterface
      */
     public function getVersionClass()
     {
-        return 'Lighthouse\\CoreBundle\\Document\\Product\\Version\\ProductVersion';
+        return ProductVersion::getClassName();
     }
 }
