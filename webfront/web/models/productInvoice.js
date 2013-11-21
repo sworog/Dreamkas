ff331d9ce0643f979a79a04b4ed7245cda5e096b
Andrew Borovin <andrew@borovin.com>
2013-11-21 15:35:56 +0400

    fix product quantity delimeter

--- webfront/web/models/productInvoice.js
+++ webfront/web/models/productInvoice.js
@@ -1,7 +1,7 @@
 define(function(require) {
     //requirements
     var Model = require('kit/core/model'),
-        computeAttr = require('kit/utils/computeAttr'),
+        compute = require('kit/utils/computeAttr'),
         currentUserModel = require('models/currentUser');
 
     require('moment');
@@ -12,14 +12,17 @@ define(function(require) {
             return LH.baseApiUrl + '/stores/' + currentUserModel.stores.at(0).id + '/products/' + this.get('product').id + '/invoiceProducts';
         },
         defaults: {
-            acceptanceDateFormatted: computeAttr(['acceptanceDate'], function(acceptanceDate){
+            acceptanceDateFormatted: compute(['acceptanceDate'], function(acceptanceDate){
                 return moment(acceptanceDate).format('DD.MM.YYYY');
             }),
-            totalPriceFormatted: computeAttr(['totalPrice'], function(totalPrice){
+            totalPriceFormatted: compute(['totalPrice'], function(totalPrice){
                 return LH.formatPrice(totalPrice);
             }),
-            priceFormatted: computeAttr(['price'], function(price){
+            priceFormatted: compute(['price'], function(price){
                 return LH.formatPrice(price);
+            }),
+            quantityElement: compute(['quantity'], function(quantity){
+                return String.prototype.split.call(quantity, '.')[0] + '<span class="layout__floatPart">,' + (String.prototype.split.call(quantity, '.')[1] || '00') + '</span>'
             })
         }
     });
