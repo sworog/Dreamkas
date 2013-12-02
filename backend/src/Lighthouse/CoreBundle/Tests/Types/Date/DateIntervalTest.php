010729bb46241fd540dd1978bc19557e6b3d76da
Mikhail Shamin <mshamin@crystals.ru>
2013-12-02 17:07:43 +0400

    DateInterval tests

--- backend/src/Lighthouse/CoreBundle/Tests/Types/Date/DateIntervalTest.php
+++ backend/src/Lighthouse/CoreBundle/Tests/Types/Date/DateIntervalTest.php
@@ -34,11 +34,48 @@ class DateIntervalTest extends TestCase
         );
     }
 
-    public function testCreateFromDateString()
+    /**
+     * @param string $time
+     * @dataProvider createFromDateStringProvider
+     */
+    public function testCreateFromDateString($time)
     {
-        $this->markTestIncomplete();
-        $interval = DateInterval::createFromDateString('-1 days');
+        $parentInterval = \DateInterval::createFromDateString($time);
+        $interval = DateInterval::createFromDateString($time);
         $this->assertInstanceOf('Lighthouse\\CoreBundle\\Types\\Date\\DateInterval', $interval);
         $this->assertFalse($interval->isEmpty());
+
+        $date1 = new \DateTime('2013-01-13 00:00:00');
+        $date2 = clone $date1;
+        $date1->add($parentInterval);
+        $date2->add($interval);
+
+        $this->assertSame($date1->format(\DateTime::ISO8601), $date2->format(\DateTime::ISO8601));
+        $this->assertSame($date1->getTimestamp(), $date2->getTimestamp());
+    }
+
+    /**
+     * @param $time
+     * @param $expectedIntervalSpec
+     * @dataProvider createFromDateStringProvider
+     */
+    public function testGetIntervalSpec($time, $expectedIntervalSpec)
+    {
+        $interval = DateInterval::createFromDateString($time);
+        $this->assertSame($expectedIntervalSpec, $interval->getIntervalSpec());
+    }
+
+    /**
+     * @return array
+     */
+    public function createFromDateStringProvider()
+    {
+        return array(
+            '-1 days' => array('-1 days', 'P0Y0M1DT0H0M0S'),
+            '1 days' => array('1 days', 'P0Y0M1DT0H0M0S'),
+            '-3 seconds' => array('-3 seconds', 'P0Y0M0DT0H0M3S'),
+            '5 second' => array('5 second', 'P0Y0M0DT0H0M5S'),
+            '-1 week -2 hours' => array('-1 week -2 hours', 'P0Y0M7DT2H0M0S')
+        );
     }
 }
