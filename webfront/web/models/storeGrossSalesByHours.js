6a0eff84dbec91560b11f3aba1e3c701c53555f9
Andrew Borovin <andrew@borovin.com>
2013-12-03 17:39:01 +0400

    store gross sales report table

--- webfront/web/models/storeGrossSalesByHours.js
+++ webfront/web/models/storeGrossSalesByHours.js
@@ -6,7 +6,7 @@ define(function(require, exports, module) {
         __name__: module.id,
         storeId: null,
         url: function(){
-            return ('http://lh.apiary.io/' || LH.baseApiUrl) + '/stores/' + this.storeId + '/reports/grossSalesByHours';
+            return LH.baseApiUrl + '/stores/' + this.storeId + '/reports/grossSalesByHours';
         }
     });
 });
