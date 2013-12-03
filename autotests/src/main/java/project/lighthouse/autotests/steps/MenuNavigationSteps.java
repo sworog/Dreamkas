06fce824405d3cb8481b1d21e824e0658cd97e97
Andrey <atolpeev@crystals.ru>
2013-12-03 16:53:22 +0400

    Added menu navigation code implementation.

---
+++ autotests/src/main/java/project/lighthouse/autotests/steps/MenuNavigationSteps.java
@@ -0,0 +1,15 @@
+package project.lighthouse.autotests.steps;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+import project.lighthouse.autotests.pages.MenuNavigation;
+
+public class MenuNavigationSteps extends ScenarioSteps {
+
+    MenuNavigation menuNavigation;
+
+    @Step
+    public void reportMenuItemClick() {
+        menuNavigation.reportMenuItemClick();
+    }
+}
---
+++ autotests/src/test/java/project/lighthouse/autotests/jbehave/menuNavigation/WhenMenuNavigationUserSteps.java
@@ -0,0 +1,16 @@
+package project.lighthouse.autotests.jbehave.menuNavigation;
+
+import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.When;
+import project.lighthouse.autotests.steps.MenuNavigationSteps;
+
+public class WhenMenuNavigationUserSteps {
+
+    @Steps
+    MenuNavigationSteps menuNavigationSteps;
+
+    @When("the user clicks the menu report item")
+    public void whenTheUserClicksTheMenuReportItemClick() {
+        menuNavigationSteps.reportMenuItemClick();
+    }
+}
