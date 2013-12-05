5bc8c19758d3c0dfd1dcbe579c404ea8c72a0f48
Mikhail Shamin <mshamin@crystals.ru>
2013-12-05 16:37:20 +0400

    Added cache clear test for better code coverage

---
+++ backend/src/Lighthouse/CoreBundle/Tests/LighthouseCoreBundleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Tests;
+
+use Lighthouse\CoreBundle\Test\ContainerAwareTestCase;
+
+class LighthouseCoreBundleTest extends ContainerAwareTestCase
+{
+    public function testCacheWarmUp()
+    {
+        $filesystem = $this->getContainer()->get('filesystem');
+        $cacheDir = sys_get_temp_dir() . '/' . uniqid('lighthouse-cache-warm-up-', true);
+        if ($filesystem->exists($cacheDir)) {
+            $filesystem->remove($cacheDir);
+        }
+        $filesystem->mkdir($cacheDir);
+        $warmer = $this->getContainer()->get('cache_warmer');
+        $warmer->enableOptionalWarmers();
+        $warmer->warmUp($cacheDir);
+    }
+}
