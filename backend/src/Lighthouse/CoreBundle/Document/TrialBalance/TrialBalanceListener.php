e04a31fbc52c5408d17b34b8daf002a415299e7f
Mikhail Shamin <mshamin@crystals.ru>
2013-12-12 16:07:13 +0400

    Persist and flush new trial balances on postFlush so they will be inserted and no extra updated as it was before

--- backend/src/Lighthouse/CoreBundle/Document/TrialBalance/TrialBalanceListener.php
+++ backend/src/Lighthouse/CoreBundle/Document/TrialBalance/TrialBalanceListener.php
@@ -4,17 +4,19 @@ namespace Lighthouse\CoreBundle\Document\TrialBalance;
 
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Event\OnFlushEventArgs;
+use Doctrine\ODM\MongoDB\Event\PostFlushEventArgs;
 use Doctrine\ODM\MongoDB\UnitOfWork;
 use JMS\DiExtraBundle\Annotation as DI;
 use Lighthouse\CoreBundle\Document\AbstractMongoDBListener;
 use Lighthouse\CoreBundle\Document\Invoice\Invoice;
 use Lighthouse\CoreBundle\Document\Invoice\Product\InvoiceProductRepository;
 use Lighthouse\CoreBundle\Document\Product\Store\StoreProductRepository;
+use SplQueue;
 
 /**
  * Class TrialBalanceListener
  *
- * @DI\DoctrineMongoDBListener(events={"onFlush"})
+ * @DI\DoctrineMongoDBListener(events={"onFlush", "postFlush"})
  */
 class TrialBalanceListener extends AbstractMongoDBListener
 {
@@ -34,6 +36,16 @@ class TrialBalanceListener extends AbstractMongoDBListener
     protected $storeProductRepository;
 
     /**
+     * @var SplQueue|TrialBalance[]
+     */
+    protected $trialBalanceQueue;
+
+    /**
+     * @var int
+     */
+    protected $postFlushCounter = 0;
+
+    /**
      * @DI\InjectParams({
      *      "trialBalanceRepository" = @DI\Inject("lighthouse.core.document.repository.trial_balance"),
      *      "invoiceProductRepository" = @DI\Inject("lighthouse.core.document.repository.invoice_product"),
@@ -51,6 +63,8 @@ class TrialBalanceListener extends AbstractMongoDBListener
         $this->trialBalanceRepository = $trialBalanceRepository;
         $this->invoiceProductRepository = $invoiceProductRepository;
         $this->storeProductRepository = $storeProductRepository;
+
+        $this->trialBalanceQueue = new SplQueue(SplQueue::IT_MODE_DELETE);
     }
 
     /**
@@ -91,20 +105,17 @@ class TrialBalanceListener extends AbstractMongoDBListener
      */
     protected function onReasonablePersist(Reasonable $document, DocumentManager $dm)
     {
-        $trialBalance = new TrialBalance();
-
         $storeProduct = $this->storeProductRepository->findOrCreateByReason($document);
+        $this->computeChangeSet($dm, $storeProduct);
 
+        $trialBalance = new TrialBalance();
         $trialBalance->price = $document->getProductPrice();
         $trialBalance->quantity = $document->getProductQuantity()->toNumber();
         $trialBalance->storeProduct = $storeProduct;
         $trialBalance->reason = $document;
         $trialBalance->createdDate = $document->getReasonDate();
 
-        $dm->persist($trialBalance);
-
-        $this->computeChangeSet($dm, $storeProduct);
-        $this->computeChangeSet($dm, $trialBalance);
+        $this->enqueueTrialBalance($trialBalance);
     }
     
     /**
@@ -159,6 +170,28 @@ class TrialBalanceListener extends AbstractMongoDBListener
         foreach ($trailBalances as $trailBalance) {
             $trailBalance->createdDate = $newAcceptanceDate;
             $this->computeChangeSet($dm, $trailBalance);
+        }
+    }
+
+    /**
+     * @param TrialBalance $trialBalance
+     */
+    protected function enqueueTrialBalance(TrialBalance $trialBalance)
+    {
+        $this->trialBalanceQueue->enqueue($trialBalance);
+    }
+
+    /**
+     * @param PostFlushEventArgs $eventArgs
+     */
+    public function postFlush(PostFlushEventArgs $eventArgs)
+    {
+        if (0 == $this->postFlushCounter++) {
+            $dm = $eventArgs->getDocumentManager();
+            foreach ($this->trialBalanceQueue as $trialBalance) {
+                $dm->persist($trialBalance);
+            }
+            $dm->flush();
         }
     }
 }
