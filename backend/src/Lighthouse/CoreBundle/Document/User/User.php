d8d6cad9f23835f5d5ac3870537900c99af7308c
Mikhail Shamin <mshamin@crystals.ru>
2013-12-19 15:45:09 +0400

    Removed not used EquatableInterface from User

--- backend/src/Lighthouse/CoreBundle/Document/User/User.php
+++ backend/src/Lighthouse/CoreBundle/Document/User/User.php
@@ -6,7 +6,6 @@ use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 use JMS\Serializer\Annotation as Serializer;
 use Lighthouse\CoreBundle\Document\AbstractDocument;
 use Symfony\Component\Security\Core\Role\Role;
-use Symfony\Component\Security\Core\User\EquatableInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Lighthouse\CoreBundle\Validator\Constraints as LighthouseAssert;
@@ -30,7 +29,7 @@ use Doctrine\Bundle\MongoDBBundle\Validator\Constraints\Unique;
  * )
  * @Unique(fields="username", message="lighthouse.validation.errors.user.username.unique")
  */
-class User extends AbstractDocument implements UserInterface, EquatableInterface
+class User extends AbstractDocument implements UserInterface
 {
     const ROLE_COMMERCIAL_MANAGER = "ROLE_COMMERCIAL_MANAGER";
     const ROLE_STORE_MANAGER = "ROLE_STORE_MANAGER";
@@ -101,54 +100,6 @@ class User extends AbstractDocument implements UserInterface, EquatableInterface
     protected $salt;
 
     /**
-     * The equality comparison should neither be done by referential equality
-     * nor by comparing identities (i.e. getId() === getId()).
-     *
-     * However, you do not need to compare every attribute, but only those that
-     * are relevant for assessing whether re-authentication is required.
-     *
-     * Also implementation should consider that $user instance may implement
-     * the extended user interface `AdvancedUserInterface`.
-     *
-     * @param UserInterface $user
-     *
-     * @return Boolean
-     */
-    public function isEqualTo(UserInterface $user)
-    {
-        if (!$user instanceof User) {
-            return false;
-        }
-
-        if ($this->password !== $user->getPassword()) {
-            return false;
-        }
-
-        if ($this->getSalt() !== $user->getSalt()) {
-            return false;
-        }
-
-        if ($this->username !== $user->getUsername()) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Returns the roles granted to the user.
-     *
-     * <code>
-     * public function getRoles()
-     * {
-     *     return array('ROLE_USER');
-     * }
-     * </code>
-     *
-     * Alternatively, the roles might be stored on a ``roles`` property,
-     * and populated in any number of different ways when the user object
-     * is created.
-     *
      * @return Role[] The user roles
      */
     public function getRoles()
@@ -159,9 +110,6 @@ class User extends AbstractDocument implements UserInterface, EquatableInterface
     /**
      * Returns the password used to authenticate the user.
      *
-     * This should be the encoded password. On authentication, a plain-text
-     * password will be salted, encoded, and then compared to this value.
-     *
      * @return string The password
      */
     public function getPassword()
@@ -171,8 +119,6 @@ class User extends AbstractDocument implements UserInterface, EquatableInterface
 
     /**
      * Returns the salt that was originally used to encode the password.
-     *
-     * This can return null if the password was not encoded using a salt.
      *
      * @return string The salt
      */
