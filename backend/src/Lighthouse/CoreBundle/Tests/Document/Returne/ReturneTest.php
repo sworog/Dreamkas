aacb9fcf2520c775aca73cf4bbb3c1e441d2ceae
Mikhail Shamin <mshamin@crystals.ru>
2013-12-20 19:50:13 +0400

    Added Returne createdDate test

---
+++ backend/src/Lighthouse/CoreBundle/Tests/Document/Returne/ReturneTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lighthouse\Returne;
+
+use Lighthouse\CoreBundle\Document\Returne\Returne;
+use Lighthouse\CoreBundle\Test\ContainerAwareTestCase;
+
+class ReturneTest extends ContainerAwareTestCase
+{
+    public function testCreatedDateIsSetOnPrePersist()
+    {
+        $this->clearMongoDb();
+
+        $dm = $this->getDocumentManager();
+        $return = new Returne();
+        $return->hash = 'hash';
+
+        $this->assertNull($return->createdDate);
+        $dm->persist($return);
+
+        $expectedTime = time();
+
+        $dm->flush();
+
+        $this->assertNotNull($return->createdDate);
+        $this->assertEquals($expectedTime, $return->createdDate->getTimestamp());
+    }
+}
