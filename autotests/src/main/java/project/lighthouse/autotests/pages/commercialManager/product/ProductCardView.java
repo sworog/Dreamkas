8b54f3e3dc762e75bdf6507d0643f5ea752e04af
Andrey <atolpeev@crystals.ru>
2014-01-20 12:56:43 +0400

    Fixed npe on items.get.

--- autotests/src/main/java/project/lighthouse/autotests/pages/commercialManager/product/ProductCardView.java
+++ autotests/src/main/java/project/lighthouse/autotests/pages/commercialManager/product/ProductCardView.java
@@ -3,16 +3,11 @@ package project.lighthouse.autotests.pages.commercialManager.product;
 import org.jbehave.core.model.ExamplesTable;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import project.lighthouse.autotests.common.CommonPageObject;
 
-public class ProductCardView extends CommonPageObject {
+public class ProductCardView extends ProductCreatePage {
 
     public ProductCardView(WebDriver driver) {
         super(driver);
-    }
-
-    @Override
-    public void createElements() {
     }
 
     public void checkCardValue(String elementName, String expectedValue) {
