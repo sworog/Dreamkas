c3dd83766f45be77a3b36821f6bf405d7102a8e1
Mikhail Shamin <mshamin@crystals.ru>
2014-01-20 17:34:00 +0400

    Fixed inspection error

--- backend/src/Lighthouse/ReportsBundle/DependencyInjection/LighthouseReportsExtension.php
+++ backend/src/Lighthouse/ReportsBundle/DependencyInjection/LighthouseReportsExtension.php
@@ -7,7 +7,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-
 class LighthouseReportsExtension extends Extension
 {
     /**
