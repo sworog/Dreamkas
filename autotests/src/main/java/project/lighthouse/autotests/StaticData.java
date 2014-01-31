5a3c357f283b40fff831d26e97c768bac11d0fea
Andrey <atolpeev@crystals.ru>
2014-01-31 19:25:38 +0400

    Decrease DEFAULT_TIMEOUT to 15 seconds.

--- autotests/src/main/java/project/lighthouse/autotests/StaticData.java
+++ autotests/src/main/java/project/lighthouse/autotests/StaticData.java
@@ -19,7 +19,7 @@ public class StaticData {
     public static HashMap<String, String> userTokens = new HashMap<>();
     public static HashMap<String, ArrayList<Product>> subCategoryProducts = new HashMap<>();
 
-    public static Integer DEFAULT_TIMEOUT = 30;
+    public static Integer DEFAULT_TIMEOUT = 15;
     public static Integer DEFAULT_PRE_LOADER_TIMEOUT = 5;
     public static Integer DEFAULT_VALIDATION_ERROR_TIMEOUT = 10;
 
