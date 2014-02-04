35e2e363d0b9dba71a306bbeeee6929740ea1aca
Mikhail Shamin <mshamin@crystals.ru>
2014-02-04 14:06:13 +0400

    Added date period to invoices import

--- backend/src/Lighthouse/CoreBundle/Command/Import/OneCInvoicesImport.php
+++ backend/src/Lighthouse/CoreBundle/Command/Import/OneCInvoicesImport.php
@@ -4,6 +4,7 @@ namespace Lighthouse\CoreBundle\Command\Import;
 
 use JMS\DiExtraBundle\Annotation as DI;
 use Lighthouse\CoreBundle\Integration\OneC\Import\Invoices\InvoicesImporter;
+use Lighthouse\CoreBundle\Types\Date\DatePeriod;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -37,6 +38,8 @@ class OneCInvoicesImport extends Command
     {
         $this->addArgument('file', InputArgument::REQUIRED, 'Csv file path');
         $this->addOption('batch', null, InputOption::VALUE_OPTIONAL, 'Batch size', 100);
+        $this->addOption('import-date', null, InputOption::VALUE_OPTIONAL, 'Invoice date to be imported to');
+        $this->addOption('original-date', null, InputOption::VALUE_OPTIONAL, 'Original invoice date');
     }
 
     /**
@@ -48,7 +51,24 @@ class OneCInvoicesImport extends Command
     {
         $filePath = $input->getArgument('file');
         $batchSize = $input->getOption('batch');
+        $importDate = $input->getOption('import-date');
+        $originalDate = $input->getOption('original-date');
+        $period = $this->getDatePeriod($importDate, $originalDate);
 
-        $this->importer->import($filePath, $batchSize, $output);
+        $this->importer->import($filePath, $batchSize, $output, $period);
+    }
+
+    /**
+     * @param string $startDate
+     * @param string $endDate
+     * @return DatePeriod
+     */
+    protected function getDatePeriod($startDate, $endDate)
+    {
+        if (null !== $startDate && null !== $endDate) {
+            return new DatePeriod($startDate, $endDate);
+        } else {
+            return null;
+        }
     }
 }
