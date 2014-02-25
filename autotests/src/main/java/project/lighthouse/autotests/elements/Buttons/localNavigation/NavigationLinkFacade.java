d04b7ffd01c9c315b56164366aaee35ee23b8554
Andrey <atolpeev@crystals.ru>
2014-02-25 16:52:54 +0400

    Refactored NavigationLinkFacade and ButtonFacade

--- autotests/src/main/java/project/lighthouse/autotests/elements/Buttons/localNavigation/NavigationLinkFacade.java
+++ autotests/src/main/java/project/lighthouse/autotests/elements/Buttons/localNavigation/NavigationLinkFacade.java
@@ -1,22 +1,21 @@
 package project.lighthouse.autotests.elements.Buttons.localNavigation;
 
-import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
-import project.lighthouse.autotests.common.CommonActions;
+import project.lighthouse.autotests.common.CommonPageObject;
 
 public class NavigationLinkFacade {
 
     private String xpath;
     private static final String xpathPattern = "//*[contains(@class, 'localNavigation__link') and normalize-space(text())='%s']";
 
-    private CommonActions commonActions;
+    private CommonPageObject pageObject;
 
-    public NavigationLinkFacade(PageObject pageObject, String linkText) {
+    public NavigationLinkFacade(CommonPageObject pageObject, String linkText) {
+        this.pageObject = pageObject;
         xpath = String.format(xpathPattern, linkText);
-        commonActions = new CommonActions(pageObject);
     }
 
     public void click() {
-        commonActions.elementClick(By.xpath(xpath));
+        pageObject.getCommonActions().elementClick(By.xpath(xpath));
     }
 }
