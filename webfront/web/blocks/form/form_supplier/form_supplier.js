d0d78a0639cb92f8e53b107af2345732b4d502d4
Andrew Borovin <andrew@borovin.com>
2014-03-03 19:07:49 +0400

    agreement upload form

--- webfront/web/blocks/form/form_supplier/form_supplier.js
+++ webfront/web/blocks/form/form_supplier/form_supplier.js
@@ -2,13 +2,21 @@ define(function(require, exports, module) {
     //requirements
     var Form = require('kit/blocks/form/form'),
         SupplierModel = require('models/supplier'),
-        FileModel = require('models/file');
+        cookie = require('kit/libs/cookie');
+
+    var templates = {
+        form_supplier__agreementField: require('tpl!./form_supplier__agreementField.html')
+    };
+
+    require('jquery');
+
+    var authorizationHeader = 'Bearer ' + cookie.get('token');
 
     return Form.extend({
         __name__: module.id,
         redirectUrl: '/suppliers',
         el: '.form_supplier',
-        model: function(){
+        model: function() {
             return new SupplierModel();
         },
         events: {
@@ -17,19 +25,25 @@ define(function(require, exports, module) {
                     reader = new FileReader(),
                     file = e.target.files[0],
                     button = e.target.parentElement,
-                    form__field = button.parentElement,
-                    agreementInput = form__field.querySelector('[name="agreement"]'),
-                    fileModel = new FileModel();
+                    $form__field = $(button).closest('.form__field'),
+                    agreementInput = $form__field.find('[name="agreement"]');
 
                 button.classList.add('preloader_rows');
                 block.disable(true);
 
                 reader.onload = function(evt) {
-                    fileModel.save({
-                        file: evt.target.result
-                    }, {
-                        success: function() {
-                            agreementInput.value = fileModel.id;
+                    $.ajax({
+                        url: LH.baseApiUrl + '/files',
+                        dataType: 'json',
+                        type: 'POST',
+                        headers: {
+                            Authorization: authorizationHeader,
+                            'x-file-name': file.name
+
+                        },
+                        data: evt.target.result,
+                        success: function(res) {
+                            block.renderAgreementField(res);
                             button.classList.remove('preloader_rows');
                             block.disable(false);
                         },
@@ -44,6 +58,14 @@ define(function(require, exports, module) {
 
                 reader.readAsBinaryString(file);
             }
+        },
+        renderAgreementField: function(agreement) {
+            var block = this,
+                form_supplier__fileField = block.el.querySelector('.form_supplier__fileField');
+
+            $(form_supplier__fileField).replaceWith(templates.form_supplier__agreementField({
+                agreement: agreement
+            }));
         }
     });
 });
