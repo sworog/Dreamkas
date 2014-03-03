4de8ed382aa08899653613bbcb47b6fbab9b683a
Mikhail Shamin <mshamin@crystals.ru>
2014-03-03 20:01:21 +0400

    Container tweaker in Test client

--- backend/src/Lighthouse/CoreBundle/Test/Client/Client.php
+++ backend/src/Lighthouse/CoreBundle/Test/Client/Client.php
@@ -3,9 +3,33 @@
 namespace Lighthouse\CoreBundle\Test\Client;
 
 use Symfony\Bundle\FrameworkBundle\Client as BaseClient;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\HttpKernel\TerminableInterface;
+use Closure;
 
 class Client extends BaseClient
 {
+    /**
+     * @var KernelInterface
+     */
+    protected $kernel;
+
+    /**
+     * @var bool
+     */
+    private $hasPerformedRequest = false;
+
+    /**
+     * @var bool
+     */
+    private $profiler = false;
+
+    /**
+     * @var Closure[]
+     */
+    protected $tweakers = array();
+
     /**
      * @param string $uri
      * @param string $content
@@ -57,5 +81,52 @@ class Client extends BaseClient
         }
 
         return $json;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function doRequest($request)
+    {
+        if ($this->hasPerformedRequest) {
+            $this->kernel->shutdown();
+        } else {
+            $this->hasPerformedRequest = true;
+        }
+
+        $this->tweakContainer();
+
+        if ($this->profiler) {
+            $this->profiler = false;
+
+            $this->kernel->boot();
+            $this->kernel->getContainer()->get('profiler')->enable();
+        }
+
+        $response = $this->kernel->handle($request, HttpKernelInterface::MASTER_REQUEST, true);
+
+        if ($this->kernel instanceof TerminableInterface) {
+            $this->kernel->terminate($request, $response);
+        }
+
+        return $response;
+    }
+
+    protected function tweakContainer()
+    {
+        $this->kernel->boot();
+        foreach ($this->tweakers as $tweaker) {
+            $tweaker($this->getContainer());
+        }
+        $this->tweakers = array();
+    }
+
+    /**
+     * @param callable $tweaker
+     */
+    public function addTweaker(Closure $tweaker)
+    {
+        $this->tweakers[] = $tweaker;
     }
 }
