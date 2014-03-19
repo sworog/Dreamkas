71deda62e3facf90b1e565ead5eb8fc2aac88dfa
Andrew Borovin <andrew@borovin.com>
2014-03-19 14:12:36 +0400

    update order permissions

--- webfront/web/pages/orders/list/list.js
+++ webfront/web/pages/orders/list/list.js
@@ -11,7 +11,7 @@ define(function(require, exports, module) {
             '#content': require('tpl!./content.html')
         },
         permissions: function() {
-            return !LH.isAllow('stores', 'GET::{store}/orders/{order}');
+            return !LH.isAllow('stores/{store}/orders', 'GET');
         },
         initialize: function() {
             var page = this;
