a472e55a5bc33f6df41b705374a05383f7cf87e9
Andrey Tolpeev <atolpeev@crystals.ru>
2014-03-23 12:15:55 +0400

    Changed @step assertion 403/404 errors.

--- autotests/src/main/java/project/lighthouse/autotests/steps/ErrorPageSteps.java
+++ autotests/src/main/java/project/lighthouse/autotests/steps/ErrorPageSteps.java
@@ -2,9 +2,9 @@ package project.lighthouse.autotests.steps;
 
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 import project.lighthouse.autotests.pages.errorPage.ErrorPage;
-
-import static org.junit.Assert.fail;
 
 public class ErrorPageSteps extends ScenarioSteps {
 
@@ -12,29 +12,25 @@ public class ErrorPageSteps extends ScenarioSteps {
 
     @Step
     public void error403IsPresent() {
-        try {
-            errorPage.getPageError403WebElement();
-        } catch (Exception e) {
-            fail("The error 403 is not present on the page!");
+        WebElement error403WEbWebElement = errorPage.getPageError403WebElement();
+        if (!errorPage.visibilityOfElementLocated(error403WEbWebElement)) {
+            Assert.fail("The error 403 page is not present on the page!");
         }
     }
 
     @Step
     public void error404isPresent() {
-        try {
-            errorPage.getPageError404WebElement();
-        } catch (Exception e) {
-            fail("The error 404 is not present on the page!");
+        WebElement error404WebElement = errorPage.getPageError404WebElement();
+        if (!errorPage.visibilityOfElementLocated(error404WebElement)) {
+            Assert.fail("The error 404 page is not present on the page!");
         }
     }
 
     @Step
     public void error403IsNotPresent() {
-        try {
-            //TODO check
-            errorPage.getWaiter().invisibilityOfElementLocated(errorPage.getPageError403WebElement());
-        } catch (Exception e) {
-            fail("The error 403 is present on the page!");
+        WebElement error403WebElement = errorPage.getPageError403WebElement();
+        if (!errorPage.invisibilityOfElementLocated(error403WebElement)) {
+            Assert.fail("The error 403 page is present on the page!");
         }
     }
 }
