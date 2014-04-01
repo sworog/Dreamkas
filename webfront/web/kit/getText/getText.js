6091c3bd7d09b98265c7270f7af0a9e3b962b8c9
Andrew Borovin <andrew@borovin.com>
2014-04-01 17:18:24 +0400

    fix plural forms

--- webfront/web/kit/getText/getText.js
+++ webfront/web/kit/getText/getText.js
@@ -53,7 +53,7 @@ define(function(require) {
             pluralForm = num % 10 == 1 && num % 100 != 11 ? 0 : num % 10 >= 2 && num % 10 <= 4 && (num % 100 < 10 || num % 100 >= 20) ? 1 : 2;
             output = getText.compile(input[pluralForm], placeholders);
         } else if (_.isArray(input) && input.length === 2) {
-            pluralForm = (n != 1);
+            pluralForm = (num != 1) ? 1 : 0;
             output = getText.compile(input[pluralForm], placeholders);
         }
 
