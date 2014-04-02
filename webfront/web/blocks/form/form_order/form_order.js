1417f84e2a44078766d9279576392dff7affb8f8
Andrew Borovin <andrew@borovin.com>
2014-04-02 19:38:43 +0400

    add remove link and product errors messages

--- webfront/web/blocks/form/form_order/form_order.js
+++ webfront/web/blocks/form/form_order/form_order.js
@@ -19,8 +19,9 @@ define(function(require, exports, module) {
         model: new OrderModel(),
         editedProductModel: null,
         nextEditedProductModel: null,
+        $errorTr: $('<tr class="table__orderProduct__error"><td colspan="5"></td></tr>'),
         events: {
-            'change [name]': function(e){
+            'change [name]': function(e) {
                 var block = this;
 
                 block.model.set(e.target.name, e.target.value);
@@ -33,49 +34,55 @@ define(function(require, exports, module) {
                         return model.cid === productCid;
                     });
 
-                if (block.editedProductModel){
+                if (block.editedProductModel && block.editedProductModel !== orderProductModel) {
                     block.nextEditedProductModel = orderProductModel;
                 } else {
                     block.editProduct(orderProductModel);
                 }
             },
-            'blur .table__orderProduct input': function(e){
-                var block = this;
-
-                if (!e.target.classList.contains('preloader_stripes')){
-                    block.finishEdit();
-                }
-            },
-            'change .table__orderProduct input': function(e){
+            'blur .table__orderProduct input': function(e) {
                 var block = this;
 
                 e.target.classList.add('preloader_stripes');
                 block.validateEditedProduct(e.target.dataset.name, e.target.value);
             },
-            'keydown .table__orderProduct input': function(e){
+            'keydown .table__orderProduct input': function(e) {
                 var block = this;
 
-                if (e.keyCode === 13){
+                if (e.keyCode === 13) {
                     e.preventDefault();
                     e.target.classList.add('preloader_stripes');
                     block.validateEditedProduct(e.target.dataset.name, e.target.value);
                 }
             },
-            'keyup .table__orderProduct input': function(e){
+            'keyup .table__orderProduct input': function(e) {
                 var block = this;
 
                 block.renderProductSum(e.target.value);
+            },
+            'click .form_order__removeProductLink': function(e) {
+                e.stopPropagation();
+
+                var block = this,
+                    productCid = e.target.dataset.product_cid,
+                    orderProductModel = block.model.get('collections.products').find(function(model) {
+                        return model.cid === productCid;
+                    });
+
+                orderProductModel.id = null;
+
+                orderProductModel.destroy();
             }
         },
         listeners: {
             'model': {
-                change: function(){
+                change: function() {
                     var block = this;
                     block.el.classList.add('form_changed');
                 }
             },
             'blocks.autocomplete': {
-                select: function(product){
+                select: function(product) {
                     var block = this;
 
                     block.model.get('collections.products').push({
@@ -88,11 +95,11 @@ define(function(require, exports, module) {
         initialize: function() {
             var block = this;
 
-            block.model.get('collections.products').on('change add remove', function(){
+            block.model.get('collections.products').on('change add remove', function() {
                 block.el.classList.add('form_changed');
             });
 
-            block.model.get('collections.products').on('add', function(orderProductModel){
+            block.model.get('collections.products').on('add', function(orderProductModel) {
                 block.editProduct(orderProductModel);
             });
 
@@ -100,7 +107,7 @@ define(function(require, exports, module) {
                 autocomplete: new Autocomplete()
             };
         },
-        editProduct: function(orderProductModel){
+        editProduct: function(orderProductModel) {
             var block = this,
                 tr = block.el.querySelector('tr[data-product_cid="' + orderProductModel.cid + '"]');
 
@@ -109,36 +116,44 @@ define(function(require, exports, module) {
             tr.classList.add('table__orderProduct_edit');
             tr.querySelector('[autofocus]').focus();
         },
-        validateEditedProduct: function(key, value){
+        validateEditedProduct: function(key, value) {
             var block = this,
                 changes = {};
+
+            block.removeProductError();
 
             changes[key] = value;
 
             block.editedProductModel.id = null;
 
             block.editedProductModel.save(changes, {
-                success: function(){
+                success: function() {
                     block.finishEdit();
 
-                    if (block.nextEditedProductModel){
+                    if (block.nextEditedProductModel) {
                         block.editProduct(block.nextEditedProductModel);
-                        block.nextEditedProductModel = null;
                     }
+
+                    block.nextEditedProductModel = null;
+                },
+                error: function(model, res) {
+                    block.showProductError(res.responseJSON);
+                    block.nextEditedProductModel = null;
                 }
             });
         },
-        finishEdit: function(){
+        finishEdit: function() {
             var block = this,
                 tr = block.el.querySelector('.table__orderProduct_edit');
 
             block.editedProductModel = null;
+            block.removeProductError();
 
-            if (tr){
+            if (tr) {
                 tr.classList.remove('table__orderProduct_edit');
             }
         },
-        renderProductSum: function(quantity){
+        renderProductSum: function(quantity) {
             var block = this,
                 productSum = block.el.querySelector('.table__orderProduct_edit .table_orderProducts__productSum');
 
@@ -146,6 +161,24 @@ define(function(require, exports, module) {
                 orderProductModel: block.editedProductModel,
                 quantity: quantity
             });
+        },
+        showProductError: function(error) {
+            var block = this,
+                tr = block.el.querySelector('.table__orderProduct_edit'),
+                quantityInput = block.el.querySelector('.table__orderProduct_edit [data-name="quantity"]');
+
+            if (quantityInput) {
+                quantityInput.classList.add('inputText_error');
+                quantityInput.classList.remove('preloader_stripes');
+                quantityInput.focus();
+            }
+
+            block.$errorTr.insertAfter(tr).find('td').html(error.children.quantity.errors.join(', '));
+        },
+        removeProductError: function() {
+            var block = this;
+
+            block.$errorTr.detach();
         }
     });
 });
