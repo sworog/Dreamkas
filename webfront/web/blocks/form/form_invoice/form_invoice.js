8c3fa3576ca0ed39eefcdf0b3e5d856aaf647144
Andrew Borovin <a.borovin@crystals.ru>
2014-04-11 18:27:38 +0400

    focus/blur in invoice product

--- webfront/web/blocks/form/form_invoice/form_invoice.js
+++ webfront/web/blocks/form/form_invoice/form_invoice.js
@@ -16,6 +16,67 @@ define(function(require) {
         },
         el: '.form_invoice',
         storeId: null,
+        editedProductModel: null,
+        editedProductField: null,
+        nextEditedProductModel: null,
+        nextEditedProductField: null,
+        $errorTr: $('<tr class="table__orderProduct__error"><td colspan="6"></td></tr>'),
+        events: {
+            'click tr[data-product_cid]': function(e) {
+                var block = this,
+                    tr = e.currentTarget,
+                    productCid = tr.dataset.product_cid,
+                    orderProductModel = block.model.get('products').find(function(model) {
+                        return model.cid === productCid;
+                    });
+
+                if (block.editedProductModel && block.editedProductModel !== orderProductModel) {
+                    block.nextEditedProductModel = orderProductModel;
+                } else {
+                    block.editProduct(orderProductModel);
+                }
+            },
+            'keydown .table__invoiceProduct input': function(e) {
+                var block = this;
+
+                if (e.keyCode === 13) {
+                    e.preventDefault();
+                    e.target.classList.add('preloader_stripes');
+                    block.validateProducts(products);
+                }
+            },
+            'blur .table__invoiceProduct input': function(e) {
+                var block = this;
+
+                if (block.editedProductModel && block.editedProductField === e.target) {
+                    e.target.classList.add('preloader_stripes');
+                    block.validateProducts(e.target.dataset.name, e.target.value);
+                }
+            },
+            'focus .table__invoiceProduct input': function(e) {
+                var block = this;
+
+                if (!block.editedProductField){
+                    block.editedProductField = e.target;
+                } else if (block.editedProductField !== e.target) {
+                    block.editedProductField.focus();
+                    block.nextEditedProductField = e.target;
+                }
+            },
+            'click .form_invoice__removeProductLink': function(e) {
+                e.stopPropagation();
+
+                var block = this,
+                    productCid = e.target.dataset.product_cid,
+                    orderProductModel = block.model.get('products').find(function(model) {
+                        return model.cid === productCid;
+                    });
+
+                orderProductModel.id = null;
+
+                orderProductModel.destroy();
+            }
+        },
         listeners: {
             'blocks.autocomplete': {
                 select: function(storeProduct) {
@@ -58,6 +119,87 @@ define(function(require) {
 
             tr.classList.add('table__invoiceProduct_edit');
             tr.querySelector('[autofocus]').focus();
+        },
+        validateProducts: function(products) {
+            var block = this;
+
+            block.removeProductError();
+
+            setTimeout(function(){
+                block.finishEdit();
+            }, 2000);
+
+//            block.editedProductModel.save(changes, {
+//                success: function() {
+//                    block.finishEdit();
+//
+//                    if (block.nextEditedProductModel) {
+//                        block.editProduct(block.nextEditedProductModel);
+//                    }
+//
+//                    block.nextEditedProductModel = null;
+//                },
+//                error: function(model, res) {
+//                    block.showProductError(res.responseJSON);
+//                    block.nextEditedProductModel = null;
+//                }
+//            });
+        },
+        finishEdit: function() {
+            var block = this,
+                trs = block.el.querySelectorAll('.table__invoiceProduct_edit'),
+                inputs = block.el.querySelectorAll('.table__invoiceProduct_edit input');
+
+            if (block.nextEditedProductModel) {
+                block.editProduct(block.nextEditedProductModel);
+                block.nextEditedProductModel = null;
+                return;
+            }
+
+            block.editedProductField = null;
+            block.removeProductError();
+
+            if (inputs.length) {
+                _.forEach(inputs, function(input){
+                    input.classList.remove('preloader_stripes');
+                });
+            }
+
+            if (block.nextEditedProductField){
+                block.editedProductField = block.nextEditedProductField;
+                block.editedProductField.focus();
+                return;
+            }
+
+            block.editedProductModel = null;
+
+            if (trs) {
+                _.forEach(trs, function(tr) {
+                    tr.classList.remove('table__invoiceProduct_edit');
+                });
+            }
+        },
+        showProductError: function(error) {
+            var block = this,
+                tr = block.el.querySelector('.table__invoiceProduct_edit'),
+                quantityInput = block.el.querySelector('.table__invoiceProduct_edit [data-name="quantity"]');
+
+            if (quantityInput) {
+                quantityInput.classList.add('inputText_error');
+                quantityInput.classList.remove('preloader_stripes');
+                quantityInput.focus();
+            }
+
+            block.$errorTr
+                .insertAfter(tr)
+                .attr('data-error', error.children.quantity.errors.join(', '))
+                .find('td')
+                .html(error.children.quantity.errors.join(', '));
+        },
+        removeProductError: function() {
+            var block = this;
+
+            block.$errorTr.detach();
         }
     });
 });
