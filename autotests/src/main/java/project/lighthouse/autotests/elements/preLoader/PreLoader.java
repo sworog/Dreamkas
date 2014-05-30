c9cf35a0d7c81064f28dd4521ea35facf0ba0228
Andrey Tolpeev <atolpeev@crystals.ru>
2014-05-30 17:00:08 +0400

    Fixed button preloader xpath locator.

--- autotests/src/main/java/project/lighthouse/autotests/elements/preLoader/PreLoader.java
+++ autotests/src/main/java/project/lighthouse/autotests/elements/preLoader/PreLoader.java
@@ -8,7 +8,7 @@ import project.lighthouse.autotests.Waiter;
 public class PreLoader {
 
     private Waiter waiter;
-    private static final String PRE_LOADER_XPATH = "//*[*[contains(@class, 'preloader_stripes')] and *[not(@status='loading']]";
+    private static final String PRE_LOADER_XPATH = "//*[*[contains(@class, 'preloader_stripes')] and *[not(@status='loading')]]";
 
     public PreLoader(WebDriver driver) {
         waiter = new Waiter(driver, StaticData.DEFAULT_PRE_LOADER_TIMEOUT);
