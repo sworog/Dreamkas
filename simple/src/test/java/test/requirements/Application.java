6b801b73c186a4680db25342a379b25d649e303a
Andrey Tolpeev <atolpeev@crystals.ru>
2014-06-04 13:03:50 +0400

    Added simple junit project

---
+++ simple/src/test/java/test/requirements/Application.java
@@ -0,0 +1,11 @@
+package test.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Search {
+        public class SearchByKeyword {}
+        public class SearchByMultipleKeywords {}
+    }
+}
