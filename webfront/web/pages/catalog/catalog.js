fa47698662937838f754c9136ad4576d677d6ef4
Andrew Borovin <a.borovin@crystals.ru>
2014-06-07 17:39:05 +0400

    catalog page

--- webfront/web/pages/catalog/catalog.js
+++ webfront/web/pages/catalog/catalog.js
@@ -1,62 +1,19 @@
-define(function(require) {
+define(function(require, exports, module) {
     //requirements
-    var Page = require('kit/core/page.deprecated'),
-        pageParams = require('pages/catalog/params'),
-        Catalog = require('blocks/catalog/catalog'),
-        СatalogGroupsCollection = require('collections/catalogGroups'),
-        currentUserModel = require('models/currentUser.inst'),
-        Page403 = require('pages/errors/403'),
-        router = require('router');
+    var Page = require('kit/page'),
+        deepExtend = require('kit/deepExtend/deepExtend');
 
     return Page.extend({
-        __name__: 'page_catalog_catalog',
         partials: {
-            '#content': require('tpl!./templates/catalog.html')
+            content: require('rv!./content.html'),
+            localNavigation: require('rv!blocks/localNavigation/localNavigation_catalog.html')
         },
-        initialize: function(params){
-            var page = this;
-
-            if (page.referrer.__name__ && page.referrer.__name__.indexOf('page_catalog') >= 0){
-                _.extend(params, pageParams);
-            } else {
-                pageParams.editMode = params.editMode || pageParams.editMode || 'false'
-            }
-
-            if (currentUserModel.stores.length){
-                pageParams.storeId = currentUserModel.stores.at(0).id;
-            }
-
-            if (!pageParams.storeId && !LH.isAllow('groups')){
-                new Page403();
-                return;
-            }
-
-            if (pageParams.storeId && !LH.isAllow('stores/{store}/groups')){
-                new Page403();
-                return;
-            }
-
-            if (!LH.isAllow('groups', 'POST')) {
-                pageParams.editMode = 'false';
-            }
-
-            router.navigate(document.location.pathname + '?editMode=' + pageParams.editMode + '&storeId=' + pageParams.storeId, {
-                replace: true,
-                trigger: false
-            });
-
-            page.catalogGroupsCollection = new СatalogGroupsCollection([], {
-                storeId: pageParams.storeId
-            });
-
-            $.when(page.catalogGroupsCollection.fetch()).then(function(){
-                page.render();
-                new Catalog({
-                    editMode: pageParams.editMode,
-                    catalogGroupsCollection: page.catalogGroupsCollection,
-                    el: document.getElementById('catalog')
-                });
-            });
+        beforeInit: function(opt) {
+            this.data = deepExtend({
+                params: {
+                    edit: 0
+                }
+            }, opt.data);
         }
     });
 });
