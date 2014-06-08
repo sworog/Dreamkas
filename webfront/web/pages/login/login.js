860695063ab4e1e5423dfb4dd3dfa6eac19f9fd4
Andrew Borovin <a.borovin@crystals.ru>
2014-06-09 00:49:51 +0400

    base classes

--- webfront/web/pages/login/login.js
+++ webfront/web/pages/login/login.js
@@ -1,15 +1,14 @@
 define(function(require, exports, module) {
     //requirements
-    var Page = require('kit/page'),
-        SignupModel = require('models/signup');
+    var Page = require('kit/page');
 
     return Page.extend({
         partials: {
             content: require('tpl!./content.ejs'),
             globalNavigation: require('tpl!blocks/globalNavigation/globalNavigation_login.ejs')
         },
-        components: {
-            'form-login': require('blocks/form/form_login/form_login')
+        blocks: {
+            form_login: require('blocks/form/form_login/form_login')
         }
     });
 });
