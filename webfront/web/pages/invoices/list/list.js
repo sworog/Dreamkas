c13c588ba4c4839a51090ceaffaadaf3f1ffa725
Andrew Borovin <a.borovin@crystals.ru>
2014-06-10 00:37:35 +0400

    store pages

--- webfront/web/pages/invoices/list/list.js
+++ webfront/web/pages/invoices/list/list.js
@@ -1,30 +1,21 @@
 define(function(require, exports, module) {
     //requirements
-    var Page = require('kit/page'),
-        InvoicesCollection = require('collections/invoices');
+    var Page = require('pages/store');
 
     return Page.extend({
         partials: {
-            content: require('rv!./content.html'),
-            localNavigation: require('rv!blocks/localNavigation/localNavigation_invoices.html'),
-            globalNavigation: require('rv!blocks/globalNavigation/globalNavigation_store.html')
+            content: require('tpl!./content.ejs'),
+            localNavigation: require('tpl!blocks/localNavigation/localNavigation_invoices.ejs')
         },
-        resources: {
+        collections: {
             invoices: function() {
                 var page = this,
-                    invoices = new InvoicesCollection();
+                    InvoicesCollection = require('collections/invoices'),
+                    invoicesCollection = new InvoicesCollection();
 
-                invoices.storeId = page.get('params.storeId');
+                invoicesCollection.storeId = page.get('params.storeId');
 
-                return invoices;
-            },
-            store: function() {
-                var page = this,
-                    StoreModel = require('models/store');
-
-                return new StoreModel({
-                    id: page.get('params.storeId')
-                });
+                return invoicesCollection;
             }
         }
     });
--- webfront/web/pages/orders/list/list.js
+++ webfront/web/pages/orders/list/list.js
@@ -1,14 +1,13 @@
 define(function(require, exports, module) {
     //requirements
-    var Page = require('kit/page');
+    var Page = require('pages/store');
 
     return Page.extend({
         partials: {
-            content: require('rv!./content.html'),
-            localNavigation: require('rv!blocks/localNavigation/localNavigation_orders.html'),
-            globalNavigation: require('rv!blocks/globalNavigation/globalNavigation_store.html')
+            content: require('tpl!./content.ejs'),
+            localNavigation: require('tpl!blocks/localNavigation/localNavigation_orders.ejs')
         },
-        resources: {
+        collections: {
             orders: function(){
                 var page = this,
                     OrdersCollection = require('collections/orders'),
@@ -17,14 +16,6 @@ define(function(require, exports, module) {
                 ordersCollection.storeId = page.get('params.storeId');
 
                 return ordersCollection;
-            },
-            store: function() {
-                var page = this,
-                    StoreModel = require('models/store');
-
-                return new StoreModel({
-                    id: page.get('params.storeId')
-                });
             }
         }
     });
