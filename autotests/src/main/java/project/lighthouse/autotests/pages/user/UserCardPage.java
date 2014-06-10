42bc1c0bec8d600e6cb381602db79dbd83e351f6
Andrey Tolpeev <atolpeev@crystals.ru>
2014-06-10 16:13:21 +0400

    Added us-77 story UserEditPage object, step/jbehave required methods implementation

--- autotests/src/main/java/project/lighthouse/autotests/pages/user/UserCardPage.java
+++ autotests/src/main/java/project/lighthouse/autotests/pages/user/UserCardPage.java
@@ -4,14 +4,8 @@ import net.thucydides.core.annotations.findby.FindBy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import project.lighthouse.autotests.common.CommonPageObject;
-import project.lighthouse.autotests.elements.Buttons.ButtonFacade;
-import project.lighthouse.autotests.elements.items.NonType;
 
 public class UserCardPage extends CommonPageObject {
-
-    @FindBy(className = "user__editLink")
-    @SuppressWarnings("unused")
-    private WebElement editButtonLinkWEbWebElement;
 
     @FindBy(className = "page__backLink")
     @SuppressWarnings("unused")
@@ -23,22 +17,11 @@ public class UserCardPage extends CommonPageObject {
 
     @Override
     public void createElements() {
-        put("name", new NonType(this, "name"));
-        put("position", new NonType(this, "position"));
-        put("username", new NonType(this, "username"));
-        put("password", new NonType(this, "password"));
-        put("role", new NonType(this, "role"));
-    }
-
-    public void editButtonClick() {
-        findVisibleElement(editButtonLinkWEbWebElement).click();
+        put("name");
+        put("email");
     }
 
     public void pageBackLinkClick() {
         findVisibleElement(pageBackLinkWebElement).click();
-    }
-
-    public void logOutButtonClick() {
-        new ButtonFacade(this, "Выйти").click();
     }
 }
