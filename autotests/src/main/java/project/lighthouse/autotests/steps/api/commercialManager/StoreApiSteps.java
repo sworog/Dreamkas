7d5a529cf403c6da4d54e4936d3d546c827d3e10
Andrey Tolpeev <atolpeev@crystals.ru>
2014-06-20 15:52:02 +0400

    Added UserContainer class and list for storing created user data via symfony:user:create cap command

--- autotests/src/main/java/project/lighthouse/autotests/steps/api/commercialManager/StoreApiSteps.java
+++ autotests/src/main/java/project/lighthouse/autotests/steps/api/commercialManager/StoreApiSteps.java
@@ -2,8 +2,10 @@ package project.lighthouse.autotests.steps.api.commercialManager;
 
 import net.thucydides.core.annotations.Step;
 import org.json.JSONException;
+import project.lighthouse.autotests.api.factories.StoreFactory;
 import project.lighthouse.autotests.objects.api.Store;
 import project.lighthouse.autotests.storage.Storage;
+import project.lighthouse.autotests.storage.containers.user.UserContainer;
 
 import java.io.IOException;
 
@@ -15,7 +17,9 @@ public class StoreApiSteps extends OwnerApi {
     }
 
     @Step
-    public Store createStoreThroughPost(String number, String address, String contacts) throws IOException, JSONException {
+    public Store createStoreThroughPost(String number,
+                                        String address,
+                                        String contacts) throws IOException, JSONException {
         Store store = new Store(number, address, contacts);
         store = apiConnect.createStoreThroughPost(store);
 
@@ -27,5 +31,18 @@ public class StoreApiSteps extends OwnerApi {
     @Step
     public String getStoreId(String storeNumber) throws JSONException {
         return apiConnect.getStoreId(storeNumber);
+    }
+
+    @Step
+    public Store createStoreThroughPostByUserWithEmail(String number,
+                                                       String address,
+                                                       String contacts,
+                                                       String email) throws JSONException, IOException {
+        Store store = new Store(number, address, contacts);
+        UserContainer userContainer = Storage.getUserVariableStorage().getUserContainers().getContainer(email);
+        store = new StoreFactory(userContainer.getEmail(), userContainer.getPassword()).create(store);
+        Storage.getStoreVariableStorage().setStore(store);
+        userContainer.setStore(store);
+        return store;
     }
 }
