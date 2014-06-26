5395c74154818a3161ef967b3f46d815801ad645
Mikhail Shamin <mshamin@crystals.ru>
2014-06-26 17:17:40 +0400

    Refactored some type classes to use Lighthouse AbstractType

--- backend/src/Lighthouse/CoreBundle/Form/ConfigType.php
+++ backend/src/Lighthouse/CoreBundle/Form/ConfigType.php
@@ -3,34 +3,27 @@
 namespace Lighthouse\CoreBundle\Form;
 
 use Lighthouse\CoreBundle\Document\Config\Config;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class ConfigType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name', 'text')
-            ->add('value', 'text');
-    }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(
-            array(
-                'data_class' => Config::getClassName(),
-                'csrf_protection' => false
-            )
-        );
+            ->add('value', 'text')
+        ;
     }
 
     /**
-     * @return string The name of this type
+     * @return string
      */
-    public function getName()
+    protected function getDataClass()
     {
-        return '';
+        return Config::getClassName();
     }
 }
--- backend/src/Lighthouse/CoreBundle/Form/DepartmentType.php
+++ backend/src/Lighthouse/CoreBundle/Form/DepartmentType.php
@@ -4,9 +4,7 @@ namespace Lighthouse\CoreBundle\Form;
 
 use Lighthouse\CoreBundle\Document\Department\Department;
 use Lighthouse\CoreBundle\Document\Store\Store;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class DepartmentType extends AbstractType
 {
@@ -26,29 +24,15 @@ class DepartmentType extends AbstractType
                     'class' => Store::getClassName(),
                     'invalid_message' => 'lighthouse.validation.errors.department.store.does_not_exists'
                 )
-            );
-    }
-
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(
-            array(
-                'data_class' => Department::getClassName(),
-                'csrf_protection' => false
             )
-        );
+        ;
     }
 
     /**
-     * Returns the name of this type.
-     *
-     * @return string The name of this type
+     * @return string
      */
-    public function getName()
+    protected function getDataClass()
     {
-        return '';
+        return Department::getClassName();
     }
 }
