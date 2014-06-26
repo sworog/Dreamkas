ed66014a4a441a66cc6e2a5df59d92305436f9df
Andrew Borovin <a.borovin@crystals.ru>
2014-06-26 15:12:11 +0400

    update momentjs

--- webfront/web/blocks/datepicker/datepicker.js
+++ webfront/web/blocks/datepicker/datepicker.js
@@ -1,56 +1,47 @@
 define(function(require) {
         //requirements
-        var Block = require('kit/core/block.deprecated'),
+        var Block = require('kit/block'),
+            $ = require('jquery'),
             moment = require('moment');
 
         return Block.extend({
-            __name__: 'datepicker',
-            className: 'datepicker',
-            visibleDate: moment().valueOf(),
+            visibleDate: null,
             selectedDate: null,
-            noTime: false,
-            dateList: [],
-            template: require('ejs!blocks/datepicker/templates/index.html'),
-            templates: {
-                index: require('ejs!blocks/datepicker/templates/index.html'),
-                controls: require('ejs!blocks/datepicker/templates/controls.html'),
-                dateList: require('ejs!blocks/datepicker/templates/dateList.html'),
-                header: require('ejs!blocks/datepicker/templates/header.html')
-            },
 
-            initialize: function() {
-                var block = this;
+            template: require('ejs!./template.ejs'),
 
-                if (!block.dateList.length) {
-                    block._generateDateList();
+            dateList: function() {
+                var block = this,
+                    dateList = [],
+                    visibleMoment = moment(block.visibleDate || moment().valueOf()),
+                    startMoment = moment(visibleMoment).startOf('month').startOf('week'),
+                    endMoment = moment(visibleMoment).endOf('month').endOf('week'),
+                    diff,
+                    itemMoment;
+
+                diff = endMoment.diff(startMoment, 'days');
+
+                for (var i = 0; i <= diff; i++) {
+                    itemMoment = moment(startMoment).add('days', i);
+
+                    dateList.push({
+                        moment: itemMoment,
+                        isOtherMonth: !visibleMoment.isSame(itemMoment, 'month'),
+                        isNow: moment().isSame(itemMoment, 'day'),
+                        isSelected: block.selectedDate ? moment(block.selectedDate).isSame(itemMoment, 'day') : false
+                    });
                 }
 
-                block.$el.toggleClass('datepicker_noTime', block.noTime);
-
-                block.render();
+                return dateList;
             },
-
+            
             events: {
-                'click .datepicker__saveLink': function(e){
+                'click .datepicker__setNowLink': function(e) {
                     e.preventDefault();
 
                     var block = this;
 
-                    block.trigger('save');
-                },
-                'click .datepicker__closeLink': function(e){
-                    e.preventDefault();
-
-                    var block = this;
-
-                    block.trigger('close');
-                },
-                'click .datepicker__setNowLink': function(e){
-                    e.preventDefault();
-
-                    var block = this;
-
-                    block.set('selectedDate', moment().valueOf());
+                    block.selectDate(moment().valueOf());
                 },
                 'click .datepicker__nextMonthLink': function(e) {
                     e.preventDefault();
@@ -77,136 +68,52 @@ define(function(require) {
                     e.preventDefault();
 
                     var block = this,
-                        selectedMoment = moment(+$(e.target).data('date'));
+                        selectedMoment = moment(+e.target.dataset.date);
 
-                    if (!block.noTime){
-                        block.$el.find('.datepicker__timeControls .inputText').each(function(){
-                            selectedMoment[$(this).attr('name')]($(this).val() || 0);
-                        });
-                    }
-
-                    block.set('selectedDate', selectedMoment.valueOf());
-                },
-                'change .datepicker__timeControls .inputText': function(e) {
-                    e.preventDefault();
-
-                    var block = this,
-                        date = moment(block.selectedDate)[$(e.target).attr('name')]($(e.target).val()).valueOf();
-
-                    block.set('selectedDate', date, {
-                        renderTimeControls: false
-                    });
-                }
-            },
-            'set:noTime': function(val){
-                var block = this;
-
-                block.$el.toggleClass('datepicker_noTime', val);
-            },
-            'set:lang': function(val){
-                var block = this;
-
-                block.moment.lang(val);
-                block.render();
-            },
-            'set:visibleDate': function(value) {
-                var block = this;
-
-                block.visibleDate = value;
-
-                block
-                    ._generateDateList()
-                    ._renderHeader()
-                    ._renderDateList();
-            },
-            'set:selectedDate': function(value, extra) {
-                var block = this;
-
-                if (!value){
-                    block._clearSelectedDate();
-                    return;
-                }
-
-                extra = _.extend({
-                    renderTimeControls: !block.noTime
-                }, extra);
-
-                block.$el.find('.datepicker__timeControls .inputText').removeAttr('disabled');
-                block.$el.find('.datepicker__dateItem_selected').removeClass('datepicker__dateItem_selected');
-                block.$el.find('.datepicker__dateItem[data-date="' + moment(value).startOf('day').valueOf() + '"]').addClass('datepicker__dateItem_selected');
-
-                if (extra.renderTimeControls){
-                    block.$el.find('.datepicker__timeControls [name="hours"]').val(moment(value).format('HH'));
-                    block.$el.find('.datepicker__timeControls [name="minutes"]').val(moment(value).format('mm'));
-                    block.$el.find('.datepicker__timeControls [name="seconds"]').val(moment(value).format('ss'));
+                    block.selectDate(selectedMoment.valueOf());
                 }
             },
             showNextMonth: function() {
                 var block = this;
 
-                block.set('visibleDate', moment(block.visibleDate).add('months', 1).valueOf());
+                block.showDate(moment(block.visibleDate).add('months', 1).valueOf());
             },
             showPrevMonth: function() {
                 var block = this;
 
-                block.set('visibleDate', moment(block.visibleDate).subtract('months', 1).valueOf());
+                block.showDate(moment(block.visibleDate).subtract('months', 1).valueOf());
             },
             showNow: function() {
                 var block = this;
 
-                block.set('visibleDate', moment().valueOf());
+                block.showDate(moment().valueOf());
             },
-            _clearSelectedDate: function(){
+            showDate: function(date){
                 var block = this;
+                
+                block.visibleDate = date;
 
-                block.$el.find('.datepicker__timeControls .inputText').val('').attr('disabled', true);
-                block.$el.find('.datepicker__dateItem_selected').removeClass('datepicker__dateItem_selected');
+                this.render();
             },
-            _generateDateList: function() {
-                var block = this,
-                    visibleMoment = moment(block.visibleDate),
-                    startMoment = moment(block.visibleDate).startOf('month').startOf('week').day(1),
-                    endMoment = moment(block.visibleDate).endOf('month').endOf('week').day(7),
-                    diff,
-                    itemMoment;
+            selectDate: function(date){
+                var block = this;
+                
+                block.selectedDate = date;
 
-                if (endMoment.date() == 7){
-                    endMoment.day(-7);
+                if (!date) {
+                    block._clearSelectedDate();
+                    return;
                 }
 
-                if (startMoment.date() == 2){
-                    startMoment.day(-6);
-                }
+                $(block.el).find('.datepicker__dateItem_selected').removeClass('datepicker__dateItem_selected');
+                $(block.el).find('.datepicker__dateItem[data-date="' + moment(date).startOf('day').valueOf() + '"]').addClass('datepicker__dateItem_selected');
 
-                diff = endMoment.diff(startMoment, 'days');
-                block.dateList = [];
-
-                for (var i = 0; i <= diff; i++) {
-                    itemMoment = moment(startMoment).add('days', i);
-
-                    block.dateList.push({
-                        moment: itemMoment,
-                        isOtherMonth: !visibleMoment.isSame(itemMoment, 'month'),
-                        isNow: moment().isSame(itemMoment, 'day'),
-                        isSelected: block.selectedDate ? moment(block.selectedDate).isSame(itemMoment, 'day') : false
-                    });
-                }
-
-                return block;
+                block.trigger('selectdate', date);
             },
-            _renderDateList: function() {
+            _clearSelectedDate: function() {
                 var block = this;
 
-                block.$dateList.html(block.templates.dateList(block));
-
-                return block;
-            },
-            _renderHeader: function() {
-                var block = this;
-
-                block.$header.html(block.templates.header(block));
-
-                return block;
+                block.el.querySelector('.datepicker__dateItem_selected').classList.remove('datepicker__dateItem_selected');
             }
         });
     }
