a322b27e5dc4e4a59c394d99304b6a8a497c7a05
Mikhail Shamin <mshamin@crystals.ru>
2014-07-25 14:13:57 +0400

    Sort StockMovements by date desc by default

--- backend/src/Lighthouse/CoreBundle/Document/StockMovement/StockMovementRepository.php
+++ backend/src/Lighthouse/CoreBundle/Document/StockMovement/StockMovementRepository.php
@@ -2,8 +2,16 @@
 
 namespace Lighthouse\CoreBundle\Document\StockMovement;
 
+use Doctrine\ODM\MongoDB\Cursor;
 use Lighthouse\CoreBundle\Document\DocumentRepository;
 
 class StockMovementRepository extends DocumentRepository
 {
+    /**
+     * @return Cursor|StockMovement[]
+     */
+    public function findAll()
+    {
+        return $this->findBy(array(), array('date' => self::SORT_DESC));
+    }
 }
