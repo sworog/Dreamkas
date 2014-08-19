493e63f2c5880d6656ed696d7646e8505552b94d
Andrew Borovin <a.borovin@crystals.ru>
2014-08-19 14:42:24 +0400

    fix quantity fields

--- webfront/web/kit/normalizeNumber/normalizeNumber.js
+++ webfront/web/kit/normalizeNumber/normalizeNumber.js
@@ -1,8 +1,15 @@
 define(function(require, exports, module) {
     //requirements
+    var _ = require('lodash');
+
     return function(string) {
-        return parseFloat((string || '').toString()
+
+        var normalizedString = (string || '').toString()
             .replace(' ', '', 'gi')
-            .replace(',', '.', 'gi'));
+            .replace(',', '.', 'gi');
+
+        var number = parseFloat(normalizedString);
+
+        return _.isNaN(number) ? '' : number;
     }
 });
