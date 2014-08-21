b2a3a71ada7a8338607853b2f69042cc09983b8a
Andrey Tolpeev <atolpeev@crystals.ru>
2014-08-21 14:02:47 +0400

    Added stockIn create/edit page objects classes

---
+++ autotests/src/main/java/project/lighthouse/autotests/pages/stockMovement/modal/stockIn/StockInCreateModalWindow.java
@@ -0,0 +1,22 @@
+package project.lighthouse.autotests.pages.stockMovement.modal.stockIn;
+
+import org.openqa.selenium.WebDriver;
+import project.lighthouse.autotests.elements.bootstrap.buttons.PrimaryBtnFacade;
+import project.lighthouse.autotests.pages.stockMovement.modal.writeOff.WriteOffCreateModalWindow;
+
+public class StockInCreateModalWindow extends WriteOffCreateModalWindow {
+
+    public StockInCreateModalWindow(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public String modalWindowXpath() {
+        return "//*[@id='modal_stockInAdd']";
+    }
+
+    @Override
+    public void confirmationOkClick() {
+        new PrimaryBtnFacade(this, "Оприходывать").click();
+    }
+}
---
+++ autotests/src/main/java/project/lighthouse/autotests/pages/stockMovement/modal/stockIn/StockInEditModalWindow.java
@@ -0,0 +1,22 @@
+package project.lighthouse.autotests.pages.stockMovement.modal.stockIn;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import project.lighthouse.autotests.pages.stockMovement.modal.writeOff.WriteOffEditModalWindow;
+
+public class StockInEditModalWindow extends WriteOffEditModalWindow {
+
+    public StockInEditModalWindow(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public String modalWindowXpath() {
+        return "//*[@id='modal_stockInEdit']";
+    }
+
+    @Override
+    public void confirmDeleteButtonClick() {
+        findVisibleElement(By.xpath(modalWindowXpath() + "//*[@class='confirmLink__confirmation']//*[@class='removeLink stockIn__removeLink']")).click();
+    }
+}
