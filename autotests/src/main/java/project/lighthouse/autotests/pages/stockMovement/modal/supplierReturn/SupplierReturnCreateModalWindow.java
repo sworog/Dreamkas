84ac2f9b07b5d2de280f9eb57e80ba697dd18cf4
Mikhail Shamin <munk13@gmail.com>
2014-08-26 15:38:44 +0400

    Supplier Return web objects and pages

---
+++ autotests/src/main/java/project/lighthouse/autotests/pages/stockMovement/modal/supplierReturn/SupplierReturnCreateModalWindow.java
@@ -0,0 +1,39 @@
+package project.lighthouse.autotests.pages.stockMovement.modal.supplierReturn;
+
+import org.openqa.selenium.WebDriver;
+import project.lighthouse.autotests.elements.items.SelectByVisibleText;
+import project.lighthouse.autotests.objects.web.stockMovement.supplierReturn.SupplierReturnProductCollection;
+import project.lighthouse.autotests.pages.stockMovement.modal.StockMovementModalPage;
+
+public class SupplierReturnCreateModalWindow extends StockMovementModalPage {
+
+    public SupplierReturnCreateModalWindow(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void createElements() {
+        super.createElements();
+        put("supplier", new SelectByVisibleText(this, "//*[@name='supplier']"));
+    }
+
+    @Override
+    public String modalWindowXpath() {
+        return "//*[contains(@class, 'modal_supplierReturn')]";
+    }
+
+    @Override
+    public SupplierReturnProductCollection getProductCollection() {
+        return new SupplierReturnProductCollection(getDriver());
+    }
+
+    @Override
+    public void addProductButtonClick() {
+        addProductButtonClick("addSupplierReturnProduct");
+    }
+
+    @Override
+    public Integer getProductRowsCount() {
+        return getProductRowsCount("table_supplierReturnProducts");
+    }
+}
