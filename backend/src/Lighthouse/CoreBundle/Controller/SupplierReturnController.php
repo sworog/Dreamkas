07f2e2eae596818908dd2ac855d12f5f117c31c8
Fedot <vfedorov@crystals.ru>
2014-08-26 15:56:47 +0400

    Added supplierReturn backend

---
+++ backend/src/Lighthouse/CoreBundle/Controller/SupplierReturnController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Controller;
+
+use Lighthouse\CoreBundle\Document\Store\Store;
+use Lighthouse\CoreBundle\Document\StockMovement\SupplierReturn\SupplierReturn;
+use Lighthouse\CoreBundle\Document\StockMovement\SupplierReturn\SupplierReturnRepository;
+use Lighthouse\CoreBundle\Form\SupplierReturnType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use JMS\DiExtraBundle\Annotation as DI;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+use JMS\SecurityExtraBundle\Annotation\SecureParam;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SupplierReturnController extends AbstractRestController
+{
+    /**
+     * @DI\Inject("lighthouse.core.document.repository.stock_movement.supplierreturn")
+     * @var SupplierReturnRepository
+     */
+    protected $documentRepository;
+
+    /**
+     * @return SupplierReturnType|FormInterface
+     */
+    protected function getDocumentFormType()
+    {
+        return new SupplierReturnType();
+    }
+
+    /**
+     * @param Request $request
+     * @return FormInterface|SupplierReturn
+     *
+     * @Rest\Route("supplierReturns")
+     * @Rest\View(statusCode=201)
+     * @Secure(roles="ROLE_COMMERCIAL_MANAGER")
+     * @ApiDoc(resource=true)
+     */
+    public function postSupplierReturnsAction(Request $request)
+    {
+        $supplierReturn = $this->documentRepository->createNew();
+        $formType = new SupplierReturnType(true);
+        return $this->processForm($request, $supplierReturn, $formType);
+    }
+
+    /**
+     * @param Store $store
+     * @param Request $request
+     * @return FormInterface|SupplierReturn
+     *
+     * @Rest\Route("stores/{store}/supplierReturns")
+     * @Rest\View(statusCode=201)
+     * @SecureParam(name="store", permissions="ACL_DEPARTMENT_MANAGER")
+     * @ApiDoc
+     */
+    public function postStoreSupplierReturnsAction(Store $store, Request $request)
+    {
+        $supplierReturn = $this->documentRepository->createNew();
+        $supplierReturn->store = $store;
+        return $this->processForm($request, $supplierReturn);
+    }
+
+    /**
+     * @param SupplierReturn $supplierReturn
+     * @param Request $request
+     * @return FormInterface|SupplierReturn
+     *
+     * @Rest\Route("supplierReturns/{supplierReturn}")
+     * @Secure(roles="ROLE_COMMERCIAL_MANAGER")
+     * @ApiDoc
+     */
+    public function putSupplierReturnsAction(SupplierReturn $supplierReturn, Request $request)
+    {
+        $formType = new SupplierReturnType(true);
+        $this->documentRepository->resetProducts($supplierReturn);
+        return $this->processForm($request, $supplierReturn, $formType);
+    }
+
+    /**
+     * @param Store $store
+     * @param SupplierReturn $supplierReturn
+     * @param Request $request
+     * @return FormInterface|SupplierReturn
+     *
+     * @Rest\Route("stores/{store}/supplierReturns/{supplierReturn}")
+     * @SecureParam(name="store", permissions="ACL_DEPARTMENT_MANAGER")
+     * @ApiDoc
+     */
+    public function putStoreSupplierReturnsAction(Store $store, SupplierReturn $supplierReturn, Request $request)
+    {
+        $this->checkSupplierReturnStore($store, $supplierReturn);
+        $this->documentRepository->resetProducts($supplierReturn);
+        return $this->processForm($request, $supplierReturn);
+    }
+
+    /**
+     * @param SupplierReturn $supplierReturn
+     *
+     * @Rest\Route("supplierReturns/{supplierReturn}")
+     * @Secure(roles="ROLE_COMMERCIAL_MANAGER")
+     * @ApiDoc
+     */
+    public function deleteSupplierReturnsAction(SupplierReturn $supplierReturn)
+    {
+        $this->processDelete($supplierReturn);
+    }
+
+    /**
+     * @param SupplierReturn $supplierReturn
+     * @return SupplierReturn
+     *
+     * @Rest\Route("supplierReturns/{supplierReturn}")
+     * @Rest\View(serializerEnableMaxDepthChecks=true)
+     * @Secure(roles="ROLE_COMMERCIAL_MANAGER")
+     * @ApiDoc
+     */
+    public function getSupplierReturnAction(SupplierReturn $supplierReturn)
+    {
+        return $supplierReturn;
+    }
+
+    /**
+     * @param Store $store
+     * @param SupplierReturn $supplierReturn
+     * @return SupplierReturn
+     *
+     * @Rest\Route("stores/{store}/supplierReturns/{supplierReturn}")
+     * @Rest\View(serializerEnableMaxDepthChecks=true)
+     * @SecureParam(name="store", permissions="ACL_DEPARTMENT_MANAGER")
+     * @ApiDoc
+     */
+    public function getStoreSupplierReturnAction(Store $store, SupplierReturn $supplierReturn)
+    {
+        $this->checkSupplierReturnStore($store, $supplierReturn);
+        return $supplierReturn;
+    }
+
+    /**
+     * @param Store $store
+     * @param SupplierReturn $supplierReturn
+     * @throws NotFoundHttpException
+     */
+    protected function checkSupplierReturnStore(Store $store, SupplierReturn $supplierReturn)
+    {
+        if ($supplierReturn->store !== $store) {
+            throw new NotFoundHttpException(sprintf("%s object not found", get_class($supplierReturn)));
+        }
+    }
+}
---
+++ backend/src/Lighthouse/CoreBundle/Document/StockMovement/SupplierReturn/SupplierReturn.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Lighthouse\CoreBundle\Document\StockMovement\SupplierReturn;
+
+use Lighthouse\CoreBundle\Document\StockMovement\StockMovement;
+use Lighthouse\CoreBundle\Document\StockMovement\SupplierReturn\Product\SupplierReturnProduct;
+use Lighthouse\CoreBundle\Document\Supplier\Supplier;
+use Lighthouse\CoreBundle\MongoDB\Generated\Generated;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Validator\Constraints as Assert;
+use JMS\Serializer\Annotation as Serializer;
+use Lighthouse\CoreBundle\Validator\Constraints as AssertLH;
+
+/**
+ * @property string $number
+ * @property bool   $paid
+ * @property Supplier $supplier
+ * @property SupplierReturnProduct[]|Collection $products
+ *
+ * @MongoDB\Document(
+ *      repositoryClass="Lighthouse\CoreBundle\Document\StockMovement\SupplierReturn\SupplierReturnRepository"
+ * )
+ */
+class SupplierReturn extends StockMovement
+{
+    const TYPE = 'SupplierReturn';
+
+    /**
+     * @Generated(startValue=10000)
+     * @var int
+     */
+    protected $number;
+
+    /**
+     * Поставщик
+     * @MongoDB\ReferenceOne(
+     *     targetDocument="Lighthouse\CoreBundle\Document\Supplier\Supplier",
+     *     simple=true
+     * )
+     * Assert\NotBlank(message="lighthouse.validation.errors.invoice.supplier.empty")
+     * @AssertLH\Reference(message="lighthouse.validation.errors.invoice.supplier.does_not_exists")
+     * @var Supplier
+     */
+    protected $supplier;
+
+    /**
+     * @MongoDB\Boolean
+     * @var bool
+     */
+    protected $paid;
+
+    /**
+     * @MongoDB\ReferenceMany(
+     *      targetDocument="Lighthouse\CoreBundle\Document\StockMovement\SupplierReturn\Product\SupplierReturnProduct",
+     *      simple=true,
+     *      cascade={"persist","remove"},
+     *      mappedBy="supplierReturn"
+     * )
+     * @Assert\Valid(traverse=true)
+     * @Assert\Count(
+     *      min=1,
+     *      minMessage="lighthouse.validation.errors.stock_movement.products.empty"
+     * )
+     * @Serializer\MaxDepth(4)
+     * @var SupplierReturnProduct[]|Collection
+     */
+    protected $products;
+
+    /**
+     * @param SupplierReturnProduct[] $products
+     */
+    public function setProducts($products)
+    {
+        foreach ($products as $product) {
+            $product->setReasonParent($this);
+        }
+
+        $this->products = $products;
+    }
+}
