283f2fda68e22edc8aa57b41fcadfd300ece3194
Mikhail Shamin <mshamin@crystals.ru>
2014-08-28 19:56:13 +0400

    Added teamcity reporter

--- webfront/web/test-remote.sh
+++ webfront/web/test-remote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 hostname=`hostname`
-node ./node_modules/karma/bin/karma start karma.remote.js --single-run --hostname="$hostname"
+node ./node_modules/karma/bin/karma start karma.remote.js --single-run --reporters=teamcity --hostname="$hostname"
