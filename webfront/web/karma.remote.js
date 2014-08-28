f10fd8e7435150885993f5b59a8c9346652e0d2d
Mikhail Shamin <mshamin@crystals.ru>
2014-08-28 19:23:23 +0400

    Karma webdriver

---
+++ webfront/web/karma.remote.js
@@ -0,0 +1,103 @@
+// Karma configuration
+// Generated on Fri Sep 27 2013 16:37:01 GMT+0400 (MSK)
+
+module.exports = function(config) {
+
+    var webDriverConfig = {
+        hostname: 'selenium.lighthouse.pro/wd/hub',
+        port: 80
+    };
+
+    config.set({
+
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
+
+        // frameworks to use
+        frameworks: ['jasmine', 'requirejs'],
+
+        // list of files / patterns to load in the browser
+        files: [
+            'karma.main.js',
+            'karma.remote.js',
+            {pattern: '**/*.js', included: false},
+            {pattern: '**/*.ejs', included: false}
+        ],
+
+        // list of files to exclude
+        exclude: [
+            'karma.conf.js',
+            'node_modules/**/*.spec.js',
+            'bower_components/**/*.spec.js'
+        ],
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['dots'],
+
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul),
+            //'requireDom.js': 'coverage'
+        },
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+        customLaunchers: {
+            'RemoteChrome': {
+                base: 'WebDriver',
+                config: webDriverConfig,
+                browserName: 'chrome'
+            },
+            'RemoteFirefox': {
+                base: 'WebDriver',
+                config: webDriverConfig,
+                browserName: 'firefox'
+            },
+            'Remote-Firefox': {
+                base: 'WebdriverJS',
+                config: {
+                    host: 'selenium.lighthouse.pro',
+                    port: 80,
+                    desiredCapabilities: {
+                        browserName: 'firefox'
+                    }
+                }
+            }
+        },
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Remote-Firefox'],
+
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: true
+    });
+};
