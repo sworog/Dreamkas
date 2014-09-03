5a2a4b18b4fe56ce7d1307bbf221872684f3acde
Andrey Tolpeev <atolpeev@crystals.ru>
2014-09-03 19:20:49 +0400

    Removed oldie deprecated catalog ApiConnect methods

--- autotests/src/main/java/project/lighthouse/autotests/StaticData.java
+++ autotests/src/main/java/project/lighthouse/autotests/StaticData.java
@@ -1,7 +1,5 @@
 package project.lighthouse.autotests;
 
-import project.lighthouse.autotests.objects.api.Category;
-import project.lighthouse.autotests.objects.api.Group;
 import project.lighthouse.autotests.objects.api.Product;
 import project.lighthouse.autotests.objects.api.SubCategory;
 import project.lighthouse.autotests.storage.Storage;
@@ -11,22 +9,14 @@ import java.util.HashMap;
 
 public class StaticData {
 
-    public static HashMap<String, Group> groups = new HashMap<>();
-    public static HashMap<String, Category> categories = new HashMap<>();
     public static HashMap<String, SubCategory> subCategories = new HashMap<>();
     public static HashMap<String, ArrayList<Product>> subCategoryProducts = new HashMap<>();
-
-    public static Boolean isGroupCreated(String groupName) {
-        return groups.containsKey(groupName);
-    }
 
     public static Boolean hasSubCategory(String subCategoryName) {
         return subCategories.containsKey(subCategoryName);
     }
 
     public static void clear() {
-        groups.clear();
-        categories.clear();
         subCategories.clear();
         subCategoryProducts.clear();
         Storage.getOrderVariableStorage().resetNumber();
