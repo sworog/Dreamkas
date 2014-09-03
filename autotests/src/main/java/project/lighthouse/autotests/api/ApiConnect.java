d8f57d535a8f1a68b151b89d56f6d57a40261355
Andrey Tolpeev <atolpeev@crystals.ru>
2014-09-03 19:26:51 +0400

    Removed deprecate ApiConnect setSet10ImportUrl method

--- autotests/src/main/java/project/lighthouse/autotests/api/ApiConnect.java
+++ autotests/src/main/java/project/lighthouse/autotests/api/ApiConnect.java
@@ -1,11 +1,9 @@
 package project.lighthouse.autotests.api;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import project.lighthouse.autotests.StaticData;
 import project.lighthouse.autotests.api.http.HttpExecutor;
 import project.lighthouse.autotests.api.http.HttpRequestable;
-import project.lighthouse.autotests.helper.UrlHelper;
 import project.lighthouse.autotests.objects.api.SubCategory;
 
 import java.io.IOException;
@@ -26,13 +24,5 @@ public class ApiConnect {
         } else {
             return StaticData.subCategories.get(subCategory.getName());
         }
-    }
-
-    public String setSet10ImportUrl(String value) throws IOException, JSONException {
-        JSONObject jsonObject = new JSONObject(httpRequestable.executeGetRequest(UrlHelper.getApiUrl("/configs/by/name?query=set10-import-url")));
-        String targetUrl = UrlHelper.getApiUrl("/configs/" + jsonObject.getString("id"));
-        return httpRequestable.executePutRequest(targetUrl, new JSONObject()
-                .put("name", "set10-import-url")
-                .put("value", value));
     }
 }
