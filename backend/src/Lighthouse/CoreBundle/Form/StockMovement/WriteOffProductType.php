6f65d7f9c16ffee41b1682133f72a5d93ffb7d0d
Mikhail Shamin <mshamin@crystals.ru>
2014-09-05 17:24:36 +0400

    Fixed WriteOffProductType cause was accedently deleted

--- backend/src/Lighthouse/CoreBundle/Form/StockMovement/WriteOffProductType.php
+++ backend/src/Lighthouse/CoreBundle/Form/StockMovement/WriteOffProductType.php
@@ -3,9 +3,21 @@
 namespace Lighthouse\CoreBundle\Form\StockMovement;
 
 use Lighthouse\CoreBundle\Document\StockMovement\WriteOff\Product\WriteOffProduct;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class WriteOffProductType extends StockMovementProductType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('cause', 'text');
+    }
+
     /**
      * @return string
      */
