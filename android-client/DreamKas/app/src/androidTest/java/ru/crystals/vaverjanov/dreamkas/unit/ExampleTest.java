cac304e0f6e53afe8c6d311c68a4df208c8aaa75
Stabilitron <stabilitron@gmail.com>
2014-09-08 19:08:12 +0400

    edit gradle config

--- android-client/DreamKas/app/src/androidTest/java/ru/crystals/vaverjanov/dreamkas/unit/ExampleTest.java
+++ android-client/DreamKas/app/src/androidTest/java/ru/crystals/vaverjanov/dreamkas/unit/ExampleTest.java
@@ -7,7 +7,7 @@ public class ExampleTest extends InstrumentationTestCase
     public void testFalse() throws Exception
     {
         final int expected = 1;
-        final int reality = 1;
+        final int reality = 5;
         assertEquals(expected, reality);
     }
 }
