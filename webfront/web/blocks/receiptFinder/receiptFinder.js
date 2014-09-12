40d416e1f66951b8dc727df8cb6c3659dc4b93a7
denisraslov <denis.raslov@yandex.ru>
2014-09-12 17:05:55 +0400

    added date range filter in refund page

--- webfront/web/blocks/receiptFinder/receiptFinder.js
+++ webfront/web/blocks/receiptFinder/receiptFinder.js
@@ -1,6 +1,8 @@
 define(function(require, exports, module) {
 	//requirements
-	var Block = require('kit/block/block');
+	var Block = require('kit/block/block'),
+		moment = require('moment'),
+		formatDate = require('kit/formatDate/formatDate');
 
 	return Block.extend({
 		template: require('ejs!./template.ejs'),
@@ -8,7 +10,16 @@ define(function(require, exports, module) {
 			receipts: require('collections/receipts/receipts')
 		},
 		blocks: {
+			inputDateRange: require('blocks/inputDateRange/inputDateRange'),
 			receiptFinder__results: require('./receiptFinder__results')
+		},
+		dateRange: function() {
+			var currentTime = Date.now();
+
+			return {
+				dateFrom: moment(currentTime).subtract(1, 'week'),
+				dateTo: formatDate(currentTime)
+			}
 		}
 	});
 });
