7a5555153984d081614de893544c1a869e109ac1
Andrew Borovin <a.borovin@crystals.ru>
2014-09-15 19:18:37 +0400

    fix pos receipt form

--- webfront/web/pages/pos/part/store/store.js
+++ webfront/web/pages/pos/part/store/store.js
@@ -1,19 +1,27 @@
 define(function(require, exports, module) {
     //requirements
-    var PosPart = require('pages/pos/part/part');
+    var PosPart = require('pages/pos/part/part'),
+        cookies = require('cookies');
 
     return PosPart.extend({
 		title: 'Касса',
         content: require('ejs!./content.ejs'),
 		activeNavigationItem: 'store',
+        initialize: function(){
+            var page = this;
+
+            cookies.set('posStoreId', page.params.storeId);
+
+            PosPart.prototype.initialize.apply(page, arguments);
+        },
         models: {
-            store: PosPart.prototype.models.store,
             receipt: require('models/receipt/receipt')
         },
         blocks: {
             productFinder: require('blocks/productFinder/productFinder'),
-            form_receipt: require('blocks/form/receipt/receipt'),
-            modal_receiptProduct: require('blocks/modal/receiptProduct/receiptProduct')
+            receipt: require('blocks/receipt/receipt'),
+            modal_receiptProduct: require('blocks/modal/receiptProduct/receiptProduct'),
+            modal_receipt: require('blocks/modal/receipt/receipt')
         }
     });
 });
