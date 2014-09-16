1d8b2be3c1873f9e443f7e188488362bbfbefdf2
denisraslov <denis.raslov@yandex.ru>
2014-09-16 19:19:46 +0400

    Search sale datepicker fix

--- webfront/web/blocks/receiptFinder/receiptFinder.js
+++ webfront/web/blocks/receiptFinder/receiptFinder.js
@@ -63,6 +63,10 @@ define(function(require, exports, module) {
 				dateToInput = this.$el.find('.inputDateRange input[name="dateTo"]'),
 				product;
 
+			//при клике на уже выбранную дату происходит её очищение - предотвращаем это
+			dateFromInput.val(dateFromInput.attr('value'));
+			dateToInput.val(dateToInput.attr('value'));
+
 			if (this.models.product) {
 				product = this.models.product.get('id');
 			}
