f1c0dbdc75b770062814a1c0ac1ef2fe12044969
Andrew Borovin <a.borovin@crystals.ru>
2014-09-17 19:08:49 +0400

    pos pages refactoring

--- webfront/web/blocks/page/pos/pos.js
+++ webfront/web/blocks/page/pos/pos.js
@@ -1,16 +1,29 @@
 define(function(require, exports, module) {
     //requirements
-    var Page = require('blocks/page/page');
+    var Page = require('blocks/page/page'),
+        checkKey = require('kit/checkKey/checkKey');
+
+    $(document)
+        .on('click', '.page__sideBarLink', function(e) {
+            e.preventDefault();
+
+            document.querySelector('.page').classList.toggle('page_sideBarVisible');
+        })
+        .on('click', function(e) {
+
+            if (!e.target.classList.contains('page__sideBarLink')){
+                document.querySelector('.page').classList.remove('page_sideBarVisible');
+            }
+        })
+        .on('keyup', function(e) {
+
+            if (checkKey(e.keyCode, ['ESC'])) {
+                document.querySelector('.page').classList.remove('page_sideBarVisible');
+            }
+        });
 
     return Page.extend({
         template: require('ejs!./template.ejs'),
-		events: {
-			'click .page_pos__PartSidebarIcon': function(e) {
-				var block = this;
-
-				block.changeSidebarVisibility();
-			}
-		},
 		models: {
 			store: function(){
 				var page = this,
@@ -19,24 +32,6 @@ define(function(require, exports, module) {
 				return new StoreModel({
 					id: page.params.storeId
 				});
-			}
-		},
-		isSidebarVisible: function() {
-			return document.isPosSidebarVisible || false;
-		},
-		changeSidebarVisibility: function() {
-			var $sidebar = $('#sidebar');
-			var $el = this.$el;
-
-			if (this.isSidebarVisible())
-			{
-				$el.removeClass('page_posPartVisibleSidebar');
-				document.isPosSidebarVisible = false;
-			}
-			else
-			{
-				$el.addClass('page_posPartVisibleSidebar');
-				document.isPosSidebarVisible = true;
 			}
 		}
     });
