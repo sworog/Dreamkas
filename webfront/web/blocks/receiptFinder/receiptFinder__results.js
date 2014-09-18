6e1257bdbea6b5f8f427888799a57780b9a4a21a
Andrew Borovin <a.borovin@crystals.ru>
2014-09-19 00:43:43 +0400

    fix dates

--- webfront/web/blocks/receiptFinder/receiptFinder__results.js
+++ webfront/web/blocks/receiptFinder/receiptFinder__results.js
@@ -7,6 +7,9 @@ define(function(require, exports, module) {
         collection: function(){
             return PAGE.collections.receipts;
         },
-        template: require('ejs!./receiptFinder__results.ejs')
+        template: require('ejs!./receiptFinder__results.ejs'),
+        isDifferentDates: function(date1, date2) {
+            return this.formatDate(date1) != this.formatDate(date2);
+        }
     });
 });
