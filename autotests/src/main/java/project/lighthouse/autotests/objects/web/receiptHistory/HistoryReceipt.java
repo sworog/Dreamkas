346e5283a4ff608991f6e92a69ff9b9e4618e7e1
Andrey Tolpeev <atolpeev@crystals.ru>
2014-09-19 12:28:52 +0400

    Added us-112.3 test scenarios, a bit of DateTime converters refactoring

--- autotests/src/main/java/project/lighthouse/autotests/objects/web/receiptHistory/HistoryReceipt.java
+++ autotests/src/main/java/project/lighthouse/autotests/objects/web/receiptHistory/HistoryReceipt.java
@@ -10,11 +10,6 @@ import project.lighthouse.autotests.objects.web.abstractObjects.objectInterfaces
 import project.lighthouse.autotests.objects.web.compare.CompareResults;
 import project.lighthouse.autotests.storage.Storage;
 
-import java.text.DateFormatSymbols;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.Map;
 
 public class HistoryReceipt extends AbstractObject implements ObjectLocatable, ResultComparable, ObjectClickable {
@@ -41,7 +36,7 @@ public class HistoryReceipt extends AbstractObject implements ObjectLocatable, R
 
     @Override
     public String getObjectLocator() {
-        return String.format("c суммой %s рублей и датой %s:%s", price, date, time);
+        return String.format("c суммой %s рублей и датой %s %s", price, date, time);
     }
 
     @Override
@@ -52,16 +47,7 @@ public class HistoryReceipt extends AbstractObject implements ObjectLocatable, R
     }
 
     private String getExpectedDate(Map<String, String> row) {
-        try {
-            String saleDate = Storage.getCustomVariableStorage().getSalesMap().get(row.get("date"));
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DateTimeHelper.ISO_8601, new Locale("ru"));
-            Date convertedSaleDate = new org.joda.time.DateTime(simpleDateFormat.parse(saleDate)).plusHours(4).toDate();
-            String[] months = {"января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"};
-            DateFormatSymbols customDateFormatSymbols = DateFormatSymbols.getInstance(new Locale("ru"));
-            customDateFormatSymbols.setMonths(months);
-            return new SimpleDateFormat("EEEE, dd MMMM HH:mm", customDateFormatSymbols).format(convertedSaleDate);
-        } catch (ParseException e) {
-            throw new AssertionError(e);
-        }
+        String saleDate = Storage.getCustomVariableStorage().getSalesMap().get(row.get("date"));
+        return DateTimeHelper.getExpectedSaleDate(saleDate, "EEEE, dd MMMM HH:mm");
     }
 }
---
+++ autotests/src/main/java/project/lighthouse/autotests/pages/pos/ReceiptElement.java
@@ -0,0 +1,52 @@
+package project.lighthouse.autotests.pages.pos;
+
+import net.thucydides.core.annotations.findby.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import project.lighthouse.autotests.common.CommonPageObject;
+import project.lighthouse.autotests.elements.items.NonType;
+import project.lighthouse.autotests.helper.DateTimeHelper;
+import project.lighthouse.autotests.helper.FieldChecker;
+import project.lighthouse.autotests.objects.web.abstractObjects.AbstractObject;
+import project.lighthouse.autotests.objects.web.abstractObjects.AbstractObjectCollection;
+import project.lighthouse.autotests.objects.web.receiptHistory.Receipt;
+import project.lighthouse.autotests.storage.Storage;
+
+public class ReceiptElement extends CommonPageObject {
+
+    public ReceiptElement(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void createElements() {
+        put("заголовок чека", new NonType(this, By.className("sale__title")));
+        put("способ оплаты", new NonType(this, By.xpath("//*[@name='bankcard' or @name='cash']")));
+        put("дата", new NonType(this, By.xpath("//*[@name='date']")) {
+
+            @Override
+            public FieldChecker getFieldChecker() {
+                return new FieldChecker(this) {
+
+                    @Override
+                    public void assertValueEqual(String expectedValue) {
+                        String saleDate = Storage.getCustomVariableStorage().getSalesMap().get(expectedValue);
+                        String expectedConvertedDate = DateTimeHelper.getExpectedSaleDate(saleDate, "dd MMMM YYYY, HH:mm");
+                        super.assertValueEqual(expectedConvertedDate);
+                    }
+                };
+            }
+        });
+    }
+
+    @Override
+    public AbstractObjectCollection getObjectCollection() {
+        return new AbstractObjectCollection(getDriver(), By.className("sale__product")) {
+
+            @Override
+            public AbstractObject createNode(WebElement element) {
+                return new Receipt(element);
+            }
+        };
+    }
+}
