51821da5419cb09e51922eb5f9df2b9c419e19a5
denisraslov <denis.raslov@yandex.ru>
2014-09-26 14:12:11 +0400

    Stock balance page fetch

--- webfront/web/pages/reports/stockBalance/stockBalance.js
+++ webfront/web/pages/reports/stockBalance/stockBalance.js
@@ -12,6 +12,22 @@ define(function(require, exports, module) {
 		blocks: {
 			select_stores: require('blocks/select/stores/stores'),
 			select_groupsSimple: require('blocks/select/groupsSimple/groupsSimple')
+		},
+		fetch: function() {
+			var block = this,
+				Products = require('collections/products/products'),
+				products;
+
+			return Page.prototype.fetch.call(block).then(function() {
+				var collections = block.collections;
+				var stores = collections.stores;
+
+				if (stores.length == 1) {
+					collections.products = new Products([], { storeId: stores.at(0).get('id') });
+
+					return collections.products.findByStore();
+				}
+			});
 		}
     });
 });
