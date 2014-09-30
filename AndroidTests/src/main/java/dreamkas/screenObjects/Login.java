463d331ba9fbd1feec22344b4d58fae631cadfa4
Andrey Tolpeev <atolpeev@crystals.ru>
2014-09-30 12:39:22 +0400

    Refactored the way of storing elements in the login screen object

--- AndroidTests/src/main/java/dreamkas/screenObjects/Login.java
+++ AndroidTests/src/main/java/dreamkas/screenObjects/Login.java
@@ -1,13 +1,25 @@
 package dreamkas.screenObjects;
 
+import net.thucydides.core.annotations.findby.FindBy;
+import net.thucydides.core.pages.WebElementFacade;
+
 public class Login extends ScreenObject {
 
+    @FindBy(id = "ru.crystals.vaverjanov.dreamkas.debug:id/txtUsername")
+    private WebElementFacade txtUsernameField;
+
+    @FindBy(id = "ru.crystals.vaverjanov.dreamkas.debug:id/txtPassword")
+    private WebElementFacade txtPasswordField;
+
+    @FindBy(id = "ru.crystals.vaverjanov.dreamkas.debug:id/btnLogin")
+    private WebElementFacade loginButton;
+
     public void inputLoginCredentials(String email, String password) {
-        getAppiumDriver().findElementById("ru.crystals.vaverjanov.dreamkas.debug:id/txtUsername").sendKeys(email);
-        getAppiumDriver().findElementById("ru.crystals.vaverjanov.dreamkas.debug:id/txtPassword").sendKeys(password);
+        txtUsernameField.type(email);
+        txtPasswordField.type(password);
     }
 
     public void clickOnLoginButton() {
-        getAppiumDriver().findElementById("ru.crystals.vaverjanov.dreamkas.debug:id/btnLogin").click();
+        loginButton.click();
     }
 }
