0ba4ad7512a3cf4d43cd8b12c7ea93b58b8291db
denisraslov <denis.raslov@yandex.ru>
2014-10-01 19:25:08 +0400

    Remote karma config fix

--- webfront/web/karma.remote.js
+++ webfront/web/karma.remote.js
@@ -44,7 +44,6 @@ module.exports = function(config) {
 			// do not include tests or libraries
 			// (these files will be instrumented by Istanbul),
 			//'requireDom.js': 'coverage',
-			'blocks/**/*.js': 'coverage',
 			'kit/**/*.js': 'coverage'
 		},
 
