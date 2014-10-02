09b289c917c5ff7830a0f831a6c018771c231ec7
denisraslov <denis.raslov@yandex.ru>
2014-10-02 18:25:57 +0400

    Collection tests

--- webfront/web/kit/collection/collection.spec.js
+++ webfront/web/kit/collection/collection.spec.js
@@ -1,4 +1,48 @@
 define(function(require, exports, module) {
-    //requirements
-    var collection = require('./collection')
+	//requirements
+	var Collection = require('./collection');
+
+	describe(module.id, function() {
+
+		var collection,
+			initFilters = { a: 1 };
+
+		beforeEach(function() {
+			collection = new Collection();
+		});
+
+		it('filter call fetch', function() {
+			spyOn(collection, 'fetch');
+
+			collection.filter(initFilters);
+
+			expect(collection.fetch).toHaveBeenCalled();
+		});
+
+		it('filter call fetch with right filters', function() {
+			var filters;
+
+			spyOn(collection, 'fetch').and.callFake(function() {
+				filters = this.filters;
+			});
+
+			collection.filter(initFilters);
+
+			expect(filters).toEqual(initFilters);
+		});
+
+		it('filter call fetch with right redefined filters', function() {
+			var filters;
+
+			spyOn(collection, 'fetch').and.callFake(function() {
+				filters = this.filters;
+			});
+
+			collection.filters = { a: 2, b: 1 };
+			collection.filter(initFilters);
+
+			expect(filters.a).toEqual(initFilters.a);
+		});
+	});
+
 });
