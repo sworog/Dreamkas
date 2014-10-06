77b2a1d1ac1ac34f7c6946be2e338f988abc5a4f
Andrew Borovin <a.borovin@crystals.ru>
2014-10-06 13:11:50 +0400

    calculate percent in gross report

--- webfront/web/blocks/table/groupStockSell/groupStockSell.js
+++ webfront/web/blocks/table/groupStockSell/groupStockSell.js
@@ -5,8 +5,32 @@ define(function(require, exports, module) {
     return Table.extend({
         template: require('ejs!./template.ejs'),
         sortBy: 'product.name',
-        collection: function(){
+        collection: function() {
             return PAGE.collections.groupStockSell;
+        },
+        render: function() {
+            var totalGrossSales = 0,
+                totalGrossMargin = 0;
+
+            this.collection.forEach(function(productStockSellModel) {
+                totalGrossSales += productStockSellModel.get('grossSales');
+                totalGrossMargin += productStockSellModel.get('grossMargin');
+            });
+
+            this.totalGrossSales = totalGrossSales;
+            this.totalGrossMargin = totalGrossMargin;
+
+            Table.prototype.render.apply(this, arguments);
+        },
+        calculateGrossSalesPercent: function(grossSales) {
+            var percent = 100 * grossSales / this.totalGrossSales;
+
+            return this.formatNumber(percent.toFixed(1));
+        },
+        calculateGrossMarginPercent: function(grossMargin) {
+            var percent = 100 * grossMargin / this.totalGrossMargin;
+
+            return this.formatNumber(percent.toFixed(1));
         }
     });
 });
