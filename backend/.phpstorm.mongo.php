f3b9631445d77327096b91bfc8f9d3e9bc68b776
Mikhail Shamin <mshamin@crystals.ru>
2014-10-07 14:40:13 +0400

    Added more Mongo classes stubs

--- backend/.phpstorm.mongo.php
+++ backend/.phpstorm.mongo.php
@@ -7,7 +7,7 @@ class MongoWriteConcernException extends MongoCursorException
      * @return array
      */
     public function getDocument()
-    {
+{
     }
 
     /**
@@ -21,4 +21,102 @@ class MongoWriteConcernException extends MongoCursorException
 
 class MongoDuplicateKeyException extends MongoWriteConcernException
 {
+}
+
+interface MongoCursorInterface extends Iterator, Traversable
+{
+    /**
+     * @param int $number
+     * @return MongoCursorInterface
+     */
+    public function batchSize($number);
+
+    /**
+     * @return array
+     */
+    public function info();
+
+    /**
+     * @return bool
+     */
+    public function dead();
+}
+
+class MongoCommandCursor implements MongoCursorInterface
+{
+    /**
+     * @param MongoClient $connection
+     * @param string $ns
+     * @param array $command
+     */
+    public function __construct(MongoClient $connection, $ns, array $command = array())
+    {
+    }
+
+    /**
+     * @param MongoClient $connection
+     * @param string $hash
+     * @param array $document
+     * @return MongoCommandCursor
+     */
+    public static function createFromDocument(MongoClient $connection, $hash, array $document)
+    {
+    }
+
+    /**
+     * @param int $number
+     * @return MongoCursorInterface
+     */
+    public function batchSize($number)
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function info()
+    {
+    }
+
+    /**
+     * @return bool
+     */
+    public function dead()
+    {
+    }
+
+    /**
+     * @return array
+     */
+    public function current()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function next()
+    {
+    }
+
+    /**
+     * @return string
+     */
+    public function key()
+    {
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+    }
+
+    /**
+     * @return void
+     */
+    public function rewind()
+    {
+    }
 }
