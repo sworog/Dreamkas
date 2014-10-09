19b4286ed8755fe538f626d26c0a8b0c3e2283d2
denisraslov <denis.raslov@yandex.ru>
2014-10-09 14:20:37 +0400

    Base block for stock sell

---
+++ webfront/web/blocks/page/stockSell/stockSell.js
@@ -0,0 +1,59 @@
+define(function(require, exports, module) {
+	//requirements
+	var Page = require('blocks/page/page');
+
+	return Page.extend({
+		activeNavigationItem: 'reports',
+		events: {
+			'change select[name="store"]': function(e) {
+				var storeId = e.target.value || undefined;
+
+				this.setParams({
+					storeId: storeId
+				});
+
+				e.target.classList.add('loading');
+
+				this.collections.stockCell.fetch({
+					filters: {
+						store: storeId
+					}
+				}).then(function() {
+					e.target.classList.remove('loading');
+				});
+			},
+			'update .inputDateRange': function(e) {
+
+				var dateFromInput = e.target.querySelector('[name="dateFrom"]'),
+					dateToInput = e.target.querySelector('[name="dateTo"]'),
+					dateFrom = dateFromInput.value || undefined,
+					dateTo = dateToInput.value || undefined;
+
+				this.setParams({
+					dateFrom: dateFrom,
+					dateTo: dateTo
+				});
+
+				dateFromInput.classList.add('loading');
+				dateToInput.classList.add('loading');
+
+				this.collections.groupStockSell.fetch({
+					filters: {
+						dateFrom: dateFrom,
+						dateTo: dateTo
+					}
+				}).then(function() {
+					dateFromInput.classList.remove('loading');
+					dateToInput.classList.remove('loading');
+				});
+			}
+		},
+		collections: {
+			stores: require('resources/store/collection')
+		},
+		blocks: {
+			select_store: require('blocks/select/store/store'),
+			inputDateRange: require('blocks/inputDateRange/inputDateRange')
+		}
+	});
+});
--- webfront/web/pages/reports/stockSell/groups/group/group.js
+++ webfront/web/pages/reports/stockSell/groups/group/group.js
@@ -1,58 +1,11 @@
 define(function(require, exports, module) {
     //requirements
-    var Page = require('blocks/page/page'),
-        router = require('router');
+	var Page_stockSell = require('blocks/page/stockSell/stockSell');
 
-    return Page.extend({
+    return Page_stockSell.extend({
         content: require('ejs!./content.ejs'),
-        activeNavigationItem: 'reports',
-        events: {
-            'change select[name="store"]': function(e) {
-                var storeId = e.target.value || undefined;
-
-                this.setParams({
-                    storeId: storeId
-                });
-
-                e.target.classList.add('loading');
-
-                this.collections.groupStockSell.fetch({
-                    filters: {
-                        store: storeId
-                    }
-                }).then(function() {
-                    e.target.classList.remove('loading');
-                });
-            },
-            'update .inputDateRange': function(e) {
-
-                var dateFromInput = e.target.querySelector('[name="dateFrom"]'),
-                    dateToInput = e.target.querySelector('[name="dateTo"]'),
-                    dateFrom = dateFromInput.value || undefined,
-                    dateTo = dateToInput.value || undefined;
-
-                this.setParams({
-                    dateFrom: dateFrom,
-                    dateTo: dateTo
-                });
-
-                dateFromInput.classList.add('loading');
-                dateToInput.classList.add('loading');
-
-                this.collections.groupStockSell.fetch({
-                    filters: {
-                        dateFrom: dateFrom,
-                        dateTo: dateTo
-                    }
-                }).then(function() {
-                    dateFromInput.classList.remove('loading');
-                    dateToInput.classList.remove('loading');
-                });
-            }
-        },
         collections: {
-            stores: require('resources/store/collection'),
-            groupStockSell: function() {
+			stockCell: function() {
                 var GroupStockSellCollection = require('resources/groupStockSell/collection');
 
                 return new GroupStockSellCollection([], {
@@ -75,8 +28,6 @@ define(function(require, exports, module) {
             }
         },
         blocks: {
-            select_store: require('blocks/select/store/store'),
-            inputDateRange: require('blocks/inputDateRange/inputDateRange'),
             table_groupStockSell: require('blocks/table/groupStockSell/groupStockSell')
         }
     });
