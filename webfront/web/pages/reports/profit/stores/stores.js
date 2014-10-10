c0b02d1ef8d3442e5ce6f05e27a36f65e1699cae
denisraslov <denis.raslov@yandex.ru>
2014-10-10 14:43:10 +0400

    Stores profit date range filter

--- webfront/web/pages/reports/profit/stores/stores.js
+++ webfront/web/pages/reports/profit/stores/stores.js
@@ -1,9 +1,17 @@
 define(function(require, exports, module) {
     //requirements
-    var Page = require('blocks/page/page');
+	var Page_productsProfit = require('blocks/page/productsProfit/productsProfit');
 
-    return Page.extend({
+    return Page_productsProfit.extend({
         content: require('ejs!./content.ejs'),
-        activeNavigationItem: 'reports'
+		ProfitCollection: require('resources/storesProfit/collection'),
+		params: {
+			dateFrom: function(){
+				var page = this,
+					currentTime = Date.now();
+
+				return page.formatDate(moment(currentTime).subtract(1, 'month'));
+			}
+		}
     });
 });
