8f11427f17d5c6b75d5f2ce1044c37d4e8db23d2
Ildar Sibagatov <siggb@yandex.com>
2014-10-10 12:09:25 +0400

    - started creation of current user helper

---
+++ ios-client/dreamkas/Sources/Helpers/CurrentUserHelper.h
@@ -0,0 +1,13 @@
+//
+//  CurrentUserHelper.h
+//  dreamkas
+//
+//  Created by sig on 10.10.14.
+//  Copyright (c) 2014 Dreamkas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CurrentUserHelper : NSObject
+
+@end
---
+++ ios-client/dreamkas/Sources/Helpers/CurrentUserHelper.m
@@ -0,0 +1,15 @@
+//
+//  CurrentUserHelper.m
+//  dreamkas
+//
+//  Created by sig on 10.10.14.
+//  Copyright (c) 2014 Dreamkas. All rights reserved.
+//
+
+#import "CurrentUserHelper.h"
+
+@implementation CurrentUserHelper
+
+// NSString *pass = [AESCrypt encrypt:param[@"password"] password:EncryptionPassword];
+
+@end
