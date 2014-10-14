32a4763c5303dc63b40c4a84fe17b1d24876e8e3
Andrey Tolpeev <atolpeev@crystals.ru>
2014-10-14 18:51:51 +0400

    Removed workAroundTypeForUserName method for firefox.

--- autotests/src/main/java/ru/dreamkas/steps/AuthorizationSteps.java
+++ autotests/src/main/java/ru/dreamkas/steps/AuthorizationSteps.java
@@ -61,7 +61,7 @@ public class AuthorizationSteps extends ScenarioSteps {
 
     @Step
     public void authorization(String email, String password, Boolean isFalse) {
-        workAroundTypeForUserName(email);
+        authorizationPage.input("userName", email);
         authorizationPage.input("password", password);
         authorizationPage.clickOnCommonItemWihName("loginButton");
         if (!isFalse) {
@@ -73,20 +73,6 @@ public class AuthorizationSteps extends ScenarioSteps {
     @Step
     public void loginButtonClick() {
         authorizationPage.clickOnCommonItemWihName("loginButton");
-    }
-
-    // TODO fix this in future
-
-    /**
-     * This is actually bad type workaround for failing logging in.
-     * For some reasons the type method is disturbed and the userName is not typed fully.
-     */
-    @Step
-    public void workAroundTypeForUserName(String inputText) {
-        authorizationPage.input("userName", inputText);
-        if (!((Findable) authorizationPage.getItems().get("userName")).getVisibleWebElementFacade().getValue().equals(inputText)) {
-            workAroundTypeForUserName(inputText);
-        }
     }
 
     @Step
