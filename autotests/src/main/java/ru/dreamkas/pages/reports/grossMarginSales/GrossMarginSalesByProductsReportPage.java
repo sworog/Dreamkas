bf377ae71c8588d52b5c66ed2256b9a9100964d4
Andrey Tolpeev <atolpeev@crystals.ru>
2014-10-15 19:30:20 +0400

    Added us-118.1 sorting test scenarios

--- autotests/src/main/java/ru/dreamkas/pages/reports/grossMarginSales/GrossMarginSalesByProductsReportPage.java
+++ autotests/src/main/java/ru/dreamkas/pages/reports/grossMarginSales/GrossMarginSalesByProductsReportPage.java
@@ -1,10 +1,11 @@
 package ru.dreamkas.pages.reports.grossMarginSales;
 
-import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.annotations.findby.By;
 import org.openqa.selenium.WebDriver;
 import ru.dreamkas.collection.reports.grossMarginSales.GrossMarginSalesByProductsObjectCollection;
 import ru.dreamkas.common.pageObjects.BootstrapPageObject;
+import ru.dreamkas.elements.items.NonType;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class GrossMarginSalesByProductsReportPage extends BootstrapPageObject {
     public GrossMarginSalesByProductsReportPage(WebDriver driver) {
@@ -13,11 +14,16 @@ public class GrossMarginSalesByProductsReportPage extends BootstrapPageObject {
 
     @Override
     public void addObjectButtonClick() {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void createElements() {
         putDefaultCollection(new GrossMarginSalesByProductsObjectCollection(getDriver(), By.name("product")));
+        put("колонка 'Товар'", new NonType(this, org.openqa.selenium.By.xpath("//*[@data-sort-by='product.name']")));
+        put("колонка 'Продажи'", new NonType(this, org.openqa.selenium.By.xpath("//*[@data-sort-by='grossSales']")));
+        put("колонка 'Себестоимость'", new NonType(this, org.openqa.selenium.By.xpath("//*[@data-sort-by='costOfGoods']")));
+        put("колонка 'Прибыль'", new NonType(this, org.openqa.selenium.By.xpath("//*[@data-sort-by='grossMargin']")));
+        put("колонка 'Количество'", new NonType(this, org.openqa.selenium.By.xpath("//*[@data-sort-by='quantity']")));
     }
 }
