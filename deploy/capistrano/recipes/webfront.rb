ff53466328cbce493219cd6ea46a75a5db5b1f81
Mikhail Shamin <munk13@gmail.com>
2014-10-16 17:50:04 +0400

    Webfront deploy build

--- deploy/capistrano/recipes/webfront.rb
+++ deploy/capistrano/recipes/webfront.rb
@@ -4,21 +4,27 @@ namespace :webfront do
         create_config
         rename_api
         build
+        symlink
     end
 
     desc "Build webfront app"
     task :build, :roles => :app, :except => { :no_release => true } do
-        run "sh -c 'cd #{latest_release} && npm run build"
+        run "sh -c 'cd #{latest_release} && #{sudo} npm install && npm run build'"
     end
 
     desc "Create config.js"
     task :create_config, :roles => :app, :except => { :no_release => true } do
-        run "cd #{latest_release} && cp config.js.template config.js"
+        run "cd #{latest_release}/src && cp config.js.template config.js"
+    end
+
+    desc "Symlink web folder"
+    task :symlink, :roles => :app, :except => { :no_release => true } do
+        run "cd #{latest_release} && ln -snf webfront/build webfront/web"
     end
 
     task :rename_api, :roles => :app, :except => { :no_release => true } do
         set :api_url, "#{host}.#{stage}.api.lighthouse.pro" unless exists?(:api_url)
         puts "--> API url name in ".yellow + "config.js".bold.yellow + " will be set to ".yellow + "#{api_url}".red
-        run "sed -i 's/%api_url%/#{api_url.gsub('/', '\\/')}/g' #{latest_release}/config.js"
+        run "sed -i 's/%api_url%/#{api_url.gsub('/', '\\/')}/g' #{latest_release}/src/config.js"
     end
 end
