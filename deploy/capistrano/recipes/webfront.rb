50a326215912619e4d94800a4a616d68e520c9a1
Mikhail Shamin <munk13@gmail.com>
2014-10-17 14:57:52 +0400

    Fixed npm build and symlink actions

--- deploy/capistrano/recipes/webfront.rb
+++ deploy/capistrano/recipes/webfront.rb
@@ -9,7 +9,7 @@ namespace :webfront do
 
     desc "Build webfront app"
     task :build, :roles => :app, :except => { :no_release => true } do
-        run "sh -c 'cd #{latest_release} && #{sudo} npm install && npm run build'"
+        run "cd #{latest_release} && #{sudo} npm install && npm run build"
     end
 
     desc "Create config.js"
@@ -19,7 +19,7 @@ namespace :webfront do
 
     desc "Symlink web folder"
     task :symlink, :roles => :app, :except => { :no_release => true } do
-        run "cd #{latest_release} && ln -snf webfront/build webfront/web"
+        run "cd #{latest_release} && ln -snf build web"
     end
 
     task :rename_api, :roles => :app, :except => { :no_release => true } do
