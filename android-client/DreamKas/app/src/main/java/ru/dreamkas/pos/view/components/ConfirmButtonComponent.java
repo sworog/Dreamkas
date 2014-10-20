c297cbd71d2d0e817afb16f80ab5454f9b4e7f06
Stabilitron <stabilitron@gmail.com>
2014-10-20 17:05:13 +0400

    add unconfirm receipt clear with tap anywhere

--- android-client/DreamKas/app/src/main/java/ru/dreamkas/pos/view/components/ConfirmButtonComponent.java
+++ android-client/DreamKas/app/src/main/java/ru/dreamkas/pos/view/components/ConfirmButtonComponent.java
@@ -1,10 +1,19 @@
 package ru.dreamkas.pos.view.components;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import ru.dreamkas.pos.R;
 
 //@EViewGroup(R.layout.product_search_component)
 public class ConfirmButtonComponent extends Button implements View.OnClickListener {
@@ -12,6 +21,7 @@ public class ConfirmButtonComponent extends Button implements View.OnClickListen
     private CharSequence mRegularText;
     private OnClickListener mExternalOnClickListener;
     private State mCurrentState = State.REGULAR;
+    private ViewGroup mLayout;
 
     public ConfirmButtonComponent(Context context) {
         super(context);
@@ -21,31 +31,11 @@ public class ConfirmButtonComponent extends Button implements View.OnClickListen
     public ConfirmButtonComponent(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
-
-       /* TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.ConfirmButtonComponent,
-                0, 0);
-        try {
-            Boolean mShowText = a.getBoolean(R.styleable.ConfirmButtonComponent_showText, false);
-        } finally {
-            a.recycle();
-        }*/
     }
 
     public ConfirmButtonComponent(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init();
-
-        /*TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.ConfirmButtonComponent,
-                0, 0);
-        try {
-            Boolean mShowText = a.getBoolean(R.styleable.ConfirmButtonComponent_showText, false);
-        } finally {
-            a.recycle();
-        }*/
     }
 
     public enum State{
@@ -54,6 +44,10 @@ public class ConfirmButtonComponent extends Button implements View.OnClickListen
 
     private void init(){
         super.setOnClickListener(this);
+    }
+
+    public void setContainer(ViewGroup parentContainer){
+        mLayout = parentContainer;
     }
 
     @Override
@@ -73,6 +67,8 @@ public class ConfirmButtonComponent extends Button implements View.OnClickListen
             return;
         }
 
+        final ConfirmButtonComponent thisBtn = this;
+
         switch (state){
             case REGULAR:
                 setTextColor(Color.DKGRAY);
@@ -83,6 +79,35 @@ public class ConfirmButtonComponent extends Button implements View.OnClickListen
                 setTextColor(Color.RED);
                 mRegularText = getText();
                 setText(mConfirmText);
+                mLayout.setVisibility(VISIBLE);
+
+                mLayout.setOnTouchListener(new OnTouchListener() {
+                    @Override
+                    public boolean onTouch(final View view, final MotionEvent event) {
+                        int x = (int) event.getX();
+                        int y = (int) event.getY();
+
+                        if (mCurrentState == State.WAIT_FOR_CONFIRM) {
+                            int[] coors = new int[]{0,0,0,0};
+
+                            //return wrong Y-coors
+                            thisBtn.getLocationOnScreen(coors);
+
+                            coors[2] = coors[0] + thisBtn.getWidth();
+                            //dirty replace wrong Y-coors
+                            coors[1] = thisBtn.getTop();
+                            coors[3] = thisBtn.getBottom();
+
+                            if (!isPointWithin(x, y, coors[0], coors[1], coors[2], coors[3]))
+                            {
+                                changeState(State.REGULAR);
+                                view.setVisibility(GONE);
+                            }
+                        }
+                        return false;
+                    }
+                });
+
                 mCurrentState = state;
                 break;
             case CONFIRMED:
@@ -91,6 +116,10 @@ public class ConfirmButtonComponent extends Button implements View.OnClickListen
                 }
                 changeState(State.REGULAR);
         }
+    }
+
+    static boolean isPointWithin(int x, int y, int x1, int y1, int x2, int y2) {
+        return (x <= x2 && x >= x1 && y <= y2 && y >= y1);
     }
 
     @Override
