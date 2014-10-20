db74be2ff2f874ddd5473a5e420965406d19929f
Andrew Borovin <a.borovin@crystals.ru>
2014-10-20 19:04:17 +0400

    cleanup

--- webfront/src/require.config.js
+++ webfront/src/require.config.js
@@ -23,7 +23,6 @@ require.config({
             'form2js': 'bower_components/form2js/src/form2js',
             'numeral': 'bower_components/numeral/numeral',
             'amd-loader': 'bower_components/amd-loader/amd-loader',
-            'sortable': 'bower_components/sortable/js/sortable',
             'datepicker': 'madmin/vendors/bootstrap-datepicker/js/bootstrap-datepicker',
             'typeahead': 'bower_components/typeahead.js/dist/typeahead.bundle'
         }
