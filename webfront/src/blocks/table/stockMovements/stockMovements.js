7158cd7ccb789575a03e51d6cf4ad2739d4b5caa
Andrew Borovin <a.borovin@crystals.ru>
2014-10-22 02:53:19 +0400

    fix stockmovement table

--- webfront/src/blocks/table/stockMovements/stockMovements.js
+++ webfront/src/blocks/table/stockMovements/stockMovements.js
@@ -4,8 +4,18 @@ define(function(require, exports, module) {
 
     return Table.extend({
         template: require('ejs!./template.ejs'),
-        collection: function(){
+        collection: function() {
             return PAGE.collections.stockMovements;
+        },
+        render: function() {
+
+            var block = this;
+
+            this.groupedByDateList = this.collection.groupBy(function(stockMovement) {
+                return block.formatDate(stockMovement.get('date'));
+            });
+
+            return Table.prototype.render.apply(this, arguments);
         }
     });
 });
