0895031c3f7b0efa89f63e40e08d6169bd724f3e
Mikhail Shamin <mshamin@crystals.ru>
2014-10-22 18:32:18 +0400

    add product metrics recalc

--- backend/import_amn.sh
+++ backend/import_amn.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 default_env=staging
+default_receipts=~/amn/
+
 env=${1:-$default_env}
 batch_size=1000
-reciepts=~/amn/
+receipts=${2:-$default_receipts}
 project=amn
 
 ./app/console cache:clear --no-warmup -e=$env
@@ -12,7 +14,7 @@ project=amn
 ./app/console doctrine:mongodb:schema:drop -e=$env
 ./app/console doctrine:mongodb:schema:create -e=$env
 
-./app/console lighthouse:user:create -e=$env --customProjectName=amn owner@lighthouse.pro lighthouse
+./app/console lighthouse:user:create -e=$env --customProjectName=$project owner@lighthouse.pro lighthouse
 
 ./app/console doctrine:mongodb:fixtures:load -e=$env --append --fixtures=src/Lighthouse/CoreBundle/DataFixtures/AMN/
 
@@ -21,7 +23,7 @@ project=amn
 
 ./app/console lighthouse:import:invoices:csv -e=$env --project=$project --original-date=2013-06-21 --import-date="-5 months 21 days" fixtures/amn-invoices.csv
 
-./app/console lighthouse:import:sales:local -e=$env --project=$project --sort=filedate --receipt-date=2013-06-21 --import-date="-5 months 21 days" $reciepts
+./app/console lighthouse:import:sales:local -e=$env --project=$project --sort=filedate --receipt-date=2013-06-21 --import-date="-5 months 21 days" $receipts
 
-
+./app/console lighthouse:products:recalculate_metrics -e=$env
 ./app/console lighthouse:reports:recalculate -e=$env
