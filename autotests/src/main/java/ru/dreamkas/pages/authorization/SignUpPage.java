a745fb5a21c0eec1340f82fdc3dbf32bcb3dfaea
Fedot <vfedorov@crystals.ru>
2014-10-24 12:26:00 +0400

    Added tests for registration user and check email password

--- autotests/src/main/java/ru/dreamkas/pages/authorization/SignUpPage.java
+++ autotests/src/main/java/ru/dreamkas/pages/authorization/SignUpPage.java
@@ -1,9 +1,12 @@
 package ru.dreamkas.pages.authorization;
 
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.dreamkas.common.pageObjects.CommonPageObject;
 import ru.dreamkas.elements.Buttons.ButtonFacade;
+import ru.dreamkas.elements.Buttons.LinkFacade;
+import ru.dreamkas.elements.bootstrap.buttons.SuccessBtnFacade;
 import ru.dreamkas.elements.items.Input;
 
 @DefaultUrl("/signup")
@@ -16,6 +19,6 @@ public class SignUpPage extends CommonPageObject {
     @Override
     public void createElements() {
         put("email", new Input(this, "email"));
-        put("signUpButton", new ButtonFacade(this, "Зарегистрироваться"));
+        put("signUpButton", new SuccessBtnFacade(this, By.xpath("//*[contains(@class, 'btn btn-success') and contains(text(), 'Зарегистрироваться')]")));
     }
 }
