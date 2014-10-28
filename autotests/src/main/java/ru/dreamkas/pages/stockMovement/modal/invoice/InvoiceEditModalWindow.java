e80bb7ae9419f907175dd92140c206902241d0e2
Mikhail Shamin <munk13@gmail.com>
2014-10-28 13:12:07 +0300

    Supplier return delete confirmation test fix

--- autotests/src/main/java/ru/dreamkas/pages/stockMovement/modal/invoice/InvoiceEditModalWindow.java
+++ autotests/src/main/java/ru/dreamkas/pages/stockMovement/modal/invoice/InvoiceEditModalWindow.java
@@ -1,19 +1,11 @@
 package ru.dreamkas.pages.stockMovement.modal.invoice;
 
 import org.openqa.selenium.WebDriver;
-import ru.dreamkas.elements.items.NonType;
 
 public class InvoiceEditModalWindow extends InvoiceCreateModalWindow {
 
     public InvoiceEditModalWindow(WebDriver driver) {
         super(driver);
-    }
-
-    @Override
-    public void createElements() {
-        super.createElements();
-
-        put("заголовок успешного удаления", new NonType(this, "//*[@name='successRemoveTitle']"));
     }
 
     @Override
