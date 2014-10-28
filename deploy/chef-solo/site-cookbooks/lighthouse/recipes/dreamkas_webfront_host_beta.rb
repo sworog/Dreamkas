db2c8638272b895af0c3efef33e7e28d01426923
Fedot <vfedorov@crystals.ru>
2014-10-28 16:55:33 +0300

    Added chef receips for beta servers

---
+++ deploy/chef-solo/site-cookbooks/lighthouse/recipes/dreamkas_webfront_host_beta.rb
@@ -0,0 +1,29 @@
+#############################################
+# nginx
+#############################################
+directory "/var/www" do
+  action :create
+  owner "www-data"
+  group "www-data"
+end
+
+directory "/var/www/nginx" do
+  action :create
+  owner "www-data"
+  group "www-data"
+end
+
+cookbook_file "nginx/favicon.ico" do
+  path "/var/www/nginx/favicon.ico"
+  action :create
+  owner "www-data"
+  mode "0644"
+end
+
+cookbook_file "nginx/dreamkas_beta_webfront.conf" do
+  source "nginx/dreamkas_beta_webfront.conf"
+  path "/etc/nginx/sites-available/dreamkas_beta_webfront.conf"
+  action :create
+end
+
+nginx_site "dreamkas_beta_webfront.conf"
