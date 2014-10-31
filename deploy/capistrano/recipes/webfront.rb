08dd711c453e2b76bd02a8253e7686e5a79c25ca
Mikhail Shamin <munk13@gmail.com>
2014-10-31 15:26:45 +0300

    Fixed webfront deploy scripts

--- deploy/capistrano/recipes/webfront.rb
+++ deploy/capistrano/recipes/webfront.rb
@@ -9,8 +9,9 @@ namespace :webfront do
 
     desc "Build webfront app"
     task :build, :roles => :app, :except => { :no_release => true } do
+        flag = fetch(:npm_flag, '')
         puts "--> NPM build".yellow
-        run "cd #{latest_release} && npm install && npm run build"
+        run "cd #{latest_release} && npm install #{flag} && npm run build"
     end
 
     desc "Create config.js"
@@ -27,5 +28,46 @@ namespace :webfront do
         set :api_url, "#{host}.#{stage}.api.lighthouse.pro" unless exists?(:api_url)
         puts "--> API url name in ".yellow + "config.js".bold.yellow + " will be set to ".yellow + "#{api_url}".red
         run "sed -i 's/%api_url%/#{api_url.gsub('/', '\\/')}/g' #{latest_release}/src/config.js"
+    end
+end
+
+def capifony_pretty_print(msg)
+    if logger.level == Capistrano::Logger::IMPORTANT
+        pretty_errors
+
+        msg = msg.slice(0, 57)
+        msg << '.' * (60 - msg.size)
+        print msg
+    else
+        puts msg.green
+    end
+end
+
+def capifony_puts_ok
+    if logger.level == Capistrano::Logger::IMPORTANT && !$error
+        puts '✔'.green
+    end
+
+    $error = false
+end
+
+def pretty_errors
+    if !$pretty_errors_defined
+        $pretty_errors_defined = true
+
+        class << $stderr
+            @@firstLine = true
+            alias _write write
+
+            def write(s)
+                if @@firstLine
+                    s = '✘' << "\n" << s
+                    @@firstLine = false
+                end
+
+                _write(s.red)
+                $error = true
+            end
+        end
     end
 end
--- webfront/config/stages/production.rb
+++ webfront/config/stages/production.rb
@@ -1,7 +1,9 @@
-server 'beta.dreamkas.ru', :app, :web, :primary => true
 set :domain,      "beta.dreamkas.ru" unless exists?(:domain)
 set :host,        "beta" unless exists?(:host)
 set :branch,      "primary" unless exists?(:branch)
-set :application, "#{host}.#{app_end}"
-set :application_url, "http://#{host}.dreamkas.ru"
-set :api_url, "api.dreamkas.ru" unless exists?(:api_url)
+set :api_url,     "api.dreamkas.ru" unless exists?(:api_url)
+
+set (:application_folder) {"#{host}.#{app_end}"}
+set (:application_url) {"http://#{host}.dreamkas.ru"}
+
+set :npm_flag, '--production'
