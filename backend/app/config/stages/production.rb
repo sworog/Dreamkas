e8b216c0beb58fd88f488d4e44b087841c5661f4
Mikhail Shamin <munk13@gmail.com>
2014-10-31 15:41:54 +0300

    Fixed backend deploy

--- backend/app/config/stages/production.rb
+++ backend/app/config/stages/production.rb
@@ -1,6 +1,6 @@
-server 'api.dreamkas.ru', :app, :web, :primary => true
-set :domain,      "api.dreamkas.ru" unless exists?(:domain)
+set :domain,      "beta.dreamkas.ru" unless exists?(:domain)
 set :host,        "beta" unless exists?(:host)
 set :branch,      "primary" unless exists?(:branch)
-set :application, "#{host}.#{app_end}"
-set :application_url, "http://api.dreamkas.ru"
+
+set (:application_folder) {"#{host}.#{app_end}"}
+set (:application_url) {"http://#{host}.dreamkas.ru"}
