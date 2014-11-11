7de725b1de825b7f87959ddd34b7271e56bb51df
Andrew Borovin <a.borovin@crystals.ru>
2014-11-11 17:00:37 +0300

    autocomplete refactoring

--- webfront/src/blocks/autocomplete/autocomplete_products/autocomplete_products.js
+++ webfront/src/blocks/autocomplete/autocomplete_products/autocomplete_products.js
@@ -3,24 +3,31 @@ define(function(require, exports, module) {
     var Autocomplete = require('blocks/autocomplete/autocomplete');
 
     return Autocomplete.extend({
-        remoteUrl: '/products/search?properties[]=name&properties[]=sku&query=%QUERY',
-        resetLink: true,
-        initTypeahead: function() {
-            var block = this;
+        template: require('ejs!./template.ejs'),
+        placeholder: 'Наименование или артикул товара',
+        source: '/products/search?properties[]=name&properties[]=sku',
+        suggestionTemplate: require('ejs!./suggestion.ejs'),
+        events: {
+            'keyup .autocomplete__input': function(e){
 
-            block.$el.find('input').typeahead({
-                    highlight: true,
-                    minLength: 3
-                },
-                {
-                    source: block.engine.ttAdapter(),
-                    displayKey: 'name',
-                    templates: {
-                        suggestion: require('ejs!./suggestion.ejs')
-                    }
-                });
+                var block = this,
+                    input = e.target;
 
-			block.$el.find('input.form-control').attr('name', 'product.name');
+                if (input.value.length){
+                    block.$tetherElement.width(block.$input.outerWidth());
+                    block.tether.enable();
+                    block.tether.position();
+                } else {
+                    block.tether.disable();
+                }
+
+            },
+            'blur .autocomplete__input': function(){
+
+                var block = this;
+
+                block.tether.disable();
+            }
         }
     });
 });
