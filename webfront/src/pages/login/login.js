19f8cabfc5fc169d9c753e00027e4957295b84cd
Andrew Borovin <a.borovin@crystals.ru>
2014-11-11 12:24:25 +0300

    success message color in forms

---
+++ webfront/src/pages/login/login.js
@@ -0,0 +1,10 @@
+define(function(require, exports, module) {
+    //requirements
+    var Page_auth = require('blocks/page/auth/auth');
+
+    return Page_auth.extend({
+        blocks: {
+            form: require('blocks/form/login/login')
+        }
+    });
+});
