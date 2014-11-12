a3e5506fb4e5696a8c45f36940bba9cbb0106b30
Andrey Tolpeev <atolpeev@crystals.ru>
2014-11-12 19:26:16 +0300

    Added alert locators for login dialog page

--- ios-test/src/main/java/ru/dreamkas/pages/dialogs/LoginDialogPage.java
+++ ios-test/src/main/java/ru/dreamkas/pages/dialogs/LoginDialogPage.java
@@ -2,6 +2,7 @@ package ru.dreamkas.pages.dialogs;
 
 import ru.dreamkas.elements.Button;
 import ru.dreamkas.elements.Input;
+import ru.dreamkas.elements.Text;
 import ru.dreamkas.pages.CommonPageObject;
 
 public class LoginDialogPage extends CommonPageObject {
@@ -11,5 +12,8 @@ public class LoginDialogPage extends CommonPageObject {
         putElement("Логин", new Input(this, "AI_LogInPage_LoginField"));
         putElement("Пароль", new Input(this, "AI_LogInPage_PwdField"));
         putElement("Войти", new Button(this, "AI_LogInPage_LogInButton"));
+        putElement("Заголовок ошибки", new Text(this, "Ошибка"));
+        putElement("Текст ошибки", new Text(this, "Во время запроса произошла ошибка. Попробуйте снова"));
+        putElement("Подтвердить закрытие ошибки", new Button(this, "OK"));
     }
 }
