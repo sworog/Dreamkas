fe8acc92012bf7753b83f13fbcb9f64fa2dd91ff
Andrew Borovin <a.borovin@crystals.ru>
2014-11-12 15:50:29 +0300

    product autocomplete deselect

--- webfront/src/blocks/form/stockMovementProducts/stockMovementProducts.js
+++ webfront/src/blocks/form/stockMovementProducts/stockMovementProducts.js
@@ -4,31 +4,31 @@ define(function(require, exports, module) {
 
     return Form.extend({
         template: require('ejs!./template.ejs'),
-		ProductList: require('./stockMovementProducts__productList'),
+        ProductList: require('./stockMovementProducts__productList'),
         events: {
-            'keyup [name="quantity"], [name="price"], [name="priceEntered"]': function(e){
+            'keyup [name="quantity"], [name="price"], [name="priceEntered"]': function(e) {
                 var block = this;
 
                 block.renderTotalSum();
             },
-            'click .table_stockMovementProducts__removeProductLink': function(e){
+            'click .table_stockMovementProducts__removeProductLink': function(e) {
                 var block = this,
                     modelCid = e.currentTarget.dataset.modelCid,
                     product;
 
-                block.collection.remove(block.collection.get(modelCid), { temp: true });
+                block.collection.remove(block.collection.get(modelCid), {temp: true});
             }
         },
         blocks: {
-            productList: function(){
+            productList: function() {
                 var ProductList = this.ProductList;
 
                 return new ProductList({
                     collection: this.collection,
-					priceField: this.priceField
+                    priceField: this.priceField
                 });
             },
-            totalPrice: function(){
+            totalPrice: function() {
                 var TotalPrice = require('./stockMovementProducts__totalPrice');
 
                 return new TotalPrice({
@@ -59,65 +59,67 @@ define(function(require, exports, module) {
         submitSuccess: function(invoice) {
             var block = this;
 
-            block.collection.push(invoice.products[0], { temp: true });
+            block.collection.push(invoice.products[0], {temp: true});
 
             block.clear();
 
             block.$('.tt-input[name="product.name"]').focus();
         },
-		showErrors: function(error){
-			var block = this,
-				productErrors = error.errors.children.products.children[0].children;
+        showErrors: function(error) {
+            var block = this,
+                productErrors = error.errors.children.products.children[0].children;
 
-			var fields = [],
-				errorMessages = [];
+            var fields = [],
+                errorMessages = [];
 
-			_.forEach(productErrors, function(error, field){
-				if (error.errors){
-					fields.push(field);
-					errorMessages = _.union(errorMessages, error.errors);
-				}
-			});
+            _.forEach(productErrors, function(error, field) {
+                if (error.errors) {
+                    fields.push(field);
+                    errorMessages = _.union(errorMessages, error.errors);
+                }
+            });
 
-			block.showGlobalError(errorMessages);
+            block.showGlobalError(errorMessages);
 
-			_.forEach(fields, function(fieldName){
+            _.forEach(fields, function(fieldName) {
 
-				if (fieldName === 'product'){
-					fieldName = 'product.name';
-				}
+                if (fieldName === 'product') {
+                    fieldName = 'product.name';
+                }
 
-				block.$('input[name="' + fieldName + '"]').addClass('invalid');
-			});
-		},
-        clear: function(){
+                block.$('input[name="' + fieldName + '"]').addClass('invalid');
+            });
+        },
+        clear: function() {
             var block = this;
 
             block.$('input').val('');
             block.$('[name="product.name"]').typeahead('val', '');
             block.renderTotalSum();
         },
-        selectProduct: function(product){
+        selectProduct: function(product) {
             var block = this;
-            
+
             block.set('data.product', {
                 id: product.id
             });
 
-            block.$('input[name="product.name"]').val(product.name);
+            block.$('input[name="product.name"]')
+                .val(product.name)
+                .focus();
 
-			block.$('input[name="' + block.priceField + '"]').val(product.purchasePrice ? block.formatMoney(product.purchasePrice) : '');
+            block.$('input[name="' + block.priceField + '"]').val(product.purchasePrice ? block.formatMoney(product.purchasePrice) : '');
 
             block.$('input[name="quantity"]').val(1);
             block.$('.product__units').html(product.units || 'шт.');
 
             block.renderTotalSum();
         },
-        deselectProduct: function(){
+        deselectProduct: function() {
 
             var block = this;
 
-            if (block.$('input[name="product.name"]').val().length){
+            if (block.$('input[name="product.name"]').val().length) {
                 block.set('data.product', {
                     id: 'xxx'
                 });
@@ -134,19 +136,19 @@ define(function(require, exports, module) {
             block.renderTotalSum();
 
         },
-		getTotalSum: function(){
-			var block = this,
-				quantity = block.normalizeNumber(block.$('input[name="quantity"]').val()),
-				purchasePrice = block.normalizeNumber(block.$('input[name="' + this.priceField + '"]').val()),
-				totalPrice = quantity * purchasePrice;
+        getTotalSum: function() {
+            var block = this,
+                quantity = block.normalizeNumber(block.$('input[name="quantity"]').val()),
+                purchasePrice = block.normalizeNumber(block.$('input[name="' + this.priceField + '"]').val()),
+                totalPrice = quantity * purchasePrice;
 
-			return typeof totalPrice === 'number' ? totalPrice : null;
-		},
-        renderTotalSum: function(){
+            return typeof totalPrice === 'number' ? totalPrice : null;
+        },
+        renderTotalSum: function() {
             var block = this,
                 totalPrice = block.getTotalSum();
 
-			block.$('.totalSum').html(totalPrice ? block.formatMoney(totalPrice) : '');
+            block.$('.totalSum').html(totalPrice ? block.formatMoney(totalPrice) : '');
         }
     });
 });
