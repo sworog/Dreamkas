1c432d86bd2eaa3e688bb3087a9567fa74a6cc9b
Ildar Sibagatov <siggb@yandex.com>
2014-11-12 15:15:47 +0300

    - two custom panel buttons added

---
+++ ios-client/dreamkas/Sources/Customs/Interface Elements/Buttons/PlusCircleButton.h
@@ -0,0 +1,13 @@
+//
+//  PlusCircleButton.h
+//  dreamkas
+//
+//  Created by sig on 10/11/14.
+//  Copyright (c) 2014 Dreamkas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlusCircleButton : UIButton
+
+@end
