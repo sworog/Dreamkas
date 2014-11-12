a25c91a1e2cedc92224017e36f983700c2285058
denisraslov <denis.raslov@yandex.ru>
2014-11-12 16:40:41 +0300

    Store select refactoring

--- webfront/src/blocks/select/store/store.js
+++ webfront/src/blocks/select/store/store.js
@@ -4,10 +4,27 @@ define(function(require, exports, module) {
 
     return Block.extend({
         template: require('ejs!./template.ejs'),
-        all: '0',
+        modalId: null,
         selected: null,
+        all: false,
+        add: false,
+        globalEvents: {
+            'submit:success': function(data, block) {
+
+                var modal = block.$el.closest('.modal')[0];
+
+                if (modal && modal.id === 'modal_store' + this.cid) {
+
+                    this.selected = data.id;
+                    this.render();
+                }
+            }
+        },
         collection: function(){
             return PAGE.collections.stores;
+        },
+        blocks: {
+            modal_store: require('blocks/modal/store/store')
         }
     });
 });
