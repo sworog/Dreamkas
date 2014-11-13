9306d77d819be69e1eec471530b989a6792270a5
denisraslov <denis.raslov@yandex.ru>
2014-11-13 14:52:49 +0300

    Store creating in stock in form

--- webfront/src/blocks/modal/store/store.js
+++ webfront/src/blocks/modal/store/store.js
@@ -7,13 +7,12 @@ define(function(require, exports, module) {
         storeId: 0,
         template: require('ejs!./template.ejs'),
         blocks: {
-            form_store: function(){
-                var block = this,
-                    Form_store = require('blocks/form/store/store');
+            form_store: function(options) {
+                var Form_store = require('blocks/form/store/store');
 
-                return new Form_store({
-                    storeId: block.storeId
-                });
+                options.storeId = this.storeId;
+
+                return new Form_store(options);
             }
         }
     });
