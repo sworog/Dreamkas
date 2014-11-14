bd35ef1a76c970c9151f547c787a0e8b3bf5158e
denisraslov <denis.raslov@yandex.ru>
2014-11-14 15:35:11 +0300

    Stock in creating on first system using autotests

--- autotests/src/main/java/ru/dreamkas/pages/stockMovement/modal/stockIn/StockInCreateModalWindow.java
+++ autotests/src/main/java/ru/dreamkas/pages/stockMovement/modal/stockIn/StockInCreateModalWindow.java
@@ -2,6 +2,8 @@ package ru.dreamkas.pages.stockMovement.modal.stockIn;
 
 import org.openqa.selenium.WebDriver;
 import ru.dreamkas.collection.stockMovement.stockIn.StockInProductCollection;
+import ru.dreamkas.elements.bootstrap.buttons.TransparentBtnFacade;
+import ru.dreamkas.elements.items.NonType;
 import ru.dreamkas.pages.stockMovement.modal.StockMovementModalPage;
 
 public class StockInCreateModalWindow extends StockMovementModalPage {
@@ -13,6 +15,15 @@ public class StockInCreateModalWindow extends StockMovementModalPage {
     @Override
     public String modalWindowXpath() {
         return "//*[@id='modal_stockIn']";
+    }
+
+    @Override
+    public void createElements() {
+        super.createElements();
+        put("кнопка 'Создать магазин'", new TransparentBtnFacade(this, "Создать магазин"));
+        put("кнопка 'Создать товар'", new TransparentBtnFacade(this, "Создать товар"));
+        put("плюсик, чтобы создать новый магазин", new NonType(this, "//*[contains(@data-modal, 'modal_store') and not(contains(@class, 'btn'))]"));
+        put("плюсик, чтобы создать новый товар", new NonType(this, "//*[contains(@data-modal, 'modal_productForAutocomplete') and not(contains(@class, 'btn'))]"));
     }
 
     @Override
