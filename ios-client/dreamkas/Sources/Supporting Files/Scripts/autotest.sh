f31ca257fc4cf44d992a5d2fd3b8c77f032e6cb9
Fedot <fedotru@gmail.com>
2014-11-14 15:25:02 +0300

    Update autotest.sh

--- ios-client/dreamkas/Sources/Supporting Files/Scripts/autotest.sh
+++ ios-client/dreamkas/Sources/Supporting Files/Scripts/autotest.sh
@@ -6,7 +6,7 @@
 #  Created by sig on 14.11.2014
 #
 
-buildpath=../../../Builds
+buildpath=Builds
 apiurl=http://ios.autotests.api.lighthouse.pro/
 
 rm -rf $buildpath
@@ -16,7 +16,7 @@ mkdir $buildpath
 -arch i386 \
 -sdk iphonesimulator8.1 \
 -configuration Debug \
--project ../../../dreamkas.xcodeproj \
+-project dreamkas.xcodeproj \
 -scheme dreamkas \
 AUTOTESTS_SERVER=$apiurl \
 clean build-tests -only dreamkas-tests \
@@ -26,7 +26,7 @@ CONFIGURATION_BUILD_DIR=$buildpath
 -arch i386 \
 -sdk iphonesimulator8.1 \
 -configuration Debug \
--project ../../../dreamkas.xcodeproj \
+-project dreamkas.xcodeproj \
 -scheme dreamkas \
 AUTOTESTS_SERVER=$apiurl \
 run-tests -only dreamkas-tests -parallelize \
