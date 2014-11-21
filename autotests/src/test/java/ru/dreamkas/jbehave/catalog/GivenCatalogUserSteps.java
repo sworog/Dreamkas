6907bb7ff9984f714c547d571e0afa6803e974f0
Andrey Tolpeev <atolpeev@crystals.ru>
2014-11-21 12:42:54 +0300

    Added us-130 product create/edit alert story test scenarios

--- autotests/src/test/java/ru/dreamkas/jbehave/catalog/GivenCatalogUserSteps.java
+++ autotests/src/test/java/ru/dreamkas/jbehave/catalog/GivenCatalogUserSteps.java
@@ -1,6 +1,7 @@
 package ru.dreamkas.jbehave.catalog;
 
 import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Alias;
 import org.jbehave.core.annotations.Given;
 import ru.dreamkas.steps.catalog.CatalogSteps;
 
@@ -10,6 +11,7 @@ public class GivenCatalogUserSteps {
     CatalogSteps catalogSteps;
 
     @Given("the user opens catalog page")
+    @Alias("пользователь открывает страницу ассортимента")
     public void givenTheUserOpensCatalogPage() {
         catalogSteps.openCatalogPage();
     }
