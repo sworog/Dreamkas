e6b1d85963b06a125a0ff5939a8c497a734804ad
Andrew Borovin <a.borovin@crystals.ru>
2014-11-24 18:58:35 +0300

    merge us-119.2

--- webfront/src/resources/supplier/model.js
+++ webfront/src/resources/supplier/model.js
@@ -1,9 +1,7 @@
 define(function(require, exports, module) {
     //requirements
     var Model = require('kit/model/model');
-
-    require('./mocks/delete');
-
+    
     return Model.extend({
         urlRoot: Model.baseApiUrl + '/suppliers',
         saveData: [
