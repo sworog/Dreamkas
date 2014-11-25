08bfc2605f1602105dcdb8b286f1a9869581cd25
Andrew Borovin <a.borovin@crystals.ru>
2014-11-25 20:35:09 +0300

    remove incoice delete mock

--- webfront/src/resources/invoice/model.js
+++ webfront/src/resources/invoice/model.js
@@ -2,9 +2,7 @@ define(function(require) {
     //requirements
     var Model = require('kit/model/model'),
         cookies = require('cookies');
-
-    require('./mocks/deleteError');
-
+    
     return Model.extend({
         storeId: null,
         fromOrder: null,
