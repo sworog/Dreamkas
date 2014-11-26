80f5c06fca3024a2fb973677a2e8d5a6f5483e60
Andrew Borovin <a.borovin@crystals.ru>
2014-11-26 17:41:11 +0300

    add cashflow resource and mocks

--- webfront/src/resources/cashFlow/collection.js
+++ webfront/src/resources/cashFlow/collection.js
@@ -2,6 +2,8 @@ define(function(require, exports, module) {
     //requirements
     var Collection = require('kit/collection/collection');
 
+    require('./mocks/get_0');
+
     return Collection.extend({
         url: Collection.baseApiUrl + '/cashFlows',
         model: require('./model')
