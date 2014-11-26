8966fa65ef9a92d2acc4bd42ef67a7b66dfd9bd9
Andrew Borovin <a.borovin@crystals.ru>
2014-11-26 19:24:06 +0300

    delete and edit mocks for cashflow

--- webfront/src/resources/cashFlow/mocks/get_10.js
+++ webfront/src/resources/cashFlow/mocks/get_10.js
@@ -11,60 +11,70 @@ define(function(require, exports, module) {
         status: 200,
         responseText: [
             {
-                direction: 'in',
-                date: "2014-11-24T11:10:56+0300",
-                amount: 10000,
-                comment: 'Нашел под кассой'
-            },
-            {
+                id: '1',
                 direction: 'out',
                 date: "2014-11-24T12:10:56+0300",
                 amount: 23000,
                 comment: 'Продажи'
             },
             {
+                id: '2',
                 direction: 'out',
                 date: "2014-11-24T13:10:56+0300",
                 amount: 45340,
                 comment: 'Списания'
             },
             {
+                id: '3',
                 direction: 'out',
                 date: "2014-11-24T11:10:56+0300",
                 amount: 56450,
                 comment: 'Аренда'
             },
             {
+                id: '4',
                 direction: 'in',
                 date: "2014-11-25T11:10:56+0300",
                 amount: 10000,
                 comment: 'Возврат от Валио'
             },
             {
+                id: '5',
                 direction: 'out',
                 date: "2014-11-26T11:10:56+0300",
                 amount: 390,
                 comment: 'Интернет'
             },
             {
+                id: '6',
                 direction: 'out',
                 date: "2014-11-28T11:10:56+0300",
                 amount: 10000,
                 comment: 'Взятка пожарным'
             },
             {
+                id: '7',
                 direction: 'in',
                 date: "2014-11-29T11:10:56+0300",
                 amount: 459890,
                 comment: 'Продажи'
             },
             {
+                id: '8',
                 direction: 'out',
                 date: "2014-11-29T13:10:56+0300",
                 amount: 45340,
                 comment: 'Списания'
             },
             {
+                id: '9',
+                direction: 'in',
+                date: "2014-11-30T11:10:56+0300",
+                amount: 10000,
+                comment: 'Нашел под кассой'
+            },
+            {
+                id: '10',
                 direction: 'in',
                 date: "2014-11-30T11:10:56+0300",
                 amount: 10000,
--- webfront/src/resources/cashFlow/model.js
+++ webfront/src/resources/cashFlow/model.js
@@ -2,6 +2,8 @@ define(function(require, exports, module) {
     //requirements
     var Model = require('kit/model/model');
 
+    require('./mocks/delete');
+
     return Model.extend({
         urlRoot: Model.baseApiUrl + '/cashFlows',
         defaults: {
