7549daa48bf28e5308360555d45b1a21f47fb7a8
Andrew Borovin <a.borovin@crystals.ru>
2014-11-26 12:48:07 +0300

    fix add store button

--- webfront/src/config.test.js
+++
@@ -1,8 +0,0 @@
-define({
-    baseUrl: 'http://borovin.staging.api.lighthouse.pro',
-    baseApiUrl: 'http://borovin.staging.api.lighthouse.pro/api/1',
-    clientId: 'webfront_webfront',
-    clientSecret: 'secret',
-    //googleAnalyticsId: 'UA-56269916-2',
-    debugLevel: 2
-});
