345da66e96850b12e0fcb7110a527199fb32338d
Andrew Borovin <a.borovin@crystals.ru>
2014-11-28 19:25:22 +0300

    webdriver tests

--- webfront/tests/browser.js
+++
@@ -1,5 +0,0 @@
-module.exports = browser
-    .setViewportSize({
-        width: 1024,
-        height: 800
-    });
--- webfront/tests/test.js
+++ webfront/tests/test.js
@@ -1,16 +1,47 @@
-var expect = require('chai').expect,
-    browser = require('./browser');
+var expect = require('chai').expect;
 
-describe('Initial tests', function () {
+describe('Initial tests', function() {
 
-    it('title is ok', function(done) {
+    //it('main page', function(done) {
+    //
+    //    browser
+    //        .url(host)
+    //        .execute(function() {
+    //            require({
+    //                deps: [
+    //                    'resources/currentUser/mocks/get'
+    //                ]
+    //            });
+    //        })
+    //        .getTitle(function(err, value) {
+    //            expect(value).to.equal('Dreamkas');
+    //        })
+    //        .waitFor('body[status="loaded"]', function() {
+    //            browser.saveScreenshot('snapshot1.png');
+    //        })
+    //        .call(done);
+    //
+    //});
+
+    it('stores page', function(done) {
 
         browser
-            .url(host)
-            .getTitle(function(err, value){
+            .url(host + '/stores')
+            .execute(function() {
+                require({
+                    deps: [
+                        'resources/currentUser/mocks/get',
+                        'resources/store/mocks/get_3'
+                    ]
+                });
+            })
+            .getTitle(function(err, value) {
                 expect(value).to.equal('Dreamkas');
             })
-            .end(done);
+            .waitFor('body[status="loaded"]', function() {
+                browser.saveScreenshot('snapshot2.png');
+                done();
+            });
 
     });
 
