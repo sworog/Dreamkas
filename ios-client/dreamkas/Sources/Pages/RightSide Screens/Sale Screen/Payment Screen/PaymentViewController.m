3dae318633769fbafd5c01e875d7744c861c2f76
Ildar Sibagatov <siggb@yandex.com>
2014-12-01 14:18:09 +0300

    - some visual stuff on payment screen

--- ios-client/dreamkas/Sources/Pages/RightSide Screens/Sale Screen/Payment Screen/PaymentViewController.m
+++ ios-client/dreamkas/Sources/Pages/RightSide Screens/Sale Screen/Payment Screen/PaymentViewController.m
@@ -8,6 +8,14 @@
 
 #import "PaymentViewController.h"
 
+@interface PaymentViewController () <UITextFieldDelegate>
+
+@property (nonatomic, weak) IBOutlet CustomEmptyButton *payButton, *payByUsingCard;
+@property (nonatomic, weak) IBOutlet CustomLabel *firstLabel, *secondLabel;
+@property (nonatomic, weak) IBOutlet CustomTextField *paymentSumField;
+
+@end
+
 @implementation PaymentViewController
 
 #pragma mark - View Lifecycle
@@ -17,13 +25,25 @@
     [super viewDidLoad];
     
     [self initCloseButton];
+    
+    self.firstLabel.font =
+    self.secondLabel.font = DefaultFont(12);
+    self.firstLabel.textColor =
+    self.secondLabel.textColor = [DefaultBlackColor colorWithAlphaComponent:0.54];
 }
 
 #pragma mark - Configuration Methods
 
 - (void)configureLocalization
 {
-    [self setTitle:NSLocalizedString(@"payment_page_title", nil)];
+    NSString *title = [NSString stringWithFormat:@"%@ ₽", [CountSaleHelper countSaleItemsTotalSum]];
+    [self setTitle:title];
+    
+    [self.payButton setTitle:NSLocalizedString(@"payment_page_pay_button", nil) forState:UIControlStateNormal];
+    [self.payByUsingCard setTitle:NSLocalizedString(@"payment_page_pay_by_using_card_button", nil) forState:UIControlStateNormal];
+    
+    [self.firstLabel setText:NSLocalizedString(@"payment_page_payment_text", nil)];
+    [self.secondLabel setText:NSLocalizedString(@"payment_page_card_payment_text", nil)];
 }
 
 - (void)configureAccessibilityLabels
