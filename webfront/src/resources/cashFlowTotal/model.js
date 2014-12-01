8b58b13ac639de3d2a2c7473719dcf93df8d6e9b
denisraslov <denis.raslov@yandex.ru>
2014-12-01 14:16:57 +0300

    Removed mock for cash flow total model

--- webfront/src/resources/cashFlowTotal/model.js
+++ webfront/src/resources/cashFlowTotal/model.js
@@ -2,8 +2,6 @@ define(function(require, exports, module) {
     //requirements
     var Model = require('kit/model/model');
 
-    require('./mocks/get');
-
     return Model.extend({
         url: Model.baseApiUrl + '/reports/cashFlows',
         defaults: {
