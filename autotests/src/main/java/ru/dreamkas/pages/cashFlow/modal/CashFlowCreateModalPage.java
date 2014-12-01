7dbb7d20100768f611ef3137c033a04089ede257
Andrey Tolpeev <atolpeev@crystals.ru>
2014-12-01 19:31:22 +0300

    Added us-132 cash flow title check test scenarios

--- autotests/src/main/java/ru/dreamkas/pages/cashFlow/modal/CashFlowCreateModalPage.java
+++ autotests/src/main/java/ru/dreamkas/pages/cashFlow/modal/CashFlowCreateModalPage.java
@@ -1,10 +1,12 @@
 package ru.dreamkas.pages.cashFlow.modal;
 
 import org.openqa.selenium.WebDriver;
+import ru.dreamkas.apihelper.DateTimeHelper;
 import ru.dreamkas.elements.bootstrap.buttons.PrimaryBtnFacade;
 import ru.dreamkas.elements.items.Input;
 import ru.dreamkas.elements.items.JSInput;
 import ru.dreamkas.elements.items.RadioButton;
+import ru.dreamkas.handler.field.FieldChecker;
 import ru.dreamkas.pages.modal.ModalWindowPage;
 
 public class CashFlowCreateModalPage extends ModalWindowPage{
@@ -38,6 +40,17 @@ public class CashFlowCreateModalPage extends ModalWindowPage{
             @Override
             public String getText() {
                 return getVisibleWebElementFacade().getText();
+            }
+
+            @Override
+            public FieldChecker getFieldChecker() {
+                return new FieldChecker(this) {
+                    @Override
+                    public void assertValueEqual(String expectedValue) {
+                        String convertedDate = DateTimeHelper.getDate(expectedValue);
+                        super.assertValueEqual(convertedDate);
+                    }
+                };
             }
         };
     }
--- autotests/src/main/java/ru/dreamkas/pages/modal/ModalWindowPage.java
+++ autotests/src/main/java/ru/dreamkas/pages/modal/ModalWindowPage.java
@@ -29,7 +29,7 @@ public abstract class ModalWindowPage extends CommonPageObject implements ModalW
     public abstract String modalWindowXpath();
 
     public String getTitle() {
-        return findVisibleElement(By.xpath(modalWindowXpath() + "//*[@class='modal-title']")).getText();
+        return findVisibleElement(By.xpath(modalWindowXpath() + "//*[@class='modal__title']")).getText();
     }
 
     protected void putDefaultConfirmationOkButton(CommonItemType commonItemType) {
