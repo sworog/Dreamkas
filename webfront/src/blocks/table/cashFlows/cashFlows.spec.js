67bcb733d01a5fa1200a1c14f365f5ae9d43231d
denisraslov <denis.raslov@yandex.ru>
2014-12-01 16:57:52 +0300

    Cash flow total calculation test

---
+++ webfront/src/blocks/table/cashFlows/cashFlows.spec.js
@@ -0,0 +1,45 @@
+define(function(require, exports, module) {
+    //requirements
+    var Model = require('kit/model/model');
+    var CashFlowsCollection = require('resources/cashFlow/collection');
+    var CashFlowsTable = require('./cashFlows');
+
+    describe(module.id, function() {
+
+        var cashFlowsCollection;
+        var cashFlowsTable;
+
+        beforeEach(function() {
+
+            cashFlowsCollection = new CashFlowsCollection([
+                {
+                    amount: 1000,
+                    direction: 'in'
+                },
+                {
+                    amount: 2000,
+                    direction: 'out'
+                },
+                {
+                    amount: 10000,
+                    direction: 'in'
+                }
+            ]);
+
+            cashFlowsTable = new CashFlowsTable({
+                collection: cashFlowsCollection
+            });
+
+            cashFlowsTable.render();
+        });
+
+        it('total calculation', function() {
+
+            var totalModel = cashFlowsTable.models.total;
+
+            expect(totalModel.get('in')).toBe(11000);
+            expect(totalModel.get('out')).toBe(2000);
+            expect(totalModel.get('balance')).toBe(9000);
+        });
+    });
+});
