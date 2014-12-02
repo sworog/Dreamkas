6afb194db7e77d2c5bc87a95b28e4d00c8de2f78
Ildar Sibagatov <siggb@yandex.com>
2014-12-02 19:15:43 +0300

    - visual improvements by using material kit

---
+++ ios-client/dreamkas/Sources/Customs/Interface Elements/Buttons/FlatButton.swift
@@ -0,0 +1,11 @@
+//
+//  FlatButton.swift
+//  dreamkas
+//
+//  Created by sig on 02.12.14.
+//  Copyright (c) 2014 Dreamkas. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import Foundation
