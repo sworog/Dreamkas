f0d3a394cd6b61c1caa0bfdc92f8434fe95c62fe
Andrew Borovin <a.borovin@crystals.ru>
2014-12-04 16:19:52 +0300

    fix karma tests

---
+++ webfront/src/config.test.js
@@ -0,0 +1,8 @@
+window.CONFIG = {
+    baseUrl: 'http://dev.staging.api.lighthouse.pro',
+    baseApiUrl: 'http://dev.staging.api.lighthouse.pro/api/1',
+    clientId: 'webfront_webfront',
+    clientSecret: 'secret',
+    //googleAnalyticsId: 'UA-56269916-2',
+    debugLevel: 2
+};
