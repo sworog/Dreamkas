aa9d24bcecae8f2e2a4f66ee07333e7b9e3fb820
denisraslov <denis.raslov@yandex.ru>
2014-12-09 14:21:02 +0300

    First start progressbar fixes

--- webfront/src/pages/dashboard/progressbar.js
+++ webfront/src/pages/dashboard/progressbar.js
@@ -4,13 +4,18 @@ define(function(require, exports, module) {
 
     return Block.extend({
         template: require('ejs!./progressbar.ejs'),
-        width: function() {
+        setFillerWidth: function() {
 
-            var width;
+            var block = this,
+                $container = block.$('.page__dashboard__progressbarContainer'),
+                $filler = block.$('.page__dashboard__progressbarFiller'),
+                $completeIcon = block.$('.page__dashboard__progressbarCompleteIcon'),
+                width;
 
-            switch (Number(this.activeStep)) {
+            switch (Number(block.activeStep)) {
                 case 1:
                     width = 5;
+                    $filler.css('background', 'transparent');
                     break;
                 case 2:
                     width = 51;
@@ -20,10 +25,20 @@ define(function(require, exports, module) {
                     break;
                 case 4:
                     width = 100;
+                    $container.css('overflow', 'hidden');
+                    $completeIcon.show();
                     break;
             }
 
-            return width;
+            $filler.width(width + '%');
+        },
+        render: function() {
+            var block = this,
+                render = Block.prototype.render.apply(block, arguments);
+
+            block.setFillerWidth();
+
+            return render;
         }
     });
 });
