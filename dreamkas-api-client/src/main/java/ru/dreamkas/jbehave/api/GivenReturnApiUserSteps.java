6a33f074a6fb75039a727b42e8e87153203481d1
Andrey Tolpeev <atolpeev@crystals.ru>
2014-12-09 14:12:41 +0300

    Fixed addProductToSaleReturn annotaion method to resolve jbehave parcer confict

--- dreamkas-api-client/src/main/java/ru/dreamkas/jbehave/api/GivenReturnApiUserSteps.java
+++ dreamkas-api-client/src/main/java/ru/dreamkas/jbehave/api/GivenReturnApiUserSteps.java
@@ -13,18 +13,18 @@ public class GivenReturnApiUserSteps {
     ReturnBuilderSteps returnBuilderSteps;
 
     @Given("пользователь создает возврат с последнего чека c датой '$date'")
-    public void gicenUserCreatesSaleWithCashType(String date) throws JSONException {
+    public void gicenUserCreatesReturnWithCashType(String date) throws JSONException {
         returnBuilderSteps.createReturnFromLastSale(date);
     }
 
-    @Given("пользователь добавляет товар в чек с именем '$productName', количеством '$quantity'")
-    public void givenUserAddsProductToSale(String productName,
-                                           String quantity) throws JSONException {
+    @Given("пользователь добавляет товар в чек возврата с именем '$productName', количеством '$quantity'")
+    public void givenUserAddsProductToReturn(String productName,
+                                             String quantity) throws JSONException {
         returnBuilderSteps.putProductToReturn(productName, quantity);
     }
 
     @Given("пользователь с адресом электронной почты '$email' в магазине с именем '$storeName' совершает возврат по чеку")
-    public void givenUserWithEmailRigisterSaleInTheStoreWithName(String email, String storeName) throws IOException, JSONException {
+    public void givenUserWithEmailRigisterReturnInTheStoreWithName(String email, String storeName) throws IOException, JSONException {
         returnBuilderSteps.registerReturn(email, storeName);
     }
 }
