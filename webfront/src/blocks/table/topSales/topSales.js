276f377b0b9121d0ceb1d3d6bedcc1d6f5be263b
denisraslov <denis.raslov@yandex.ru>
2014-12-11 14:50:04 +0300

    Top sales period select

--- webfront/src/blocks/table/topSales/topSales.js
+++ webfront/src/blocks/table/topSales/topSales.js
@@ -3,6 +3,16 @@ define(function(require, exports, module) {
     var Table = require('blocks/table/table');
 
     return Table.extend({
-        template: require('ejs!./template.ejs')
+        template: require('ejs!./template.ejs'),
+        events: {
+            'change select[name="period"]': function(e) {
+                var block = this,
+                    select = e.currentTarget;
+
+            }
+        },
+        blocks: {
+            select_period: require('blocks/select/period/period')
+        }
     });
 });
