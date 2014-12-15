56d0146d07df4c610a646cdfd87a73f4ee6f1e5e
denisraslov <denis.raslov@yandex.ru>
2014-12-15 12:49:13 +0300

    Top sales mocks fix

--- webfront/src/resources/topSales/mocks/get.js
+++ webfront/src/resources/topSales/mocks/get.js
@@ -4,86 +4,62 @@ define(function(require, exports, module) {
 
     return ajaxMock({
         //request
-        url: '/reports/grossSalesByStores',
+        url: '/reports/grossSalesTop',
         type: 'GET',
 
         //response
         status: 200,
         responseText: [
             {
-                "store": {
-                    "id": "sdfhjsgdgfi9wegfbfdfsd0",
-                    "name": "Первый",
-                    "address": "Курортная улица 35"
+                "product": {
+                    "typeUnits": "unit",
+                    "type": "unit",
+                    "id": "548adb6c2cde6ed60f8b45680",
+                    "name": "Молоко",
+                    "units": "л",
+                    "typeProperties": {
+
+                    },
+                    "vat": 0,
+                    "sku": "10001",
+                    "retailPricePreference": "retailMarkup"
                 },
-                "today": {
-                    "now": {
-                        "date": "2013-11-25T16:37:25+0400",
-                        "value": 645466.45
-                    }
-                },
-                "yesterday": {
-                    "now": {
-                        "date": "2013-11-24T16:37:25+0400",
-                        "value": 542234.45
-                    }
-                },
-                "weekAgo": {
-                    "now": {
-                        "date": "2013-11-18T16:37:25+0400",
-                        "value": 710126.45
-                    }
-                }
+                "quantity": 33,
+                "grossSales": 10324.23
             },
             {
-                "store": {
-                    "id": "sdfhjsgdgfi9wegfbfdfsd1",
-                    "name": "Второй",
-                    "address": "Курортная улица 35"
+                "product": {
+                    "typeUnits": "unit",
+                    "type": "unit",
+                    "id": "548adb6c2cde6ed60f8b45681",
+                    "name": "Apple iPhone 6 Gold 64GB",
+                    "units": "шт",
+                    "typeProperties": {
+
+                    },
+                    "vat": 0,
+                    "sku": "10001",
+                    "retailPricePreference": "retailMarkup"
                 },
-                "today": {
-                    "now": {
-                        "date": "2013-11-25T16:37:25+0400",
-                        "value": 645466.45
-                    }
-                },
-                "yesterday": {
-                    "now": {
-                        "date": "2013-11-24T16:37:25+0400",
-                        "value": 54200034.45
-                    }
-                },
-                "weekAgo": {
-                    "now": {
-                        "date": "2013-11-18T16:37:25+0400",
-                        "value": 71126.45
-                    }
-                }
+                "quantity": 330,
+                "grossSales": 10324.23
             },
             {
-                "store": {
-                    "id": "sdfhjsgdgfi9wegfbfdfsd2",
-                    "name": "Третий",
-                    "address": "Курортная улица 35"
+                "product": {
+                    "typeUnits": "unit",
+                    "type": "unit",
+                    "id": "548adb6c2cde6ed60f8b45682",
+                    "name": "Apple iPhone 6 Space Grey 64GB",
+                    "units": "шт",
+                    "typeProperties": {
+
+                    },
+                    "vat": 0,
+                    "sku": "10001",
+                    "retailPricePreference": "retailMarkup"
                 },
-                "today": {
-                    "now": {
-                        "date": "2013-11-25T16:37:25+0400",
-                        "value": 645466.45
-                    }
-                },
-                "yesterday": {
-                    "now": {
-                        "date": "2013-11-24T16:37:25+0400",
-                        "value": 542234.45
-                    }
-                },
-                "weekAgo": {
-                    "now": {
-                        "date": "2013-11-18T16:37:25+0400",
-                        "value": 710126.45
-                    }
-                }
+                "quantity": 330,
+                "grossSales": 10324.23
             }
         ]
     });
