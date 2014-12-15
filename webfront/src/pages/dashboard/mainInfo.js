29b184f95e3840e0b93781408b1b5d1796d3a303
Andrew Borovin <a.borovin@crystals.ru>
2014-12-15 13:53:32 +0300

    move phrases to common dictionary

--- webfront/src/pages/dashboard/mainInfo.js
+++ webfront/src/pages/dashboard/mainInfo.js
@@ -1,21 +1,13 @@
 define(function(require, exports, module) {
     //requirements
     var Block = require('kit/block/block'),
+        getText = require('kit/getText/getText'),
         moment = require('moment');
 
     return Block.extend({
         template: require('ejs!./mainInfo.ejs'),
-        weekAgoDays: {
-            0: 'прошлый понедельник',
-            1: 'прошлый вторник',
-            2: 'прошлую среду',
-            3: 'прошлый четверг',
-            4: 'прошлую пятницу',
-            5: 'прошлую субботу',
-            6: 'прошлое воскресенье'
-        },
         weekAgoDay: function() {
-            return this.weekAgoDays[moment(this.resource.get('today').now.date).weekday()];
+            return getText('for last week', [moment(this.resource.get('today').now.date).weekday()]);
         }
     });
 });
