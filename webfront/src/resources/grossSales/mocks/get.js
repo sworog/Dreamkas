68b76cd5ca0909363fe526eea20ab9bc6b79577a
Andrew Borovin <a.borovin@crystals.ru>
2014-12-16 12:46:03 +0300

    update mocks

--- webfront/src/resources/grossSales/mocks/get.js
+++ webfront/src/resources/grossSales/mocks/get.js
@@ -13,21 +13,21 @@ define(function(require, exports, module) {
             today: {
                 now: {
                     date: "2013-11-25T16:37:25+0400",
-                    value: 645466.45
+                    value: 12345.45
                 }
             },
             yesterday: {
                 now: {
                     date: "2013-11-24T16:37:25+0400",
-                    value: 542234.45,
-                    diff: 10.12
+                    value: 45623.45,
+                    diff: 5.12
                 }
             },
             weekAgo: {
                 now: {
                     date: "2014-12-05T16:00:25+0400",
-                    value: 710126.45,
-                    diff: -9.25
+                    value: 983478.45,
+                    diff: -2.25
                 }
             }
         }
