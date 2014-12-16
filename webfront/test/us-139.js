42ba66ce29bbe51e516ad8ab0bb667da349e0dcc
Andrew Borovin <a.borovin@crystals.ru>
2014-12-16 14:09:04 +0300

    add page mock

--- webfront/test/us-139.js
+++ webfront/test/us-139.js
@@ -6,26 +6,8 @@ describe.only('US 139: Дашборд', function() {
     this.timeout(10000);
 
     beforeEach(function(done) {
-
         browser
-            .url(process.env.host + '/test.html')
-            .execute(function() {
-                requirejs([
-                    'app',
-                    'resources/currentUser/mocks/get',
-                    'resources/grossMargin/mocks/get',
-                    'resources/grossSales/mocks/get',
-                    'resources/storesGrossSales/mocks/get',
-                    'resources/topSales/mocks/get',
-                    'resources/firstStart/mocks/get_success',
-                    'resources/store/mocks/get_0',
-                    'resources/group/mocks/get_0',
-                    'resources/supplier/mocks/get_0',
-                    'resources/product/mocks/get_0'
-                ], function(app) {
-                    app.start('/');
-                });
-            })
+            .url(process.env.host + '/pages/dashboard/mocks/1.html')
             .waitFor('body[status="loaded"]', 5000)
             .pause(1000, done)
     });
