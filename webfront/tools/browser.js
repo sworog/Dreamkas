8b7db4b0a8feb2082a125e209945807b8ffed555
Andrew Borovin <a.borovin@crystals.ru>
2014-12-16 13:57:52 +0300

    add origin screenshot

--- webfront/tools/browser.js
+++ webfront/tools/browser.js
@@ -29,8 +29,8 @@ var browser = webdriverio
         y: 0
     })
     .setViewportSize({
-        width: 1024,
-        height: 500
+        width: 1280,
+        height: 900
     })
     .timeoutsAsyncScript(5000);
 
